200e1b1b5793beff02774d5502e49cc851565053
David Bloomin <daveey@gmail.com>
2024-08-18 21:08:51 -0700

    add setup script

---
+++ devops/setup_build.sh
@@ -0,0 +1,25 @@
+mkdir deps
+cd deps
+
+git clone https://github.com/Metta-AI/fast_gae.git
+cd fast_gae
+python setup.py build_ext --inplace
+pip install -e .
+cd ..
+
+git clone https://github.com/Metta-AI/puffergrid.git
+cd puffergrid
+python setup.py build_ext --inplace
+pip install -e .
+cd ..
+
+git clone https://github.com/Metta-AI/mettagrid.git
+cd mettagrid
+python setup.py build_ext --inplace
+pip install -e .
+cd ..
+
+git clone https://github.com/Metta-AI/sample_factory.git
+cd sample_factory
+pip install -e .
+cd ..
