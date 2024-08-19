8f4c156700c35553e7cdf0ccb37f06ea0142bf70
David Bloomin <daveey@gmail.com>
2024-08-18 21:55:19 -0700

    update docker to mettaai

--- devops/docker/build.sh
+++ devops/docker/build.sh
@@ -1,2 +1,2 @@
-docker build -f devops/docker/Dockerfile.base -t docker.io/daveey/metta-base:latest .
-docker build -f devops/docker/Dockerfile -t docker.io/daveey/metta:latest .
+docker build -f devops/docker/Dockerfile.base -t docker.io/mettaai/metta-base:latest .
+docker build -f devops/docker/Dockerfile -t docker.io/mettaai/metta:latest .
