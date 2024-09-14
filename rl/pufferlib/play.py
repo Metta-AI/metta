cdd0a2abf53a93550c9e9d51232b945ff107e405
David Bloomin <daveey@gmail.com>
2024-09-14 14:57:14 -0700

    improve play logic

--- rl/pufferlib/play.py
+++ rl/pufferlib/play.py
@@ -5,19 +5,19 @@ import time
 import numpy as np
 from rl.pufferlib.policy import load_policy_from_uri
 from util.stats import print_policy_stats
+from mettagrid.renderer.raylib_renderer import MettaGridRaylibRenderer
 
 def play(cfg: OmegaConf, vecenv):
     device = cfg.framework.pufferlib.device
-
     policy = load_policy_from_uri(cfg.eval.policy_uri, cfg)
     agents_count = cfg.env.game.num_agents
 
     obs, _ = vecenv.reset()
+    env = vecenv.envs[0]
+    renderer = MettaGridRaylibRenderer(env._c_env.map_width(), env._c_env.map_height(), cfg.env)
     policy_rnn_state = None
 
-    start = time.time()
     total_rewards = np.zeros(vecenv.num_agents)
-    agent_stats = [{} for a in range(vecenv.num_agents)]
     while True:
         with torch.no_grad():
             obs = torch.as_tensor(obs).to(device=device)
@@ -29,9 +29,14 @@ def play(cfg: OmegaConf, vecenv):
                 actions, _, _, _ = policy(obs)
 
         actions = actions.view(agents_count, -1)
-        render_result = vecenv.envs[0].render()
-        if render_result["action"] is not None and render_result["selected_agent_idx"] is not None:
-            actions[render_result["selected_agent_idx"]] = torch.tensor(render_result["action"], device=actions.device)
+        render_result = renderer.render(
+            env._c_env.current_timestep(),
+            env._c_env.grid_objects(),
+            actions
+        )
 
-        obs, rewards, dones, truncated, infos = vecenv.step(actions.cpu().numpy())
+        obs, rewards, dones, truncated, infos = vecenv.step(render_result["actions"].cpu().numpy())
         total_rewards += rewards
+        if any(dones) or any(truncated):
+            print(f"Total rewards: {total_rewards}")
+            break
