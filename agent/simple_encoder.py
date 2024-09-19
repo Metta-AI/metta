e518d135f8e809762caf275571860dc8f4aa40f6
David Bloomin <daveey@gmail.com>
2024-09-19 16:34:41 -0700

    add normalization to simple encoder

--- agent/simple_encoder.py
+++ agent/simple_encoder.py
@@ -1,6 +1,7 @@
 from torch import nn
 from .lib.util import layer_init
 from omegaconf import OmegaConf
+import torch
 
 class SimpleConvAgent(nn.Module):
 
@@ -23,10 +24,43 @@ class SimpleConvAgent(nn.Module):
             layer_init(nn.Linear(self._cnn_channels, self._output_dim)),
             nn.ReLU(),
         )
+        obs_norms = [1] * self._num_objects
+        if cfg["normalize_features"]:
+            obs_norms = [
+                1, # 'agent',
+                1, # 'agent:hp',
+                1, # 'agent:frozen',
+                255, # 'agent:energy',
+                1, # 'agent:orientation',
+                1, # 'agent:shield',
+                5, # 'agent:inv:r1',
+                5, # 'agent:inv:r2',
+                5, # 'agent:inv:r3',
+                1, # 'wall',
+                10, # 'wall:hp',
+                1, # 'generator',
+                30, # 'generator:hp',
+                30, # 'generator:r1',
+                1, # 'generator:ready',
+                1, # 'converter',
+                30, # 'converter:hp',
+                5, # 'converter:input_resource',
+                5, # 'converter:output_resource',
+                100, # 'converter:output_energy',
+                1, # 'converter:ready',
+                1, # 'altar',
+                30, # 'altar:hp',
+                1, # 'altar:ready'
+            ]
+
+
+        self._obs_norm = torch.tensor(obs_norms, dtype=torch.float32)
+        self._obs_norm = self._obs_norm.view(1, self._num_objects, 1, 1)
 
     def forward(self, obs_dict):
-        obs_transformed = obs_dict[self._obs_key]
-        return self.network(obs_transformed)
+        self._obs_norm.to(obs_dict[self._obs_key].device)
+        obs = obs_dict[self._obs_key] / self._obs_norm
+        return self.network(obs)
 
     def output_dim(self):
         return self._output_dim
