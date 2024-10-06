ab03b984428fda9ce83f582c5d58b2f5e22e58a7
David Bloomin <daveey@gmail.com>
2024-10-06 16:45:29 -0700

    update wandb carbs

--- devops/run.sh
+++ devops/run.sh
@@ -10,6 +10,7 @@ cd deps/puffergrid && git pull && python setup.py build_ext --inplace && cd ../.
 # cd deps/pufferlib && git pull && python setup.py build_ext --inplace && cd ../..
 cd deps/fast_gae && git pull && python setup.py build_ext --inplace && cd ../..
 cd deps/mettagrid && git pull && python setup.py build_ext --inplace && cd ../..
+cd deps/wandb_carbs && git pull && cd ../..
 HYDRA_FULL_ERROR=1 python -m tools.$cmd \
     hardware=pufferbox \
     wandb.enabled=true \
