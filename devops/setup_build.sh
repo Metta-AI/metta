f4c7a36c3486c27293fca1e6d7477ecd349696ff
David Bloomin <daveey@gmail.com>
2024-10-16 15:59:28 -0700

    update the pufferlib branch

--- devops/setup_build.sh
+++ devops/setup_build.sh
@@ -18,7 +18,7 @@ cd ..
 git clone https://github.com/Metta-AI/pufferlib.git
 cd pufferlib
 git fetch
-git checkout dev
+git checkout metta
 python setup.py build_ext --inplace
 pip install -e .
 cd ..
