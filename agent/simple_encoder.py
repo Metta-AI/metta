ec57af7d268fa0217714728f3269cdb9c2de5260
David Bloomin <daveey@gmail.com>
2024-10-23 17:44:50 -0700

    simplify env configs, and support auto normalization in simple encoder

--- agent/simple_encoder.py
+++ agent/simple_encoder.py
@@ -1,11 +1,10 @@
 from typing import List
 
-import torch
 from omegaconf import OmegaConf
 from pufferlib.pytorch import layer_init
 from torch import nn
 
-
+from agent.feature_encoder import FeatureListNormalizer
 class SimpleConvAgent(nn.Module):
 
     def __init__(self,
@@ -29,42 +28,52 @@ class SimpleConvAgent(nn.Module):
             layer_init(nn.Linear(self._cnn_channels, self._output_dim)),
             nn.ReLU(),
         )
-        obs_norms = [1] * self._num_objects
-        if cfg["normalize_features"]:
-            obs_norms = [
-                1, # 'agent',
-                1, # 'agent:hp',
-                1, # 'agent:frozen',
-                255, # 'agent:energy',
-                1, # 'agent:orientation',
-                1, # 'agent:shield',
-                5, # 'agent:inv:r1',
-                5, # 'agent:inv:r2',
-                5, # 'agent:inv:r3',
-                1, # 'wall',
-                10, # 'wall:hp',
-                1, # 'generator',
-                30, # 'generator:hp',
-                30, # 'generator:r1',
-                1, # 'generator:ready',
-                1, # 'converter',
-                30, # 'converter:hp',
-                5, # 'converter:input_resource',
-                5, # 'converter:output_resource',
-                100, # 'converter:output_energy',
-                1, # 'converter:ready',
-                1, # 'altar',
-                30, # 'altar:hp',
-                1, # 'altar:ready'
-            ]
+
+        self._normalizer = None
+        if cfg.normalize_features:
+            self._normalizer = FeatureListNormalizer(
+                grid_features, obs_space[self._obs_key].shape[1:])
+
+        # obs_norms = [1] * self._num_objects
+        # if cfg.normalize_features:
+        #     obs_norms = [
+        #         1, # 'agent',
+        #         1, # 'agent:hp',
+        #         1, # 'agent:frozen',
+        #         255, # 'agent:energy',
+        #         1, # 'agent:orientation',
+        #         1, # 'agent:shield',
+        #         5, # 'agent:inv:r1',
+        #         5, # 'agent:inv:r2',
+        #         5, # 'agent:inv:r3',
+        #         1, # 'wall',
+        #         10, # 'wall:hp',
+        #         1, # 'generator',
+        #         30, # 'generator:hp',
+        #         30, # 'generator:r1',
+        #         1, # 'generator:ready',
+        #         1, # 'converter',
+        #         30, # 'converter:hp',
+        #         5, # 'converter:input_resource',
+        #         5, # 'converter:output_resource',
+        #         100, # 'converter:output_energy',
+        #         1, # 'converter:ready',
+        #         1, # 'altar',
+        #         30, # 'altar:hp',
+        #         1, # 'altar:ready'
+        #     ]
 
 
-        self._obs_norm = torch.tensor(obs_norms, dtype=torch.float32)
-        self._obs_norm = self._obs_norm.view(1, self._num_objects, 1, 1)
+        # self._obs_norm = torch.tensor(obs_norms, dtype=torch.float32)
+        # self._obs_norm = self._obs_norm.view(1, self._num_objects, 1, 1)
 
     def forward(self, obs_dict):
-        self._obs_norm = self._obs_norm.to(obs_dict[self._obs_key].device)
-        obs = obs_dict[self._obs_key] / self._obs_norm
+        obs = obs_dict[self._obs_key]
+        if self._normalizer:
+            self._normalizer(obs)
+
+        # self._obs_norm = self._obs_norm.to(obs_dict[self._obs_key].device)
+        # obs = obs_dict[self._obs_key] / self._obs_norm
         return self.network(obs)
 
     def output_dim(self):
