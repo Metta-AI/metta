da4dba0bdda758421d2068b220a77020fe037702
treeform <starplant@gmail.com>
2024-12-17 14:27:04 -0800

    working

--- agent/simple_encoder.py
+++ agent/simple_encoder.py
@@ -64,6 +64,8 @@ class SimpleConvAgent(nn.Module):
                 1, # 'altar',
                 30, # 'altar:hp',
                 1, # 'altar:ready'
+                10, # 'last_action'
+                10, # 'last_action_argument'
             ]
 
             self._obs_norm = torch.tensor(obs_norms, dtype=torch.float32)
