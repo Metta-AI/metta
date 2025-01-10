f6baa9a42861875c723350220f16c95b2c973627
treeform <starplant@gmail.com>
2025-01-10 09:39:38 -0800

    Move kinship into cfg.game.

--- mettagrid/mettagrid.pyx
+++ mettagrid/mettagrid.pyx
@@ -30,64 +30,66 @@ cdef class MettaGrid(GridEnv):
         list _agents_to_team
         list _team_to_agents
 
-    def __init__(self, cfg: OmegaConf, map: np.ndarray):
-        cfg = OmegaConf.create(cfg)
+    def __init__(self, env_cfg: OmegaConf, map: np.ndarray):
+        cfg = OmegaConf.create(env_cfg.game)
         self._cfg = cfg
+        print(cfg)
 
         obs_encoder = MettaObservationEncoder()
-        if cfg.compact_obs:
+        if env_cfg.compact_obs:
             obs_encoder = MettaCompactObservationEncoder()
 
         actions = []
-        if cfg.game.actions.noop.enabled:
-            actions.append(Noop(cfg.game.actions.noop))
-        if cfg.game.actions.move.enabled:
-            actions.append(Move(cfg.game.actions.move))
-        if cfg.game.actions.rotate.enabled:
-            actions.append(Rotate(cfg.game.actions.rotate))
-        if cfg.game.actions.use.enabled:
-            actions.append(Use(cfg.game.actions.use))
-        if cfg.game.actions.attack.enabled:
-            actions.append(Attack(cfg.game.actions.attack))
-        if cfg.game.actions.shield.enabled:
-            actions.append(Shield(cfg.game.actions.shield))
-        if cfg.game.actions.gift.enabled:
-            actions.append(Gift(cfg.game.actions.gift))
-        if cfg.game.actions.swap.enabled:
-            actions.append(Swap(cfg.game.actions.swap))
+
+        if cfg.actions.noop.enabled:
+            actions.append(Noop(cfg.actions.noop))
+        if cfg.actions.move.enabled:
+            actions.append(Move(cfg.actions.move))
+        if cfg.actions.rotate.enabled:
+            actions.append(Rotate(cfg.actions.rotate))
+        if cfg.actions.use.enabled:
+            actions.append(Use(cfg.actions.use))
+        if cfg.actions.attack.enabled:
+            actions.append(Attack(cfg.actions.attack))
+        if cfg.actions.shield.enabled:
+            actions.append(Shield(cfg.actions.shield))
+        if cfg.actions.gift.enabled:
+            actions.append(Gift(cfg.actions.gift))
+        if cfg.actions.swap.enabled:
+            actions.append(Swap(cfg.actions.swap))
 
         GridEnv.__init__(
             self,
-            cfg.game.num_agents,
+            cfg.num_agents,
             map.shape[1],
             map.shape[0],
-            cfg.game.max_steps,
+            cfg.max_steps,
             dict(ObjectLayers).values(),
-            cfg.game.obs_width, cfg.game.obs_height,
+            cfg.obs_width, cfg.obs_height,
             obs_encoder,
             actions,
             [ ResetHandler() ],
-            use_flat_actions=cfg.flatten_actions,
-            track_last_action=cfg.track_last_action
+            use_flat_actions=env_cfg.flatten_actions,
+            track_last_action=env_cfg.track_last_action
         )
 
         cdef Agent *agent
         for r in range(map.shape[0]):
             for c in range(map.shape[1]):
                 if map[r,c] == "W":
-                    self._grid.add_object(new Wall(r, c, cfg.game.objects.wall))
+                    self._grid.add_object(new Wall(r, c, cfg.objects.wall))
                     self._stats.game_incr("objects.wall")
                 elif map[r,c] == "g":
-                    self._grid.add_object(new Generator(r, c, cfg.game.objects.generator))
+                    self._grid.add_object(new Generator(r, c, cfg.objects.generator))
                     self._stats.game_incr("objects.generator")
                 elif map[r,c] == "c":
-                    self._grid.add_object(new Converter(r, c, cfg.game.objects.converter))
+                    self._grid.add_object(new Converter(r, c, cfg.objects.converter))
                     self._stats.game_incr("objects.converter")
                 elif map[r,c] == "a":
-                    self._grid.add_object(new Altar(r, c, cfg.game.objects.altar))
+                    self._grid.add_object(new Altar(r, c, cfg.objects.altar))
                     self._stats.game_incr("objects.altar")
                 elif map[r,c][0] == "A":
-                    agent = new Agent(r, c, cfg.game.objects.agent)
+                    agent = new Agent(r, c, cfg.objects.agent)
                     self._grid.add_object(agent)
                     self.add_agent(agent)
                     self._stats.game_incr("objects.agent")
