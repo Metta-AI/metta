f407970e68d59612ad734a0eeca31078e3cc4eea
treeform <starplant@gmail.com>
2025-01-13 20:40:44 -0800

    Copy over puffergrid files and change imports.

---
+++ mettagrid/action.pyx
@@ -0,0 +1,23 @@
+from mettagrid.grid_object cimport GridObjectId
+from mettagrid.grid_env cimport GridEnv
+from mettagrid.action cimport ActionArg
+
+cdef class ActionHandler:
+    def __init__(self, string action_name):
+        self._action_name = action_name
+
+    cdef void init(self, GridEnv env):
+        self.env = env
+
+    cdef bint handle_action(
+        self,
+        unsigned int actor_id,
+        GridObjectId actor_object_id,
+        ActionArg arg):
+        return False
+
+    cdef unsigned char max_arg(self):
+        return 0
+
+    cpdef string action_name(self):
+        return self._action_name
