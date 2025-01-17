f4a3348a85308ca18c12fcf9071970fb2490936c
David Bloomin <daveey@gmail.com>
2025-01-17 15:20:01 -0800

    fix docker file to reference correct base

--- devops/docker/build.sh
+++ devops/docker/build.sh
@@ -1,2 +1,2 @@
-docker build -f devops/docker/Dockerfile.base -t docker.io/mettaai/metta-base:latest .
-docker build -f devops/docker/Dockerfile -t docker.io/mettaai/metta:latest .
+docker build -f devops/docker/Dockerfile.base -t mettaai/metta-base:latest .
+docker build -f devops/docker/Dockerfile -t mettaai/metta:latest .
