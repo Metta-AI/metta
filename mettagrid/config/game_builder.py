cf45822018e30955404c9f5bb1ae7327d1ae7a94
daphnedemekas <daphnedemekas@gmail.com>
2025-01-22 16:28:26 -0800

    room builders

--- mettagrid/config/game_builder.py
+++ mettagrid/config/game_builder.py
@@ -1,4 +1,3 @@
-from typing import Dict, List
 import numpy as np
 import yaml
 from omegaconf import OmegaConf
@@ -49,12 +48,59 @@ class MettaGridGameBuilder():
 
         if self.map_config.uri:
             return self.build_map_from_ascii(self.map_config.uri)
+        
+        elif self.map_config.rooms: #target is a list of room classes and their params?
+            return self.build_map_from_target(self.map_config.rooms)
 
         elif "rooms" in layout:
             return self.build_map(layout.rooms)
         else:
             return self.build_map(
                 [["room"] * layout.rooms_x] * layout.rooms_y)
+        
+    def build_map_from_target(self, room_configs):
+
+        rooms = []
+
+        max_height, max_width = 0, 0
+
+        for room in room_configs:
+            room_array = room.build_room()
+            max_height = max(max_height, room_array.shape[0])
+            max_width = max(max_width, room_array.shape[1])
+            rooms.append(room_array)
+
+        # Determine grid dimensions based on number of rooms
+        n_rooms = len(rooms)
+
+        grid_rows = int(np.ceil(np.sqrt(n_rooms)))
+        grid_cols = int(np.ceil(n_rooms / grid_rows))
+
+        # Create empty grid to hold all rooms
+        level = np.full((grid_rows * max_height, grid_cols * max_width), 
+                      self._symbols["wall"], dtype="U6")
+        
+        # Place rooms into grid
+        for i in range(n_rooms):
+            row = i // grid_cols
+            col = i % grid_cols
+            room_height = rooms[i].shape[0]
+            room_width = rooms[i].shape[1]
+            
+            # Calculate starting position to center the room in its grid cell
+            start_row = row * max_height + (max_height - room_height) // 2
+            start_col = col * max_width + (max_width - room_width) // 2
+            
+            # Place room in centered position
+            level[start_row:start_row + room_height,
+                 start_col:start_col + room_width] = rooms[i]
+            
+        b = self.map_config.border
+        h, w = level.shape
+        final_level = np.full((h + b * 2, w + b * 2), self._symbols["wall"], dtype="U6")
+        final_level[b:b + h, b:b + w] = level
+
+        return final_level
         
     def build_map_from_ascii(self, ascii_map_uri):
         """Currently this is only for a single room"""
