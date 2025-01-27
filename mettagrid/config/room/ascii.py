d2eba9bd988077935851dd1eb683176bbe88581f
daphnedemekas <daphnedemekas@gmail.com>
2025-01-26 22:10:58 -0800

    some renaming and make odd

--- mettagrid/config/room/ascii.py
+++ mettagrid/config/room/ascii.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from mettagrid.config.room.room import Room, OBJECTS, GameObject
 
-class AsciiRoom(Room):
+class Asci(Room):
     def __init__(self, uri: str, border_width: int = 0, border_object: GameObject = OBJECTS.Wall):
         super().__init__(border_width=border_width, border_object=border_object)
         with open(uri, "r", encoding="utf-8") as f:
