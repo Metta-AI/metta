c6e9f5f95e12fc9d18e13c2f72191d54e62ffede
David Bloomin <daveey@gmail.com>
2025-01-30 17:11:37 -0800

    fix stats

--- mettagrid/actions/attack.pyx
+++ mettagrid/actions/attack.pyx
@@ -43,13 +43,13 @@ cdef class Attack(MettaActionHandler):
             if agent_target.shield:
                 shield_damage = -agent_target.update_energy(-actor.attack_damage, NULL)
                 self.env._stats.agent_add(actor_id, "shield_damage", shield_damage)
-                self.env._stats.agent_add(actor_id, actor.species_name + b".shield_damage", shield_damage)
+                self.env._stats.agent_add(actor_id, actor.species_name + ".shield_damage", shield_damage)
             if shield_damage < actor.attack_damage:
                 agent_target.shield = False
                 agent_target.frozen = agent_target.freeze_duration
                 agent_target.update_energy(-agent_target.energy, NULL)
-                self.env._stats.agent_incr(actor_id, b"attack.frozen")
-                self.env._stats.agent_incr(actor_id, actor.species_name + b".attack.frozen")
+                self.env._stats.agent_incr(actor_id, "attack.frozen")
+                self.env._stats.agent_incr(actor_id, actor.species_name + ".attack.frozen")
                 for item in range(InventoryItem.InventoryCount):
                     actor.update_inventory(item, agent_target.inventory[item])
                     self.env._stats.agent_add(actor_id, InventoryItemNames[item] + ".stolen", agent_target.inventory[item])
@@ -66,8 +66,8 @@ cdef class Attack(MettaActionHandler):
             self.env._stats.agent_incr(actor_id, self._stats.target[object_target._type_id].c_str())
             self.env._stats.agent_incr(actor_id, actor.species_name + "." + self._stats.target[object_target._type_id].c_str())
             object_target.hp -= 1
-            self.env._stats.agent_incr(actor_id, b"damage." + ObjectTypeNames[object_target._type_id])
-            self.env._stats.agent_incr(actor_id, actor.species_name + b".damage." + ObjectTypeNames[object_target._type_id])
+            self.env._stats.agent_incr(actor_id, "damage." + ObjectTypeNames[object_target._type_id])
+            self.env._stats.agent_incr(actor_id, actor.species_name + ".damage." + ObjectTypeNames[object_target._type_id])
             if object_target.hp <= 0:
                 self.env._grid.remove_object(object_target)
                 self.env._stats.agent_incr(actor_id, "destroyed." + ObjectTypeNames[object_target._type_id])
