f4e0f1de41f5a1afff1e964859fc83cfbc51ba41
Gabriel Keith <gabriel@stem.ai>
2025-02-13 01:00:31 -0500

    updated amago object_normalizer

--- rl/amago/cnn.py
+++ rl/amago/cnn.py
@@ -4,7 +4,7 @@ import gin
 import torch
 import torch.nn as nn
 
-from agent.simple_encoder import OBS_NORMALIZATIONS
+from agent.lib.observation_normalizer import ObservationNormalizer
 
 
 @gin.configurable
@@ -25,16 +25,13 @@ class MettaAmagoCNN(CNN):
         self.conv1 = nn.Conv2d(C, channels[0], kernel_size=5, stride=3)
         self.conv2 = nn.Conv2d(channels[0], channels[1], kernel_size=3, stride=1)
 
-        obs_norms = [OBS_NORMALIZATIONS[k] for k in grid_features] 
-        self._obs_norm = torch.tensor(obs_norms, dtype=torch.float32)
-        self._obs_norm = self._obs_norm[None, :, None, None]
+        self.object_normalizer = ObservationNormalizer(grid_features)
 
     def conv_forward(self, imgs):
         obs = imgs
 
-        if self._obs_norm is not None:
-            self._obs_norm = self._obs_norm.to(obs.device)
-            obs = obs / self._obs_norm
+        if self.object_normalizer is not None:
+            obs = self.object_normalizer(obs)
         
         x = self.activation(self.conv1(obs))
         x = self.activation(self.conv2(x))
