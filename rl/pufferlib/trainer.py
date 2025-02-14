3e8c7fe6b163ce541779d916350e1bdf89e48668
daphnedemekas <daphnedemekas@gmail.com>
2025-02-13 16:41:58 -0800

    remove comment

--- rl/pufferlib/trainer.py
+++ rl/pufferlib/trainer.py
@@ -120,8 +120,6 @@ class PufferTrainer:
         self.cfg.eval.policy_uri = self.last_pr.uri
         eval = hydra.utils.instantiate(self.cfg.eval, self.policy_store, self.cfg.env, _recursive_ = False)
         stats = eval.evaluate()
-
-        #Here, we were only ever logging from the file that was just saved (always saving to file, whose name is the same as the latest policy)
         self.eval_stats_logger.log(stats)
 
         eval_stats_db = EvalStatsDB.from_uri(self.cfg.eval.eval_db_uri, self.wandb_run)
