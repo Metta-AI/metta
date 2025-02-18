55c477b78588c2bcb2b68898e3f08c904fe0800c
Daphne <45669133+daphnedemekas@users.noreply.github.com>
2025-02-18 11:39:51 -0800

    Evaluation suite (#54)
    * maze generation tool
    
    * add cylinder task script
    
    * change maze generation script so we stack mazes in multiple rooms
    
    * multi room cylinder
    
    * remove scripts from here because we move them to mettagrid, just hae maps
    
    * remove old map files
    
    * Update mettagrid.yaml
    
    * remove map yaml
    
    * rename and add default
    
    * pad walls instead of making odd
    
    * update configs for cylinder and ascii
    
    * do some renaming and some updating
    
    * remove make odd in yaml
    
    * renaming
    
    * updated after convo with david
    
    * update eval suite pipeline
    
    * clean up config
    
    * clean up configs
    
    * change mode to class variable
    
    * change to list of baselines and add environment name to logging
    
    * update cylinder config
    
    * update eval suite code
    
    * change convention for indexing
    
    * updated configs
    
    * Update cylinder.yaml
    
    * add in the ability to run with npcs
    
    * add config for explore exploit eval
    
    * update
    
    * cleanup eval suite code
    
    * update train and sweep evaluation but need to update the metrics logging for these, currently commented out
    
    * log locally and to wandb
    
    * analyzer with wandb and duckdb
    
    * clean up
    
    * cleanup
    
    * checkpoint
    
    * merge main
    
    * cp
    
    * restructure analysis code
    
    * cp
    
    * cleanup anaylsis code
    
    * fix filters for stats
    
    * Update policy_store.py
    
    * simplify configs
    
    * remove mann whitney for now
    
    * revert to old statslibrary
    
    * Update stats_library.py
    
    * Update logging.py
    
    * fix play
    
    * fix evalsuite
    
    * edits
    
    * update analyzer
    
    * evalsuite
    
    * update rollout
    
    * rearrange
    
    * rename
    
    * rename
    
    * fix rollout
    
    * Delete configs/analyzer/example.yaml
    
    * Update sweep.yaml
    
    * Update sweep.yaml
    
    * Update trainer.yaml
    
    * remove import
    
    * eval metric
    
    * change from print to log
    
    * cleanup and delete stats library
    
    * update PR
    
    * fix file
    
    * fix rollout
    
    * update rollout and add cacheing
    
    * fix config
    
    * sweep eval
    
    * del
    
    * remove comment
    
    * change elo to epoch
    
    * Update config.yaml
    
    * changes from review
    
    * mfix daphne
    
    * take care of the case with omegaconf lists in env stats
    
    * take into account null eval db uri
    
    ---------
    
    Co-authored-by: David Bloomin <daveey@gmail.com>

---
+++ rl/eval/eval_stats_analyzer.py
@@ -0,0 +1,97 @@
+import logging
+from omegaconf import DictConfig
+from typing import Dict, Any, List, Optional
+from rl.eval.eval_stats_db import EvalStatsDB
+from tabulate import tabulate
+import fnmatch
+import pandas as pd
+from rl.eval.stats import significance_test
+logger = logging.getLogger("eval_stats_analyzer")
+
+class EvalStatsAnalyzer:
+    def __init__(
+        self,
+        stats_db: EvalStatsDB,
+        analysis: DictConfig,
+        **kwargs):
+        self.analysis = analysis
+        self.stats_db = stats_db
+        self.global_filters = analysis.get('filters', None)
+
+    def _filters(self, item):
+        local_filters = item.get('filters')
+
+        if not self.global_filters:
+            return local_filters
+
+        if not local_filters:
+            return self.global_filters
+
+        merged = {}
+        for key in set(self.global_filters) | set(local_filters):
+            global_val = self.global_filters.get(key)
+            local_val = local_filters.get(key)
+            # If both values exist and are lists, concatenate them.
+            if isinstance(global_val, list) and isinstance(local_val, list):
+                merged[key] = global_val + local_val
+            # Otherwise, prefer the local value if it exists; if not, use the global value.
+            else:
+                merged[key] = local_val if key in local_filters else global_val
+
+        return merged
+
+    @staticmethod
+    def log_significance(comparison_data, metrics, filters):
+        comparison_df = pd.DataFrame(comparison_data, columns=['Policy 1', 'Policy 2', 'p-value', 'effect size', 'interpretation', 'metric'])
+        logger.info(f"\nSignificance test results for {metrics}:\n" + f"with filters {filters}" +
+                   tabulate(comparison_df, headers='keys', tablefmt='grid'))
+
+
+    def log_result(self, result, metric, filters, significance):
+        result_table = tabulate(result, headers=["policy_name"] + list(result.keys()), tablefmt="grid", maxcolwidths=25)
+        logger.info(f"Results for {metric} with filters {filters}:\n{result_table}")
+        if len(significance) > 0:
+            self.log_significance(significance, metric, filters)
+
+    def _analyze_metrics(self, metric_fields: List[str], filters: Optional[Dict[str, Any]] = None, group_by_episode: bool = False) -> pd.DataFrame:
+        result_dfs = []
+        significance_results = []
+        for metric in metric_fields:
+            if not metric in self.stats_db.available_metrics:
+                logger.info(f"Metric {metric} not found in stats_db")
+                continue
+            df_per_episode = self.stats_db._metric(metric, filters, group_by_episode)
+
+            df_per_episode, df_metric = self.stats_db._metric(metric, filters, group_by_episode)
+            result_dfs.append(df_metric)
+
+            # Only calculate significance if there are at least 2 policies
+            if df_per_episode.shape[1] > 1:
+                significance_results += significance_test(df_per_episode, metric)
+
+        metrics_df = pd.concat(result_dfs, axis=1) if len(result_dfs) > 0 else None
+
+        return metrics_df, significance_results
+
+    def analyze(self):
+        metric_configs = {}
+        for cfg in self.analysis.metrics:
+            metric_configs[cfg] = fnmatch.filter(self.stats_db.available_metrics, cfg.metric)
+        results = []
+        significances = []
+
+        filters = None
+        for cfg, metrics in metric_configs.items():
+            filters = self._filters(cfg)
+            group_by_episode = cfg.get('group_by_episode', False)
+            result, significance = self._analyze_metrics(metrics, filters, group_by_episode)
+
+            if result is None:
+                logger.info(f"No data found for {metrics} with filters {filters}" + "\n")
+                continue
+
+            self.log_result(result, metrics, filters, significance)
+            results.append(result)
+            significances.append(significance)
+
+        return results, significances
