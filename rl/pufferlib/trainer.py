41206d03c7e25469216233227ead7f01a02d9304
al-does <avardakostas@gmail.com>
2025-02-21 16:20:06 -0800

    sp

--- rl/pufferlib/trainer.py
+++ rl/pufferlib/trainer.py
@@ -13,6 +13,7 @@ from fast_gae import fast_gae
 from omegaconf import OmegaConf
 from pathlib import Path
 from tqdm import tqdm
+from typing import List, Tuple
 
 from agent.policy_store import PolicyStore
 from rl.eval.eval_stats_logger import EvalStatsLogger
@@ -22,7 +23,6 @@ from rl.pufferlib.profile import Profile
 from rl.pufferlib.trainer_checkpoint import TrainerCheckpoint
 from rl.pufferlib.vecenv import make_vecenv
 from rl.pufferlib.trace import save_trace_image
-from rl.pufferlib.kickstarter import Kickstarter
 torch.set_float32_matmul_precision('high')
 
 logger = logging.getLogger("trainer")
@@ -166,12 +166,6 @@ class PufferTrainer:
             infos = defaultdict(list)
             lstm_h, lstm_c = experience.lstm_h, experience.lstm_c
             e3b_inv = experience.e3b_inv
-
-            #ks need to get each teacher's lstm
-            for teacher in self.kickstarter.teachers:
-
-                with profile.eval_misc:
-                    actions, logprob, _, value, (h, c), next_e3b, intrinsic_reward = teacher(o_device, (h, c), e3b=e3b)
 
         while not experience.full:
             with profile.env:
@@ -548,3 +542,93 @@ class AbortingTrainer(PufferTrainer):
         }, allow_val_change=True)
 
 
+class Kickstarter:
+    def __init__(self, trainer: PufferTrainer):
+        self.tr = trainer
+        self.activated = self.tr.trainer_cfg.kickstart
+        if not self.activated:
+            return
+        
+        self._load_policies()
+        self._make_experience_buffer()
+
+    def _load_policies(self):
+        self.teachers = []
+        teachers_cfgs = list(self.tr.trainer_cfg.teachers)
+        for teacher_cfg in teachers_cfgs:
+            policy_record = self.tr.policy_store.policy(teacher_cfg['policy_uri'])
+            policy = policy_record.policy()
+            policy.action_coef = teacher_cfg['action_coef']
+            policy.value_coef = teacher_cfg['value_coef']
+            if self.tr.trainer_cfg.compile:
+                policy = torch.compile(policy, mode=self.tr.trainer_cfg.compile_mode)
+            self.teachers.append(policy)
+
+    def _make_experience_buffer(self):
+        obs_shape = self.tr.vecenv.single_observation_space.shape
+        obs_dtype = self.tr.vecenv.single_observation_space.dtype
+        atn_shape = self.tr.vecenv.single_action_space.shape
+        atn_dtype = self.tr.vecenv.single_action_space.dtype
+        total_agents = self.tr.vecenv.num_agents
+
+        self.teacher_experiences = []
+        for teacher in self.teachers:
+            lstm = teacher.lstm
+            self.teacher_experiences.append(Experience(self.tr.trainer_cfg.batch_size, self.tr.trainer_cfg.bptt_horizon,
+                self.tr.trainer_cfg.minibatch_size, self.tr.policy.hidden_size, obs_shape, obs_dtype, atn_shape, atn_dtype,
+                self.tr.trainer_cfg.cpu_offload, self.tr.device, lstm, total_agents))
+            
+    def reset_experiences(self):
+        if not self.activated:
+            return
+        
+        for teacher_experience in self.teacher_experiences:
+            teacher_experience.ptr = 0
+            teacher_experience.step = 0
+        
+    def forward(self, o_device, env_id):
+        if not self.activated:
+            return
+        
+        for i, teacher_experience in enumerate(self.teacher_experiences):
+            if teacher_experience.lstm_h is not None:
+                h = teacher_experience.lstm_h[:, env_id]
+                c = teacher_experience.lstm_c[:, env_id]
+            else:
+                h = None
+                c = None
+            
+            # need to support teacher e3b
+            actions, logprob, _, value, (h, c), next_e3b, intrinsic_reward = self.teachers[i](o_device, (h, c), e3b=None)
+
+            value = value.flatten()
+            actions = actions.cpu().numpy()
+            # mask = torch.as_tensor(mask)# * policy.mask)
+            o = o if self.tr.trainer_cfg.cpu_offload else o_device
+            teacher_experience.store(o, value, actions, logprob, _, _, env_id, _)
+            # does omitting all this prevent sorting from working??
+
+    # def sort_experiences(self) -> List[np.ndarray]:
+    #     if not self.activated:
+    #         return []
+    #     idxs = []
+    #     for teacher_experience in self.teacher_experiences:
+    #         idxs.append(teacher_experience.sort_training_data())
+    #     return idxs
+
+    def loss(self, mb, student_logprobs, student_values) -> Tuple[torch.Tensor, torch.Tensor]:
+        ks_value_loss = torch.tensor(0.0, device=self.tr.device)
+        ks_action_loss = torch.tensor(0.0, device=self.tr.device)
+
+        if not self.activated:
+            return ks_action_loss, ks_value_loss
+
+        for i, teacher_experience in enumerate(self.teacher_experiences):
+            teacher_logprobs = teacher_experience.logprobs_np[mb]
+            teacher_values = teacher_experience.values_np[mb]
+
+            # fix cross entropy loss formula
+            ks_action_loss -= (teacher_logprobs * student_logprobs).sum(dim=-1).mean() * self.teachers[i].action_coef
+            ks_value_loss += (teacher_values - student_values) ** 2 * self.teachers[i].value_coef
+            
+        return ks_action_loss, ks_value_loss
