fc4341b8b2bd50d461f7fbb2503f4a5941cb37ec
David Bloomin <daveey@gmail.com>
2025-02-23 21:51:10 -0800

    cp

--- rl/pufferlib/warnings_config.py
+++ rl/pufferlib/warnings_config.py
@@ -4,22 +4,23 @@ import sys
 
 def configure_warnings():
     """Configure warning filters for the current process."""
-    # Configure warning filters for pkg_resources deprecation warnings
-    warnings.filterwarnings('ignore', category=DeprecationWarning, module='pkg_resources')
-    warnings.filterwarnings('ignore', message='.*declare_namespace.*', category=DeprecationWarning)
-    warnings.filterwarnings('ignore', message='.*pkg_resources is deprecated.*', category=DeprecationWarning)
-    warnings.filterwarnings('ignore', message='.*Deprecated call to `pkg_resources.declare_namespace.*', category=DeprecationWarning)
+    # Disable all DeprecationWarnings by default
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-    # Configure warning filters for specific modules
-    warnings.filterwarnings('ignore', category=DeprecationWarning, module='pygame.pkgdata')
-    warnings.filterwarnings('ignore', category=DeprecationWarning, module='google')
+    # Disable specific warnings
+    warnings.filterwarnings("ignore", category=DeprecationWarning, module="pkg_resources")
+    warnings.filterwarnings("ignore", category=DeprecationWarning, module="pygame.pkgdata")
+    warnings.filterwarnings("ignore", message=".*declare_namespace.*")
+    warnings.filterwarnings("ignore", message=".*pkg_resources is deprecated.*")
+    warnings.filterwarnings("ignore", message=".*Deprecated call to `pkg_resources.declare_namespace.*")
 
-    # Set environment variables for warning suppression
-    os.environ["PYTHONWARNINGS"] = "ignore::DeprecationWarning:pkg_resources,ignore::DeprecationWarning:pygame.pkgdata,ignore::DeprecationWarning:google"
+    # Set environment variable to suppress warnings
+    os.environ["PYTHONWARNINGS"] = "ignore::DeprecationWarning"
 
     # Force warnings to be ignored immediately
     if not sys.warnoptions:
         warnings.simplefilter("ignore", DeprecationWarning)
+        warnings.simplefilter("ignore", ImportWarning)
 
 # Configure warnings in the main process
 configure_warnings()
