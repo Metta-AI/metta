c6a1aad79dfa78860c921a093b539f99617dd6ea
David Bloomin <daveey@gmail.com>
2025-02-25 16:37:10 -0800

    cp

--- rl/pufferlib/trainer.py
+++ rl/pufferlib/trainer.py
@@ -115,6 +115,9 @@ class PufferTrainer:
             self.policy.l2_reg_loss = orig_policy.l2_reg_loss
             self.policy.l2_init_loss = orig_policy.l2_init_loss
 
+        if self._master:
+            logger.info("Training policy:", self.policy)
+
         logger.info("Creating experience buffer")
         self._make_experience_buffer()
 
