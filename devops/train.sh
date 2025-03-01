88dd7cb03fb865d3da47fd5dadee51dd6cffa472
David Bloomin <daveey@gmail.com>
2025-02-28 17:50:09 -0800

    cp

--- devops/train.sh
+++ devops/train.sh
@@ -13,6 +13,21 @@ if [ -z "$NUM_GPUS" ]; then
     NUM_GPUS=1
 fi
 
+# same for NUM_NODES
+if [ -z "$NUM_NODES" ]; then
+    NUM_NODES=1
+fi
+
+# same for MASTER_ADDR
+if [ -z "$MASTER_ADDR" ]; then
+    MASTER_ADDR="localhost"
+fi
+
+# same for NODE_RANK
+if [ -z "$NODE_RANK" ]; then
+    NODE_RANK=0
+fi
+
 git pull
 # cd deps/pufferlib && git pull && python setup.py build_ext --inplace && cd ../..
 cd deps/fast_gae && git pull && python setup.py build_ext --inplace && cd ../..
@@ -20,9 +35,10 @@ cd deps/mettagrid && git pull && python setup.py build_ext --inplace && cd ../..
 cd deps/wandb_carbs && git pull && cd ../..
 
 torchrun \
-    --standalone \
-    --nnodes=1 \
+    --nnodes=$NUM_NODES \
     --nproc-per-node=$NUM_GPUS \
+    --master-addr=$MASTER_ADDR \
+    --node-rank=$NODE_RANK \
     tools/train.py \
     hardware=pufferbox \
     wandb.enabled=true \
