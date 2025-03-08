78f4793a8fa144c8ce5da9cdc2f396e24348b38f
David Bloomin <daveey@gmail.com>
2025-03-08 13:57:44 -0800

    cp

--- devops/sweep.sh
+++ devops/sweep.sh
@@ -1,3 +1,18 @@
 #/bin/bash -e
 
-./devops/run.sh sweep "$@"
+# keep running sweep. until it fails N consecutive times
+N=10
+consecutive_failures=0
+while true; do
+    ./devops/run.sh sweep "$@"
+    if [ $? -ne 0 ]; then
+        consecutive_failures=$((consecutive_failures + 1))
+        if [ $consecutive_failures -ge $N ]; then
+            echo "Sweep failed $N consecutive times, exiting"
+            exit 1
+        fi
+    else
+        consecutive_failures=0
+    fi
+done
+
