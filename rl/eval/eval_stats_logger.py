28ef06b6957e1d74a5e5ef64f8d71e0ed5378ed7
David Bloomin <daveey@gmail.com>
2025-03-08 01:28:52 -0800

    cp

--- rl/eval/eval_stats_logger.py
+++ rl/eval/eval_stats_logger.py
@@ -9,11 +9,11 @@ import gzip
 logger = logging.getLogger("eval_stats_logger.py")
 
 class EvalStatsLogger:
-    def __init__(self, cfg, wandb_run):
+    def __init__(self, cfg, wandb_run, save_path: str=None):
         self._cfg = cfg
         self._wandb_run = wandb_run
         # We want local stats dir to be the same for train, analysis and eval for a particular run
-        save_dir = cfg.run_dir.replace("analyze", "train").replace("eval", "train")
+        save_dir = (save_path or cfg.run_dir).replace("analyze", "train").replace("eval", "train")
 
         artifact_name = None
         if cfg.eval.eval_db_uri is None:
@@ -87,9 +87,8 @@ class EvalStatsLogger:
         else:
             self._add_additional_fields(eval_stats)
 
-        if self.artifact_name is None:
-            self._log_to_file(eval_stats)
-        else:
+        self._log_to_file(eval_stats)
+        if self.artifact_name is not None:
             self._log_to_wandb(self.artifact_name, eval_stats)
 
         return eval_stats
