724c0808dcda21197f5b630818ef2ee97a089a64
David Bloomin <daveey@gmail.com>
2025-03-08 17:21:21 -0800

    cp

--- devops/docker/push_image.sh
+++ devops/docker/push_image.sh
@@ -2,5 +2,6 @@
 # docker push mettaai/metta:latest
 # docker push mettaai/metta-base:latest
 
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 767406518141.dkr.ecr.us-east-1.amazonaws.com
 docker tag mettaai/metta:latest 767406518141.dkr.ecr.us-east-1.amazonaws.com/metta:latest
 docker push 767406518141.dkr.ecr.us-east-1.amazonaws.com/metta:latest
