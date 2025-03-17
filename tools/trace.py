c2d9cbb742c0123605c713cefcce854968d8deca
treeform <starplant@gmail.com>
2025-03-17 15:43:08 -0700

    Add json trace generator for MettaScope.

--- tools/trace.py
+++ tools/trace.py
@@ -7,9 +7,10 @@ import hydra
 from omegaconf import OmegaConf
 from agent.policy_store import PolicyStore
 from rl.wandb.wandb_context import WandbContext
-from rl.pufferlib.trace import save_trace_image
+from rl.pufferlib.trace import save_trace_image, save_replay
 from util.runtime_configuration import setup_metta_environment
 from util.config import config_from_path
+
 
 @hydra.main(version_base=None, config_path="../configs", config_name="simulator")
 def main(cfg):
@@ -21,12 +22,17 @@ def main(cfg):
     with WandbContext(cfg) as wandb_run:
         policy_store = PolicyStore(cfg, wandb_run)
         policy_record = policy_store.policy(cfg.policy_uri)
+
         image_path = f"{cfg.run_dir}/traces/trace.png"
         save_trace_image(cfg, policy_record, image_path)
-
         if platform.system() == "Darwin":
             # Open image in Preview.
             subprocess.run(["open", image_path])
+
+        replay_path = f"{cfg.run_dir}/replays/replay.json"
+        save_replay(cfg, policy_record, replay_path)
+        # Open image in Finder
+        subprocess.run(["open", image_path])
 
 if __name__ == "__main__":
     main()
