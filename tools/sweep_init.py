43d902cbc65b286702a21880951558d16fe1002b
David Bloomin <daveey@gmail.com>
2025-03-18 18:21:18 -0700

    cp

--- tools/sweep_init.py
+++ tools/sweep_init.py
@@ -1,25 +1,22 @@
 #!/usr/bin/env python3
+import json
+import logging
 import os
 import sys
-import argparse
-import logging
 import time
-from typing import List, Optional
-import hydra
-from util.runtime_configuration import setup_omega_conf
-import wandb
-from omegaconf import OmegaConf, DictConfig
-from rich.logging import RichHandler
-import yaml
-import random
-from rl.wandb.sweep import generate_run_id_for_sweep, sweep_id_from_name
-from rl.carbs.metta_carbs import carbs_params_from_cfg
-import wandb_carbs
-import json
-from rl.carbs.metta_carbs import MettaCarbs
-from util.efs_lock import efs_lock
 
+import hydra
+import wandb
+import yaml
+from omegaconf import DictConfig, OmegaConf
+from rich.logging import RichHandler
+from rl.carbs.metta_carbs import MettaCarbs, carbs_params_from_cfg
+from rl.wandb.sweep import generate_run_id_for_sweep, sweep_id_from_name
 from rl.wandb.wandb_context import WandbContext
+from util.efs_lock import efs_lock
+from util.runtime_configuration import setup_omega_conf
+
+import wandb_carbs
 
 # Configure rich colored logging to stderr instead of stdout
 logging.basicConfig(
