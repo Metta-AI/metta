4534564a4e901ceef583a86d13955b455ca91f2d
David Bloomin <daveey@gmail.com>
2025-03-18 18:39:40 -0700

    cp

--- tools/play.py
+++ tools/play.py
@@ -3,6 +3,7 @@ import signal  # Aggressively exit on ctrl+c
 
 import hydra
 from agent.policy_store import PolicyStore
+from env.util import config_from_path
 from util.runtime_configuration import setup_metta_environment
 from omegaconf import OmegaConf
 from rl.pufferlib.play import play
@@ -13,6 +14,7 @@ signal.signal(signal.SIGINT, lambda sig, frame: os._exit(0))
 @hydra.main(version_base=None, config_path="../configs", config_name="eval")
 def main(cfg):
     setup_metta_environment(cfg)
+    cfg.env = config_from_path(cfg.eval.env, cfg.eval.env_overrides)
 
     with WandbContext(cfg) as wandb_run:
         policy_store = PolicyStore(cfg, wandb_run)
