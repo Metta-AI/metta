fd596c6aa621f990264257e5bb14396381925b00
daphnedemekas <daphnedemekas@gmail.com>
2025-03-25 14:45:11 -0700

    remove wandb run

--- rl/pufferlib/trainer.py
+++ rl/pufferlib/trainer.py
@@ -59,7 +59,7 @@ class PufferTrainer:
         self.wandb_run = wandb_run
         self.policy_store = policy_store
         self.use_e3b = self.trainer_cfg.use_e3b
-        self.eval_stats_logger = EvalStatsLogger(cfg, self._env_cfg, wandb_run)
+        self.eval_stats_logger = EvalStatsLogger(cfg, self._env_cfg)
         self.average_reward = 0.0  # Initialize average reward estimate
         self._policy_fitness = []
         self._effective_rank = []
