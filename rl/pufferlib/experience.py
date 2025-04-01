32772334dedbc71984ab2f83e6e5c8dbb6a4b5c6
David Bloomin <daveey@gmail.com>
2025-04-01 15:43:20 -0700

    cp

--- rl/pufferlib/experience.py
+++ rl/pufferlib/experience.py
@@ -24,7 +24,9 @@ import pufferlib
 import pufferlib.utils
 import pufferlib.pytorch
 
-class Experience:
+class ExperienceBuffer:
+    pass
+class Experience(ExperienceBuffer):
     '''Flat tensor storage and array views for faster indexing'''
     def __init__(self, batch_size, bptt_horizon, minibatch_size, max_minibatch_size, hidden_size,
                  obs_shape, obs_dtype, atn_shape, atn_dtype, cpu_offload=False,
@@ -39,6 +41,7 @@ class Experience:
         atn_dtype = pufferlib.pytorch.numpy_to_torch_dtype_dict[atn_dtype]
         pin = device == 'cuda' and cpu_offload
         obs_device = device if not pin else 'cpu'
+
         self.obs=torch.zeros(batch_size, *obs_shape, dtype=obs_dtype,
             pin_memory=pin, device=device if not pin else 'cpu')
         self.actions=torch.zeros(batch_size, *atn_shape, dtype=atn_dtype, device=device)
@@ -47,43 +50,26 @@ class Experience:
         self.dones=torch.zeros(batch_size, device=device)
         self.truncateds=torch.zeros(batch_size, device=device)
 
-        self.use_e3b = use_e3b
+        self.e3b = None
         if use_e3b:
-            self.e3b_inv = torch.eye(hidden_size).repeat(lstm_total_agents, 1, 1).to(device) / e3b_lambda
-            self.e3b_orig = self.e3b_inv.clone()
-            self.e3b_mean = None
-            self.e3b_std = None
+            self.e3b = E3BExperience(hidden_size, lstm_total_agents, device, e3b_lambda)
 
-        self.use_diayn = use_diayn
+        self.diayn = None
         if use_diayn:
-            #self.diayn_archive = torch.randn(diayn_archive, hidden_size, dtype=torch.float32, device=device)
-            self.diayn_archive = torch.nn.functional.one_hot(torch.arange(diayn_archive), diayn_archive).to(device).float()
-            self.diayn_skills = torch.randint(0, diayn_archive, (lstm_total_agents,), dtype=torch.long, device=device)
-            self.diayn_batch = torch.zeros(batch_size, dtype=torch.long, device=device)
+            self.diayn = DIYANExperience(diayn_archive, lstm_total_agents, batch_size, device)
 
-        self.use_p3o = use_p3o
-        self.p3o_horizon = p3o_horizon
+        self.p3o = None
         if use_p3o:
-            self.values_mean=torch.zeros(batch_size, p3o_horizon, device=device)
-            self.values_std=torch.zeros(batch_size, p3o_horizon, device=device)
-            self.reward_block = torch.zeros(batch_size, p3o_horizon, dtype=torch.float32, device=device)
-            self.mask_block = torch.ones(batch_size, p3o_horizon, dtype=torch.float32, device=device)
-            self.buf = torch.zeros(batch_size, p3o_horizon, dtype=torch.float32, device=device)
-            self.advantages = torch.zeros(batch_size, dtype=torch.float32, device=device)
-            self.bounds = torch.zeros(batch_size, dtype=torch.int32, device=device)
-            self.vstd_max = 1.0
+            self.p3o = P3OExperience(batch_size, p3o_horizon, device)
         else:
-            self.values = torch.zeros(batch_size, device=device)
+            self.ppo = PPOExperience(batch_size, device)
+
+        self.lstm = None
+        if lstm is not None:
+            self.lstm = LSTMExperience(lstm.num_layers, lstm_total_agents, lstm.hidden_size, device)
 
         self.sort_keys = np.zeros((batch_size, 3), dtype=np.int32)
         self.sort_keys[:, 0] = np.arange(batch_size)
-
-        self.lstm_h = self.lstm_c = None
-        if lstm is not None:
-            assert lstm_total_agents > 0
-            shape = (lstm.num_layers, lstm_total_agents, lstm.hidden_size)
-            self.lstm_h = torch.zeros(shape).to(device)
-            self.lstm_c = torch.zeros(shape).to(device)
 
         minibatch_size = min(minibatch_size, max_minibatch_size)
         num_minibatches = batch_size / minibatch_size
@@ -103,10 +89,14 @@ class Experience:
         self.device = device
         self.ptr = 0
         self.step = 0
+        self.experience_buffers = []
 
     @property
     def full(self):
         return self.ptr >= self.batch_size
+
+    def add_experience_buffer(self, experience_buffer):
+        self.experience_buffers.append(experience_buffer)
 
     def store(self, state, cpu_obs, gpu_obs, value, action, logprob, reward, done, env_id, mask):
         # Mask learner and Ensure indices do not exceed batch size
@@ -128,14 +118,13 @@ class Experience:
         else:
             self.obs[dst] = cpu_obs[cpu_inds]
 
-        if self.use_diayn:
-            self.diayn_batch[dst] = state.diayn_z_idxs[gpu_inds]
+        for experience_buffer in self.experience_buffers:
+            experience_buffer.store(dst, state=state[gpu_inds])
 
-        if self.use_p3o:
-            self.values_mean[dst] = value.mean[gpu_inds]
-            self.values_std[dst] = value.std[gpu_inds]
+        if self.p3o is not None:
+            self.p3o.store(value.mean[gpu_inds], value.std[gpu_inds], dst)
         else:
-            self.values[dst] = value[gpu_inds].flatten()
+            self.ppo.store(value[gpu_inds].flatten(), dst)
 
         self.actions[dst] = action[gpu_inds]
         self.logprobs[dst] = logprob[gpu_inds]
@@ -176,27 +165,85 @@ class Experience:
         self.b_dones = self.dones.to(self.device, non_blocking=True)[b_idxs]
         self.b_obs = self.obs[self.b_idxs_obs]
 
-        if self.use_p3o:
-            self.reward_block = torch.as_tensor(reward_block).to(self.device)
-            self.b_reward_block = self.reward_block.reshape(
-                self.minibatch_rows, self.num_minibatches, self.bptt_horizon, self.p3o_horizon
-                ).transpose(0, 1).reshape(self.num_minibatches, self.minibatch_size, self.p3o_horizon)
-
-            b_mask_block = torch.as_tensor(mask_block).to(self.device)
-            self.b_mask_block = b_mask_block.reshape(
-                self.minibatch_rows, self.num_minibatches, self.bptt_horizon, self.p3o_horizon
-                ).transpose(0, 1).reshape(self.num_minibatches, self.minibatch_size, self.p3o_horizon)
-
-            self.b_values_mean = self.values_mean.to(self.device, non_blocking=True)[b_flat]
-            self.b_values_std = self.values_std.to(self.device, non_blocking=True)[b_flat]
-            self.b_returns = self.buf.to(self.device, non_blocking=True).reshape(
-                self.minibatch_rows, self.num_minibatches, self.bptt_horizon, self.p3o_horizon
-                ).transpose(0, 1).reshape(self.num_minibatches, self.minibatch_size, self.p3o_horizon)
+        if self.p3o is not None:
+            self.p3o.flatten(b_flat, advantages, reward_block, mask_block)
         else:
-            self.b_values = self.values.to(self.device, non_blocking=True)[b_flat]
-            self.returns = advantages + self.values # Check sorting of values here
-            self.b_returns = self.b_advantages + self.b_values # Check sorting of values here
+            self.ppo.flatten(b_flat, advantages)
 
-        if self.use_diayn:
-            self.b_diayn_z_idxs = self.diayn_batch.to(self.device, non_blocking=True)[b_flat]
-            self.b_diayn_z = self.diayn_archive[self.b_diayn_z_idxs]
+        if self.diayn is not None:
+            self.diayn.flatten(b_flat)
+
+class E3BExperience():
+    def __init__(self, hidden_size, lstm_total_agents, device, e3b_lambda):
+        self.inv = torch.eye(hidden_size).repeat(lstm_total_agents, 1, 1).to(device) / e3b_lambda
+        self.orig = self.inv.clone()
+        self.mean = None
+        self.std = None
+
+class DIYANExperience():
+    def __init__(self, diayn_archive, lstm_total_agents, batch_size, device):
+        self.archive = torch.nn.functional.one_hot(torch.arange(diayn_archive), diayn_archive).to(device).float()
+        self.skills = torch.randint(0, diayn_archive, (lstm_total_agents,), dtype=torch.long, device=device)
+        self.batch = torch.zeros(batch_size, dtype=torch.long, device=device)
+
+    def store(self, dst, state, **kwargs):
+        self.batch[dst] = state.diayn_z_idxs
+
+    def flatten(self, b_flat):
+        self.b_diayn_z_idxs = self.batch.to(self.device, non_blocking=True)[b_flat]
+        self.b_diayn_z = self.archive[self.b_diayn_z_idxs]
+
+class PPOExperience():
+    def __init__(self, batch_size, device):
+        self.values = torch.zeros(batch_size, device=device)
+        self.advantages = torch.zeros(batch_size, device=device)
+        self.returns = torch.zeros(batch_size, device=device)
+        self.logprobs = torch.zeros(batch_size, device=device)
+        self.entropy = torch.zeros(batch_size, device=device)
+
+    def store(self, dst, value, **kwargs):
+        self.values[dst] = value
+
+    def flatten(self, b_flat, advantages):
+        self.b_values = self.values.to(self.device, non_blocking=True)[b_flat]
+        self.returns = advantages + self.values # Check sorting of values here
+        self.b_returns = self.b_advantages + self.b_values # Check sorting of values here
+
+class P3OExperience():
+    def __init__(self, batch_size, p3o_horizon, device):
+        self.values_mean = torch.zeros(batch_size, p3o_horizon, device=device)
+        self.values_std = torch.zeros(batch_size, p3o_horizon, device=device)
+        self.reward_block = torch.zeros(batch_size, p3o_horizon, dtype=torch.float32, device=device)
+        self.mask_block = torch.ones(batch_size, p3o_horizon, dtype=torch.float32, device=device)
+        self.buf = torch.zeros(batch_size, p3o_horizon, dtype=torch.float32, device=device)
+        self.advantages = torch.zeros(batch_size, dtype=torch.float32, device=device)
+        self.bounds = torch.zeros(batch_size, dtype=torch.int32, device=device)
+        self.vstd_max = 1.0
+
+    def store(self, dst, value, **kwargs):
+        self.values_mean[dst] = value.mean
+        self.values_std[dst] = value.std
+
+    def flatten(self, b_flat, advantages, reward_block, mask_block):
+        self.reward_block = torch.as_tensor(reward_block).to(self.device)
+        self.b_reward_block = self.reward_block.reshape(
+            self.minibatch_rows, self.num_minibatches, self.bptt_horizon, self.p3o_horizon
+            ).transpose(0, 1).reshape(self.num_minibatches, self.minibatch_size, self.p3o_horizon)
+
+        b_mask_block = torch.as_tensor(mask_block).to(self.device)
+        self.b_mask_block = b_mask_block.reshape(
+            self.minibatch_rows, self.num_minibatches, self.bptt_horizon, self.p3o_horizon
+            ).transpose(0, 1).reshape(self.num_minibatches, self.minibatch_size, self.p3o_horizon)
+
+        self.b_values_mean = self.values_mean.to(self.device, non_blocking=True)[b_flat]
+        self.b_values_std = self.values_std.to(self.device, non_blocking=True)[b_flat]
+        self.b_returns = self.buf.to(self.device, non_blocking=True).reshape(
+            self.minibatch_rows, self.num_minibatches, self.bptt_horizon, self.p3o_horizon
+            ).transpose(0, 1).reshape(self.num_minibatches, self.minibatch_size, self.p3o_horizon)
+
+class LSTMExperience():
+    def __init__(self, lstm_num_layers, lstm_total_agents, lstm_hidden_size, device):
+        assert lstm_total_agents > 0
+        shape = (lstm_num_layers, lstm_total_agents, lstm_hidden_size)
+        self.h = torch.zeros(shape).to(device)
+        self.c = torch.zeros(shape).to(device)
