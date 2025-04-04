5326b5989493e1f44c016b5970028c0e75bf2324
daphnedemekas <daphnedemekas@gmail.com>
2025-04-04 11:31:52 -0700

    fix configs

--- run_navigation_evals.sh
+++ run_navigation_evals.sh
@@ -2,13 +2,20 @@
 
 # Define the list of policy URIs
 POLICIES=(
-    "b.daphne.navigation_varied_obstacle_shapes_pretrained.r.1"
-    "b.daphne.navigation_varied_obstacle_shapes.r.0"
-    "navigation_poisson_sparser.r.2"
-    "navigation_infinite_cooldown_sparser_pretrained.r.0"
-    "navigation_infinite_cooldown_sparser.r.0"
-    "navigation_poisson_sparser_pretrained.r.6"
+    # "b.daphne.navigation_varied_obstacle_shapes_pretrained.r.1"
+    # "b.daphne.navigation_varied_obstacle_shapes.r.0"
+    # "navigation_poisson_sparser.r.2"
+    # "navigation_infinite_cooldown_sparser_pretrained.r.0"
+    # "navigation_infinite_cooldown_sparser.r.0"
+    # "navigation_poisson_sparser_pretrained.r.6"
+    "b.daveey.t.8.rdr9.3"
+    "b.daveey.t.4.rdr9.3"
+    "b.daveey.t.8.rdr9.mb2.1"
+    "daveey.t.1.pi.dpm"
     "b.daveey.t.64.dr90.1"
+    "b.daveey.t.8.rdr9.sb"
+    "navigation_infinite_cooldown_sweep"
+    "navigation_infinite_cooldown_sweep.r.0"
 )
 
 # Loop through the policies and run evaluations
