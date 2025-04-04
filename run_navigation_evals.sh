7044de277d9ad464f5a39d5f71ff367b213ae39c
daphnedemekas <daphnedemekas@gmail.com>
2025-04-04 15:13:04 -0700

    action space

--- run_navigation_evals.sh
+++ run_navigation_evals.sh
@@ -18,26 +18,26 @@ POLICIES_FULL_SEQUENCE=(
     "navigation_infinite_cooldown_sparser_pretrained.r.0"
     "navigation_infinite_cooldown_sparser.r.0"
     "navigation_poisson_sparser_pretrained.r.6"
-    "b.daveey.t.8.rdr9.3"
-    "b.daveey.t.4.rdr9.3"
-    "b.daveey.t.8.rdr9.mb2.1"
-    "daveey.t.1.pi.dpm"
-    "b.daveey.t.64.dr90.1"
-    "b.daveey.t.8.rdr9.sb"
-)
+#     "b.daveey.t.8.rdr9.3"
+#     "b.daveey.t.4.rdr9.3"
+#     "b.daveey.t.8.rdr9.mb2.1"
+#     "daveey.t.1.pi.dpm"
+#     "b.daveey.t.64.dr90.1"
+#     "b.daveey.t.8.rdr9.sb"
+# )
 
-# Loop through the policies and run evaluations
-for i in "${!POLICIES_HEARTS[@]}"; do
-    POLICY_URI=${POLICIES_HEARTS[$i]}
-    IDX=$((i + 1))
+# # Loop through the policies and run evaluations
+# for i in "${!POLICIES_HEARTS[@]}"; do
+#     POLICY_URI=${POLICIES_HEARTS[$i]}
+#     IDX=$((i + 1))
 
-    echo "Running only hearts eval for policy $POLICY_URI"
-    python3 -m tools.eval \
-        eval=navigation_evals_onlyheart \
-        run=navigation_evaluation_onlyhearts$IDX \
-        eval.policy_uri=wandb://run/$POLICY_URI \
-        eval_db_uri=wandb://artifacts/navigation_evaldb_only_hearts2
-done
+#     echo "Running only hearts eval for policy $POLICY_URI"
+#     python3 -m tools.eval \
+#         eval=navigation_evals_onlyheart \
+#         run=navigation_evaluation_onlyhearts$IDX \
+#         eval.policy_uri=wandb://run/$POLICY_URI \
+#         eval_db_uri=wandb://artifacts/navigation_evaldb_only_hearts2
+# done
 
 for i in "${!POLICIES_FULL_SEQUENCE[@]}"; do
     POLICY_URI=${POLICIES_FULL_SEQUENCE[$i]}
