cb737faa7690e0c85c6e1eaf4123d46e8096a8b4
daphnedemekas <daphnedemekas@gmail.com>
2025-04-04 15:19:30 -0700

    revert to full sequence

--- run_navigation_evals.sh
+++ run_navigation_evals.sh
@@ -18,14 +18,13 @@ POLICIES_FULL_SEQUENCE=(
     "navigation_infinite_cooldown_sparser_pretrained.r.0"
     "navigation_infinite_cooldown_sparser.r.0"
     "navigation_poisson_sparser_pretrained.r.6"
+    "b.daveey.t.8.rdr9.3"
+    "b.daveey.t.4.rdr9.3"
+    "b.daveey.t.8.rdr9.mb2.1"
+    "daveey.t.1.pi.dpm"
+    "b.daveey.t.64.dr90.1"
+    "b.daveey.t.8.rdr9.sb"
 )
-#     "b.daveey.t.8.rdr9.3"
-#     "b.daveey.t.4.rdr9.3"
-#     "b.daveey.t.8.rdr9.mb2.1"
-#     "daveey.t.1.pi.dpm"
-#     "b.daveey.t.64.dr90.1"
-#     "b.daveey.t.8.rdr9.sb"
-# )
 
 # # Loop through the policies and run evaluations
 # for i in "${!POLICIES_HEARTS[@]}"; do
@@ -47,7 +46,7 @@ for i in "${!POLICIES_FULL_SEQUENCE[@]}"; do
     echo "Running full sequence eval for policy $POLICY_URI"
     python3 -m tools.eval \
         eval=navigation_evals_fullsequence \
-        run=navigation_evaluation_fullsequence$IDX \
+        run=navigation_evaluation_learning_fullsequence$IDX \
         eval.policy_uri=wandb://run/$POLICY_URI \
-        eval_db_uri=wandb://artifacts/navigation_evaldb_sequence2
+        eval_db_uri=wandb://artifacts/navigation_evaldb_learning_fullsequence
 done
