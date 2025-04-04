553df894191a6b2951be9ff185dbafce905b4340
daphnedemekas <daphnedemekas@gmail.com>
2025-04-04 15:01:20 -0700

    remove training suite changes

--- util/config.py
+++ util/config.py
@@ -1,8 +1,5 @@
 import hydra
 from omegaconf import DictConfig, OmegaConf
-from omegaconf.errors import ConfigKeyError
-import logging
-logger = logging.getLogger(__name__)
 
 def config_from_path(config_path: str, overrides: DictConfig = None) -> DictConfig:
     env_cfg = hydra.compose(config_name=config_path)
@@ -12,8 +9,5 @@ def config_from_path(config_path: str, overrides: DictConfig = None) -> DictConf
     for p in path[:-1]:
         env_cfg = env_cfg[p]
     if overrides is not None:
-        try:
-            env_cfg = OmegaConf.merge(env_cfg, overrides)
-        except ConfigKeyError:
-            logger.warning(f"Cannot parse overrides when using multienv_mettagrid")
+        env_cfg = OmegaConf.merge(env_cfg, overrides)
     return env_cfg
