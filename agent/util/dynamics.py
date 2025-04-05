cdd6252525b0a2eb18b26a7311a664832c42cc57
al-does <avardakostas@gmail.com>
2025-04-04 18:31:35 -0700

    fixed vars not being sent to cpu or device

--- agent/util/dynamics.py
+++ agent/util/dynamics.py
@@ -9,10 +9,6 @@ def analyze_sv(S: torch.Tensor):
     ''' Lars, go to town! S is the tensor of singular values. I wrote an example of how to use this function. 
     Right now, it supports outputting two metrics but we can obv add more.
     '''
-    # eigens = S ** 2
-    # sorted_eigens = torch.sort(eigens, descending=True).values
-    # largest_eigen = sorted_eigens[0].item()
-    # smallest_eigen = sorted_eigens[-1].item()
     
     sorted_sv = torch.sort(S, descending=True).values
     largest_sv = sorted_sv[0].item()
@@ -24,10 +20,10 @@ def analyze_sv(S: torch.Tensor):
     assert len(sorted_sv) > 5, "Need at least 6 singular values for meaningful fit"
 
     # Linear fit in log-log space to check for power law (indicator of criticality)
-    log_indices = np.log(np.arange(1, len(sorted_sv) + 1))
-    log_sv = torch.log(sorted_sv + 1e-10)
+    log_indices_np = torch.log(torch.arange(1, len(sorted_sv) + 1, device=S.device).float()).cpu().numpy()
+    log_sv_np = torch.log(sorted_sv + 1e-10).cpu().numpy()
 
-    slope, intercept, r_value, p_value, std_err = stats.linregress(log_indices, log_sv)
+    slope, intercept, r_value, p_value, std_err = stats.linregress(log_indices_np, log_sv_np)
 
     # TODO:  make the regime determination more sophisticated
     # Determine regime
