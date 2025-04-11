34d6bdea0d05c37a4bb6a1540ccab862dd12d5a6
Andre von Houck <starplant@gmail.com>
2025-04-11 14:21:03 -0700

    Check and help people log into AWS or W&B. (#127)

--- tools/play.py
+++ tools/play.py
@@ -4,7 +4,7 @@ import signal  # Aggressively exit on ctrl+c
 import hydra
 from agent.policy_store import PolicyStore
 from util.config import config_from_path
-from util.runtime_configuration import setup_metta_environment
+from util.runtime_configuration import setup_mettagrid_environment
 from rl.pufferlib.play import play
 from rl.wandb.wandb_context import WandbContext
 
@@ -12,7 +12,7 @@ signal.signal(signal.SIGINT, lambda sig, frame: os._exit(0))
 
 @hydra.main(version_base=None, config_path="../configs", config_name="eval")
 def main(cfg):
-    setup_metta_environment(cfg)
+    setup_mettagrid_environment(cfg)
     cfg.eval.env = config_from_path(cfg.eval.env, cfg.eval.env_overrides)
 
     with WandbContext(cfg) as wandb_run:
