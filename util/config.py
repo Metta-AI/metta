e9531649424c423aba8bdcff26665518faa06072
Daphne <45669133+daphnedemekas@users.noreply.github.com>
2025-04-11 14:28:21 -0700

    Training suite (#113)

--- util/config.py
+++ util/config.py
@@ -10,7 +10,9 @@ def config_from_path(config_path: str, overrides: DictConfig = None) -> DictConf
     path = config_path.split("/")
     for p in path[:-1]:
         env_cfg = env_cfg[p]
-    if overrides is not None:
+    if overrides not in [None, {}]:
+        if env_cfg._target_ == "mettagrid.mettagrid_env.MettaGridEnvSet":
+            raise NotImplementedError("Cannot parse overrides when using multienv_mettagrid")
         env_cfg = OmegaConf.merge(env_cfg, overrides)
     return env_cfg
 
