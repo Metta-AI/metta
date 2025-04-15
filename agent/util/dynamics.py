ebbbad292cc9d75c366cced848e9ba48c7108c8b
Lars Sandved <lars.sandvedsmith@gmail.com>
2025-04-15 16:24:06 +0200

    Excluded SVs close to zero for analysis

--- agent/util/dynamics.py
+++ agent/util/dynamics.py
@@ -5,19 +5,17 @@ from scipy import stats
 
 def analyze_sv(S: torch.Tensor): 
     sorted_sv = torch.sort(S, descending=True).values
+    sorted_sv = sorted_sv[sorted_sv > 1e-10] # ignore singular values close to zero as they do not contribute to the dynamics
     mean_sv = torch.mean(S).item()
     largest_sv = sorted_sv[0].item()
     smallest_sv = sorted_sv[-1].item()
-    non_zero_sv = sorted_sv[sorted_sv > 0]
-    smallest_non_zero_sv = non_zero_sv[-1].item()
-    sv_ratio = largest_sv / smallest_non_zero_sv # also known as the condition number   
+
+    sv_ratio = largest_sv / smallest_sv # also known as the condition number   
 
     if len(sorted_sv) <= 5:
         print(f"Not enough singular values to fit a power law. Only {len(sorted_sv)} singular values.")
         return {}
-    
-    #TODO: compare R2 values when using all the singular values vs. only the non-zero ones
-    
+        
     # Linear fit in log-log space to check for power law (indicator of criticality)
     log_indices_np = torch.log(torch.arange(1, len(sorted_sv) + 1, device=S.device).float()).cpu().numpy()
     log_sv_np = torch.log(sorted_sv + 1e-10).cpu().numpy()
