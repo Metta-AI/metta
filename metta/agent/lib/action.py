a882def0d5d4450e4903403bb494dbc7eff31a00
Jack Heart <jack@stem.ai>
2025-04-15 13:46:38 -0700

    Pull out simulation into its own module (#143)
    This pulls out all of the code that is used to do inference (i.e.
    simulation) into its own library. This makes it easier to avoid cyclic
    imports, because rl should depend on simulation, but simulation doesnt
    need RL. We also pulled out wandb into util because while mostly we
    should only use wandb during RL, that's not the case right now, so this
    was necessary to preserve the fact that sim doesnt depend on RL.
    
    This is all a prelude to introducing a typed SimConfig.
    
    Also, add some backwards-compatability to allow us to load old policies
    with the new refactor into a metta-scoped agent module.
    
    ---------
    
    Co-authored-by: Jack Heart <jack@softmax.com>

--- metta/agent/lib/action.py
+++ metta/agent/lib/action.py
@@ -1,13 +1,13 @@
-import metta.agent.lib.nn_layer_library as nn_layer_library
+from metta.agent.lib.nn_layer_library import Linear
 
 
-class ActionType(nn_layer_library.Linear):
+class ActionType(Linear):
     def __init__(self, action_type_size, **cfg):
         super().__init__(**cfg)
         self._output_size = action_type_size
 
 
-class ActionParam(nn_layer_library.Linear):
+class ActionParam(Linear):
     def __init__(self, action_param_size, **cfg):
         super().__init__(**cfg)
         self._output_size = action_param_size
