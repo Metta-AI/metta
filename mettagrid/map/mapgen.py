aaf42cc1cb89ae5042fc70dbdd240b3d39c63e47
Slava Matyukhin <me@berekuk.ru>
2025-04-16 15:43:22 -0300

    New map generator (#73)
    # Summary
    
    `mettagrid.map.mapgen.MapGen` class provides a new way to construct
    maps, based on scenes that can include other scenes recursively, and you
    select children areas to do that in bulk, all through YAML configs.
    
    Example: "render a grid of 2x3 rooms; then add two mazes in two random
    rooms; add 1 agent in each room; add 1 mine and 2 generators in each
    room; add this handcoded ASCII map in a room that's not a maze".
    
    `mapgen_demo.yaml` config, that can be viewed with `python -m
    tests.show_map game/map_builder=mapgen_demo game.num_agents=6`, should
    serve as a detailed summary of current capabilities.
    
    <img width="740" alt="Screenshot 2025-04-07 at 20 23 29"
    src="https://github.com/user-attachments/assets/d8341c21-5f19-4d98-a3e5-0d3ed17b3488"
    />
    
    
    This PR includes the commits from #68, because I needed the
    `tests.show_map` script for development.
    
    # Notes
    
    ## `configs/game/map_builder/` refactoring
    
    I wanted to make configs reusable, so I've moved parts of old
    `configs/*.yaml` files to separate files.
    
    This caused two problems:
    1) If this PR is merged, the main Metta repo will break - it symlinks to
    only some configs, which I didn't realize initially.
    2) In the current design, `game.map_builder` is tightly coupled with
    `game.num_agents`.
    
    I'd argue that maybe `game.num_agents` shouldn't exist at all — the env
    constructor could export `map_agents` by counting the agents that are
    present on the map. But this is not a blocker for this PR.
    
    For symlinks, I can revert the refactorings for older configs. Another
    thing that could be done is to `ln -s deps/mettagrid/configs
    configs/env/mettagrid`, and move extra mettagrid configs in Metta repo
    under `configs/env/mettagrid-extra`.
    
    ## Demos for old -> new system
    
    `configs/game/map_builder/` includes separate `maze.yaml` and
    `mapgen_maze.yaml` and similar pairs, to check that the new system
    supports all previous maps.
    
    (I haven't converted cognitive_evals maps yet.)
    
    The maps are not 100% exactly identical, but they're very close.
    
    ## Future plans
    
    On my current roadmap:
    - BSP tree maze generation (rougelike), since it's the most
    straightforward way to demo the new capabilities in a less boring
    environment than separate isolated rooms
    - I'm reimplementing this from scratch, btw, because the full scene
    implementation needs to export *areas* (rooms and corridors), so I
    couldn't use [urizen](https://github.com/vurmux/urizen)
    - WaveFunctionCollapse integration
    - Non-rectangular areas (probably with something like... rectangular
    area + a bitmask?)
    - Rationale/example: imagine that BSP scene exports "room" areas and
    "corridor" areas; then we want to populate corridors with items. If the
    corridor has turns then the rectangular area that covers the corridor
    could cover a corner of some unrelated room.
    - Other scenes, such as:
    - "fill this area with walls of this shape, but make checks that it's
    still fully connected"
    - Layout scenes that allow you to target sub-areas like "left side of
    the map", or "top-left corner with 20% width/height"
    - Built-in config flag for rotating scenes

---
+++ mettagrid/map/mapgen.py
@@ -0,0 +1,46 @@
+# Root map generator, based on nodes.
+import hydra
+import numpy as np
+from omegaconf import DictConfig
+from .scene import Scene
+
+
+class MapGen:
+    def __init__(
+        self, width: int, height: int, root: DictConfig, border_width: int = 1
+    ):
+        self._width = width
+        self._height = height
+        self._border_width = border_width
+        self._root_config = root
+
+        if isinstance(self._root_config, DictConfig):
+            # recursive_map_builder is set to false, so we need to instantiate the root config
+            self._root = hydra.utils.instantiate(self._root_config, _recursive_=False)
+        elif isinstance(self._root_config, Scene):
+            self._root = self._root_config
+        else:
+            raise ValueError("Root config must be a DictConfig or a Node")
+
+        self._grid = np.full(
+            (height + 2 * border_width, width + 2 * border_width), "empty", dtype="<U50"
+        )
+        self._grid[:border_width, :] = "wall"
+        self._grid[-border_width:, :] = "wall"
+        self._grid[:, :border_width] = "wall"
+        self._grid[:, -border_width:] = "wall"
+
+    def _inner_grid(self):
+        if self._border_width > 0:
+            return self._grid[
+                self._border_width : -self._border_width,
+                self._border_width : -self._border_width,
+            ]
+        else:
+            return self._grid
+
+    def build(self):
+        root_node = self._root.make_node(self._inner_grid())
+
+        root_node.render()
+        return self._grid
