ea957359b4a7cdd66604c091083a35ab76dbd349
daphnedemekas <daphnedemekas@gmail.com>
2025-04-17 17:04:39 -0700

    update overview

--- metta/rl/pufferlib/trainer.py
+++ metta/rl/pufferlib/trainer.py
@@ -568,7 +568,9 @@ class PufferTrainer:
 
         navigation_score = np.mean([r["candidate_mean"] for r in self._eval_results if "navigation" in r["eval"]])
         object_use_score = np.mean([r["candidate_mean"] for r in self._eval_results if "object_use" in r["eval"]])
-        against_npc_score = np.mean([r["candidate_mean"] for r in self._eval_results if "npc" in r["eval"]])
+        against_npc_score = np.mean(
+            [r["candidate_mean"] for r in self._eval_results if r["npc_policy_uri"] is not None]
+        )
 
         if not np.isnan(navigation_score):
             overview["navigation_evals"] = navigation_score
