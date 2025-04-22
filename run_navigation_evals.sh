6db4456f6ece61dfd6ad255ae720146095cdfc32
daphnedemekas <daphnedemekas@gmail.com>
2025-04-22 10:14:37 -0700

    eval next one

--- run_navigation_evals.sh
+++ run_navigation_evals.sh
@@ -2,6 +2,7 @@
 
 # Define the list of policy URIs
 POLICIES=(
+  "b.daphne.terrain_multienv_lessactions"
   "b.daphne.terrain_multienv_altar_resets_lessactions"
   "b.daphne.terrain_multienv_altar_resets_withgenerators"
   "b.daphne.varied_terrain_lessactions"
