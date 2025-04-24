c25885a98c39581032aee17dc8ce39cb563f0d6b
Robb Walters <rjwalters@users.noreply.github.com>
2025-04-24 10:49:02 -0700

    feat: add clang formatting for C++ files (#196)
    # Add clang formatting for C++ files
    
    ## Description
    This PR introduces C++ code formatting standards using clang-format.
    While we already use ruff for Python formatting, this extends our code
    style enforcement to C++ files in the mettagrid directory.
    
    ## Changes
    - Added a `.clang-format` file with Google-based style configurations
    and customizations
    - Created a Makefile in /deps/mettagrid to:
      - Install clang-format tools (with platform detection)
      - Format C++ files while preserving Cython files
    - Formatted all existing C++ files according to the new style guide
    - Added relevant VSCode extensions to improve C++ and Cython development
    experience
    
    ## Notes
    - The formatter is currently only applied manually via `make format`
    command
    - Not enforcing C++ formatting in CI yet, but can be added in the future
    - Settings prioritize readability with 4-space indentation and 120
    character line limit

--- deps/mettagrid/mettagrid/objects/wall.hpp
+++ deps/mettagrid/mettagrid/objects/wall.hpp
@@ -1,39 +1,40 @@
 #ifndef WALL_HPP
 #define WALL_HPP
 
-#include <vector>
 #include <string>
+#include <vector>
+
 #include "../grid_object.hpp"
 #include "constants.hpp"
 #include "metta_object.hpp"
 
 class Wall : public MettaObject {
 public:
-    bool _swappable;
+  bool _swappable;
 
-    Wall(GridCoord r, GridCoord c, ObjectConfig cfg) {
-        GridObject::init(ObjectType::WallT, GridLocation(r, c, GridLayer::Object_Layer));
-        MettaObject::init_mo(cfg);
-        this->_swappable = cfg["swappable"];
-    }
+  Wall(GridCoord r, GridCoord c, ObjectConfig cfg) {
+    GridObject::init(ObjectType::WallT, GridLocation(r, c, GridLayer::Object_Layer));
+    MettaObject::init_mo(cfg);
+    this->_swappable = cfg["swappable"];
+  }
 
-    virtual void obs(ObsType *obs, const std::vector<unsigned int> &offsets) const override {
-        obs[offsets[0]] = 1;
-        obs[offsets[1]] = this->hp;
-        obs[offsets[2]] = this->_swappable;
-    }
+  virtual void obs(ObsType* obs, const std::vector<unsigned int>& offsets) const override {
+    obs[offsets[0]] = 1;
+    obs[offsets[1]] = this->hp;
+    obs[offsets[2]] = this->_swappable;
+  }
 
-    static std::vector<std::string> feature_names() {
-        std::vector<std::string> names;
-        names.push_back("wall");
-        names.push_back("hp");
-        names.push_back("swappable");
-        return names;
-    }
+  static std::vector<std::string> feature_names() {
+    std::vector<std::string> names;
+    names.push_back("wall");
+    names.push_back("hp");
+    names.push_back("swappable");
+    return names;
+  }
 
-    virtual bool swappable() const override {
-        return this->_swappable;
-    }
+  virtual bool swappable() const override {
+    return this->_swappable;
+  }
 };
 
 #endif
