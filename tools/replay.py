4d2e3f093341c770611d89bf4dda395813eb69b9
Jack Heart <jack@stem.ai>
2025-04-24 18:38:12 -0700

    Improve config syntax; switch to using pydantic as a base (#211)
    This does two things:
    
    (1) makes it so you can directly instantiate configs from dictconfigs,
    ala SimulationConfig(cfg.sim)
    
    (2) switches to pydantic as the validation base model, allowing us to
    remove most of the code i wrote previously to do it manually before

--- tools/replay.py
+++ tools/replay.py
@@ -1,19 +1,17 @@
 # Generate a graphical trace of multiple runs.
 
-from dataclasses import dataclass
 
 import hydra
 
 from metta.agent.policy_store import PolicyStore
 from metta.sim.replay_helper import ReplayHelper
 from metta.sim.simulation_config import SimulationConfig
-from metta.util.config import dictconfig_to_dataclass, setup_metta_environment
+from metta.util.config import Config, setup_metta_environment
 from metta.util.runtime_configuration import setup_mettagrid_environment
 from metta.util.wandb.wandb_context import WandbContext
 
 
-@dataclass
-class ReplayJob:
+class ReplayJob(Config):
     sim: SimulationConfig
     policy_uri: str
 
@@ -25,7 +23,7 @@ def main(cfg):
 
     with WandbContext(cfg) as wandb_run:
         policy_store = PolicyStore(cfg, wandb_run)
-        replay_job = dictconfig_to_dataclass(ReplayJob, cfg.replay_job)
+        replay_job = ReplayJob(cfg.replay_job)
         policy_record = policy_store.policy(replay_job.policy_uri)
         replay_helper = ReplayHelper(replay_job.sim, policy_record, wandb_run)
         replay_path = f"{cfg.run_dir}/replays/replay.json.z"
