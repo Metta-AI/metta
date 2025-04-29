163e0d334eff8e8139591f36fe5400336760c3e3
David Bloomin <daveey@gmail.com>
2025-04-29 01:23:25 -0700

    cp

---
+++ metta/agent/policy_state.py
@@ -0,0 +1,18 @@
+import torch
+from tensordict import TensorDict, tensorclass
+
+
+@tensorclass
+class PolicyState(TensorDict):
+    lstm_h: torch.Tensor
+    lstm_c: torch.Tensor
+    hidden: torch.Tensor
+
+    def __init__(self, lstm_h=None, lstm_c=None, hidden=None):
+        super().__init__(
+            {
+                "lstm_h": lstm_h,
+                "lstm_c": lstm_c,
+                "hidden": hidden,
+            }
+        )
