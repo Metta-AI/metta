76ca36ec4beb62f9238951c964454ba4bd3a9a66
David Bloomin <daveey@gmail.com>
2025-04-29 11:52:28 -0700

    cp

--- metta/rl/pufferlib/trainer.py
+++ metta/rl/pufferlib/trainer.py
@@ -387,7 +387,8 @@ class PufferTrainer:
 
                 with profile.train_forward:
                     logits, newvalue = self.policy(obs, lstm_state, action=atn)
-                    lstm_state = PolicyState(lstm_h=lstm_state.lstm_h.detach(), lstm_c=lstm_state.lstm_c.detach())
+                    lstm_state.lstm_h.detach_()
+                    lstm_state.lstm_c.detach_()
                     _, newlogprob, entropy, new_normalized_logits = sample_logits(logits)
                     if self.device == "cuda":
                         torch.cuda.synchronize()
