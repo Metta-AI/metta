c762bfb0e03c03a6db472d3db4d8c41ff74f33f2
Vyacheslav Matyukhin <me@berekuk.ru>
2025-05-02 21:50:25 -0300

    separate stack resources

--- devops/tf/spacelift/stacks.tf
+++ devops/tf/spacelift/stacks.tf
@@ -8,13 +8,31 @@ locals {
   ]
 }
 
-resource "spacelift_stack" "stacks" {
-  for_each     = { for stack in local.stacks : stack.name => stack }
-  name         = each.value.name
+resource "spacelift_stack" "spacelift" {
+  name         = "spacelift"
   space_id     = "root"
   repository   = "metta"
   branch       = "main"
-  project_root = "devops/tf/${each.value.name}"
+  project_root = "devops/tf/spacelift"
+
+  terraform_version            = "1.9.1"
+  terraform_workflow_tool      = "OPEN_TOFU"
+  terraform_smart_sanitization = true
+
+  enable_well_known_secret_masking = true
+  enable_local_preview             = true
+  github_action_deploy             = false
+
+  # spacelift stack uses this for managing other stacks
+  administrative = true
+}
+
+resource "spacelift_stack" "shared_efs" {
+  name         = "shared-efs"
+  space_id     = "root"
+  repository   = "metta"
+  branch       = "main"
+  project_root = "devops/tf/shared-efs"
 
   terraform_version            = "1.9.1"
   terraform_workflow_tool      = "OPEN_TOFU"
@@ -22,16 +40,19 @@ resource "spacelift_stack" "stacks" {
 
   enable_well_known_secret_masking = true
   github_action_deploy             = false
-
-  # spacelift stack uses this for managing other stacks
-  administrative = each.value.name == "spacelift"
 }
 
 # Allow each stack to deploy to AWS
-resource "spacelift_aws_integration_attachment" "aws_integrations" {
-  for_each       = { for stack in local.stacks : stack.name => stack }
+resource "spacelift_aws_integration_attachment" "spacelift" {
   integration_id = spacelift_aws_integration.softmax.id
-  stack_id       = spacelift_stack.stacks["${each.value.name}"].id
+  stack_id       = spacelift_stack.spacelift.id
+  read           = true
+  write          = true
+}
+
+resource "spacelift_aws_integration_attachment" "shared_efs" {
+  integration_id = spacelift_aws_integration.softmax.id
+  stack_id       = spacelift_stack.shared_efs.id
   read           = true
   write          = true
 }
@@ -40,11 +61,11 @@ resource "spacelift_aws_integration_attachment" "aws_integrations" {
 # (In stack settings, under "Environment variables", add the variables one by one when needed.)
 
 import {
-  to = spacelift_stack.stacks["spacelift"]
+  to = spacelift_stack.spacelift
   id = "spacelift"
 }
 
 import {
-  to = spacelift_stack.stacks["shared-efs"]
+  to = spacelift_stack.shared_efs
   id = "efs"
 }
