2a77b3cef95581d2e5ef7634bda9a3a27aba86a9
David Bloomin <daveey@gmail.com>
2025-05-04 21:23:59 -0700

    cp

--- devops/skypilot/install.sh
+++ devops/skypilot/install.sh
@@ -5,4 +5,4 @@ uv venv .venv/skypilot --python=3.11 --no-project
 source .venv/skypilot/bin/activate
 
 # Install SkyPilot with all cloud providers
-uv pip install "skypilot"
+uv pip install skypilot==0.9.2 --prerelease=allow
