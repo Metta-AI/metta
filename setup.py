d79620650757952e86538644883d19ac0901a9c5
David Bloomin <daveey@gmail.com>
2025-05-04 23:31:03 -0700

    cp

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-"""Setup script for fast_gae package."""
-
 import os
 
 import numpy
@@ -8,23 +6,23 @@ from setuptools import Extension, find_packages, setup
 
 ext_modules = [
     Extension(
-            name="fast_gae.fast_gae",
-            sources=["fast_gae/fast_gae.pyx"],
-            define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')],
-            include_dirs=[numpy.get_include()],
-        )
+        name="metta.rl.fast_gae",
+        sources=["metta/rl/fast_gae/fast_gae.pyx"],
+        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+        include_dirs=[numpy.get_include()],
+    )
 ]
 
-debug = os.getenv('DEBUG', '0') == '1'
-annotate = os.getenv('ANNOTATE', '0') == '1'
-BUILD_DIR = 'build'
+debug = os.getenv("DEBUG", "0") == "1"
+annotate = os.getenv("ANNOTATE", "0") == "1"
+BUILD_DIR = "build"
 if debug:
-    BUILD_DIR = 'build_debug'
+    BUILD_DIR = "build_debug"
 
 os.makedirs(BUILD_DIR, exist_ok=True)
 
 setup(
-    name='fast_gae',
+    name="metta",
     packages=find_packages(),
     ext_modules=cythonize(
         ext_modules,
@@ -44,14 +42,13 @@ setup(
             "linetrace": debug,
             "c_string_encoding": "utf-8",
             "c_string_type": "str",
-
         },
         annotate=debug or annotate,
     ),
-    description='',
-    url='https://github.com/Metta-AI/fast_gae',
+    description="",
+    url="https://github.com/Metta-AI/metta",
     install_requires=[
-        'numpy',
-        'cython==3.0.11',
+        "numpy",
+        "cython==3.0.11",
     ],
 )
