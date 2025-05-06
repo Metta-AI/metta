51810890b865d7b6497c63acd79e6dba6a361315
Paul Tsier <paul@elementanalytics.com>
2025-05-06 16:43:31 -0700

    Log more

--- metta/sim/simulation.py
+++ metta/sim/simulation.py
@@ -189,6 +189,7 @@ class Simulation:
 
                 # Parallelize across opponents
                 policy = self._policy_pr.policy()  # policy to evaluate
+                print(f"Obs: ${my_obs}")
                 policy_actions, _, _, _, _ = policy(my_obs, policy_state)
                 print(f"Policy actions: {policy_actions}")
 
