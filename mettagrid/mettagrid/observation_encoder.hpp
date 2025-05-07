2df560af662c45c9f16b173d0a21a62d7d436ab4
Paul Tsier <ptsier@gmail.com>
2025-05-07 15:56:49 -0700

    Make feature offsets deterministic (#420)
    Co-authored-by: Paul Tsier <paul@elementanalytics.com>

--- mettagrid/mettagrid/observation_encoder.hpp
+++ mettagrid/mettagrid/observation_encoder.hpp
@@ -40,9 +40,11 @@ public:
 
     for (size_t type_id = 0; type_id < ObjectType::Count; ++type_id) {
       for (size_t i = 0; i < _type_feature_names[type_id].size(); ++i) {
-        if (features.count(_type_feature_names[type_id][i]) == 0) {
-          features[_type_feature_names[type_id][i]] = features.size();
-          _feature_names.push_back(_type_feature_names[type_id][i]);
+        std::string feature_name = _type_feature_names[type_id][i];
+        if (features.count(feature_name) == 0) {
+          size_t index = features.size();
+          features.insert({feature_name, index});
+          _feature_names.push_back(feature_name);
         }
       }
     }
