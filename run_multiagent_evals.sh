41816f9e5fe07d8ab5ddcaede0eaac6e0f3e20da
daphnedemekas <daphnedemekas@gmail.com>
2025-05-07 14:54:43 -0700

    update runs

--- run_multiagent_evals.sh
+++ run_multiagent_evals.sh
@@ -3,7 +3,27 @@
 # Define the list of policy URIs
 POLICIES=(
     "multiagent_objects_nocolors2"
+    "multienv_nocolors2"
+    "multiagent_colors"
+    "multiagent_objects"
     "b.daphne.multiagent_nocolors"
+    "b.daphne.multiagent_colors_noinitialhearts"
+    "george_24_nosharing_pretrained"
+    "b.daphne.multiagent_mixed_noinitialheart"
+    "b.daphne.multiagent_mixed"
+    "b.daphne.multiagent_colors_noinitialhearts_pretrained"
+    "b.daphne.multiagent_nocolors_noinitialhearts_pretrained"
+    "george_sharing_24"
+    "george_sharing_24_sharing_06"
+    "george_sharing_24_sharing03"
+    "george_24_no_sharing"
+    "george_sharing_48_range_pretrained"
+    "george_sharing48_sharing06_pretrained"
+    "george_sharing_24_range_pretrained"
+    "george_sharing24_sharing06_pretrained"
+    "george_sharing24_sharing03_pretrained"
+    "george_48_no_sharing_prterained"
+    "george_24_no_sharing_pretrained"
 )
 
 for i in "${!POLICIES[@]}"; do
@@ -12,42 +32,29 @@ for i in "${!POLICIES[@]}"; do
     echo "Running full sequence eval for policy $POLICY_URI"
     RANDOM_NUM=$((RANDOM % 1000))
     IDX="${IDX}_${RANDOM_NUM}"
+
+
     python3 -m tools.sim \
         sim=multiagent \
         run=multiagent$IDX \
         policy_uri=wandb://run/$POLICY_URI \
-        +eval_db_uri=wandb://artifacts/multiagent \
-        ++sim_job.selector_type=top \
-        # ++sim_job.metric=navigation_score
+        +eval_db_uri=wandb://artifacts/multiagent_db \
 
-    # python3 -m tools.sim \
-    #     sim=multiagent \
-    #     run=multiagent$IDX \
-    #     policy_uri=wandb://run/$POLICY_URI \
-    #     +eval_db_uri=wandb://artifacts/multiagent_db \
-    #     ++sim_job.selector_type=top \
-    #     ++sim_job.metric=multiagent_score
+    python3 -m tools.sim \
+        sim=memory \
+        run=memory$IDX \
+        policy_uri=wandb://run/$POLICY_URI \
+        +eval_db_uri=wandb://artifacts/memory_db \
 
+    python3 -m tools.sim \
+        sim=cards \
+        run=cards$IDX \
+        policy_uri=wandb://run/$POLICY_URI \
+        +eval_db_uri=wandb://artifacts/cards_db \
 
-    # python3 -m tools.sim \
-    #     sim=memory \
-    #     run=memory$IDX \
-    #     policy_uri=wandb://run/$POLICY_URI \
-    #     +eval_db_uri=wandb://artifacts/memory_db \
-    #     ++sim_job.selector_type=top \
-    #     ++sim_job.metric=memory_score
-
-    # python3 -m tools.sim \
-    #     sim=cards \
-    #     run=cards$IDX \
-    #     policy_uri=wandb://run/$POLICY_URI \
-    #     +eval_db_uri=wandb://artifacts/cards_db \
-    #     ++sim_job.selector_type=latest
-
-    # python3 -m tools.sim \
-    #     sim=object_use \
-    #     run=object_use$IDX \
-    #     policy_uri=wandb://run/$POLICY_URI \
-    #     +eval_db_uri=wandb://artifacts/object_use_db \
-    #     ++sim_job.selector_type=latest
+    python3 -m tools.sim \
+        sim=object_use \
+        run=object_use$IDX \
+        policy_uri=wandb://run/$POLICY_URI \
+        +eval_db_uri=wandb://artifacts/object_use_db \
 done
