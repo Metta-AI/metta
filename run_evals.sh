f5ecba0449a32611ab224f4f3afe6ae46cfca652
daphnedemekas <daphnedemekas@gmail.com>
2025-05-07 15:16:49 -0700

    fix runs

--- run_evals.sh
+++ run_evals.sh
@@ -2,17 +2,29 @@
 
 # Define the list of policy URIs
 POLICIES=(
-<<<<<<< Updated upstream
-    "b.daphne.navigation_multiagent_24_rewardsharing_maxinv"
-    "b.daphne.navigation_multiagent_48_rewardsharing_maxinv"
-    "b.daphne.navigation_multiagent_24_norewardsharing_maxinv"
-    "b.daphne.navigation_multiagent_48_norewardsharing_maxinv"
-    "daphne.navigation"
-    "b.daphne.navigation"
-    "b.daphne.navigation2"
-=======
-    "daphne.navigation:v127"
->>>>>>> Stashed changes
+    "multiagent_objects_nocolors2"
+    "multienv_nocolors2"
+    "multiagent_colors"
+    "multiagent_objects"
+    "george_24_nosharing_pretrained"
+    "b.daphne.multiagent_mixed_noinitialheart"
+    "b.daphne.multiagent_nc_pretrained"
+    "b.daphne.multiagent_c_pretrained"
+    "b.daphne.multiagent_mix_pretrained"
+    "b.daphne.multiagent_nc"
+    "b.daphne.multiagent_c"
+    "b.daphne.multiagent_mix"
+    "george_sharing_24"
+    "george_sharing_24_sharing_06"
+    "george_sharing_24_sharing03"
+    "george_24_no_sharing"
+    "george_sharing_48_range_pretrained"
+    "george_sharing48_sharing06_pretrained"
+    "george_sharing_24_range_pretrained"
+    "george_sharing24_sharing06_pretrained"
+    "george_sharing24_sharing03_pretrained"
+    "george_48_no_sharing_prterained"
+    "george_24_no_sharing_pretrained"
 )
 
 for i in "${!POLICIES[@]}"; do
@@ -21,55 +33,29 @@ for i in "${!POLICIES[@]}"; do
     echo "Running full sequence eval for policy $POLICY_URI"
     RANDOM_NUM=$((RANDOM % 1000))
     IDX="${IDX}_${RANDOM_NUM}"
+
+
     python3 -m tools.sim \
-        sim=navigation \
-        run=navigation$IDX \
-<<<<<<< Updated upstream
+        sim=multiagent \
+        run=multiagent$IDX \
         policy_uri=wandb://run/$POLICY_URI \
-        +eval_db_uri=wandb://artifacts/test_navigation \
-        ++sim_job.selector_type=top \
-        # ++sim_job.metric=navigation_score
-=======
-        policy_uri=wandb://run/daphne.navigation:v127 \
-        +eval_db_uri=wandb://artifacts/test_navigation_main
->>>>>>> Stashed changes
+        +eval_db_uri=wandb://artifacts/multiagent_db \
 
-    # python3 -m tools.sim \
-    #     sim=multiagent \
-    #     run=multiagent$IDX \
-    #     policy_uri=wandb://run/$POLICY_URI \
-<<<<<<< Updated upstream
-    #     +eval_db_uri=wandb://artifacts/multiagent_db \
-    #     ++sim_job.selector_type=top \
-    #     ++sim_job.metric=multiagent_score
+    python3 -m tools.sim \
+        sim=memory \
+        run=memory$IDX \
+        policy_uri=wandb://run/$POLICY_URI \
+        +eval_db_uri=wandb://artifacts/memory_db \
 
-=======
-    #     +eval_db_uri=wandb://artifacts/multiagent_db
->>>>>>> Stashed changes
+    python3 -m tools.sim \
+        sim=cards \
+        run=cards$IDX \
+        policy_uri=wandb://run/$POLICY_URI \
+        +eval_db_uri=wandb://artifacts/cards_db \
 
-    # python3 -m tools.sim \
-    #     sim=memory \
-    #     run=memory$IDX \
-    #     policy_uri=wandb://run/$POLICY_URI \
-<<<<<<< Updated upstream
-    #     +eval_db_uri=wandb://artifacts/memory_db \
-    #     ++sim_job.selector_type=top \
-    #     ++sim_job.metric=memory_score
-
-    # python3 -m tools.sim \
-    #     sim=cards \
-    #     run=cards$IDX \
-    #     policy_uri=wandb://run/$POLICY_URI \
-    #     +eval_db_uri=wandb://artifacts/cards_db \
-    #     ++sim_job.selector_type=latest
-
-    # python3 -m tools.sim \
-    #     sim=object_use \
-    #     run=object_use$IDX \
-    #     policy_uri=wandb://run/$POLICY_URI \
-    #     +eval_db_uri=wandb://artifacts/object_use_db \
-    #     ++sim_job.selector_type=latest
-=======
-    #     +eval_db_uri=wandb://artifacts/memory_db
->>>>>>> Stashed changes
+    python3 -m tools.sim \
+        sim=object_use \
+        run=object_use$IDX \
+        policy_uri=wandb://run/$POLICY_URI \
+        +eval_db_uri=wandb://artifacts/object_use_db \
 done
--- run_multiagent_evals.sh
+++ run_multiagent_evals.sh
@@ -3,27 +3,28 @@
 # Define the list of policy URIs
 POLICIES=(
     "multiagent_objects_nocolors2"
-    "multienv_nocolors2"
-    "multiagent_colors"
-    "multiagent_objects"
-    "b.daphne.multiagent_nocolors"
-    "b.daphne.multiagent_colors_noinitialhearts"
-    "george_24_nosharing_pretrained"
-    "b.daphne.multiagent_mixed_noinitialheart"
-    "b.daphne.multiagent_mixed"
-    "b.daphne.multiagent_colors_noinitialhearts_pretrained"
-    "b.daphne.multiagent_nocolors_noinitialhearts_pretrained"
-    "george_sharing_24"
-    "george_sharing_24_sharing_06"
-    "george_sharing_24_sharing03"
-    "george_24_no_sharing"
-    "george_sharing_48_range_pretrained"
-    "george_sharing48_sharing06_pretrained"
-    "george_sharing_24_range_pretrained"
-    "george_sharing24_sharing06_pretrained"
-    "george_sharing24_sharing03_pretrained"
-    "george_48_no_sharing_prterained"
-    "george_24_no_sharing_pretrained"
+    # "multienv_nocolors2"
+    # "multiagent_colors"
+    # "multiagent_objects"
+    # "george_24_nosharing_pretrained"
+    # "b.daphne.multiagent_mixed_noinitialheart"
+    # "b.daphne.multiagent_nc_pretrained"
+    # "b.daphne.multiagent_c_pretrained"
+    # "b.daphne.multiagent_mix_pretrained"
+    # "b.daphne.multiagent_nc"
+    # "b.daphne.multiagent_c"
+    # "b.daphne.multiagent_mix"
+    # "george_sharing_24"
+    # "george_sharing_24_sharing_06"
+    # "george_sharing_24_sharing03"
+    # "george_24_no_sharing"
+    # "george_sharing_48_range_pretrained"
+    # "george_sharing48_sharing06_pretrained"
+    # "george_sharing_24_range_pretrained"
+    # "george_sharing24_sharing06_pretrained"
+    # "george_sharing24_sharing03_pretrained"
+    # "george_48_no_sharing_prterained"
+    # "george_24_no_sharing_pretrained"
 )
 
 for i in "${!POLICIES[@]}"; do
@@ -38,23 +39,5 @@ for i in "${!POLICIES[@]}"; do
         sim=multiagent \
         run=multiagent$IDX \
         policy_uri=wandb://run/$POLICY_URI \
-        +eval_db_uri=wandb://artifacts/multiagent_db \
-
-    python3 -m tools.sim \
-        sim=memory \
-        run=memory$IDX \
-        policy_uri=wandb://run/$POLICY_URI \
-        +eval_db_uri=wandb://artifacts/memory_db \
-
-    python3 -m tools.sim \
-        sim=cards \
-        run=cards$IDX \
-        policy_uri=wandb://run/$POLICY_URI \
-        +eval_db_uri=wandb://artifacts/cards_db \
-
-    python3 -m tools.sim \
-        sim=object_use \
-        run=object_use$IDX \
-        policy_uri=wandb://run/$POLICY_URI \
-        +eval_db_uri=wandb://artifacts/object_use_db \
+        +eval_db_
 done
