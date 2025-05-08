b4a17e113bb703da864d51a165b4ae89f07cd541
Vyacheslav Matyukhin <me@berekuk.ru>
2025-05-08 17:39:58 -0300

    allow execute-command

--- devops/tf/shared-efs/ecs.tf
+++ devops/tf/shared-efs/ecs.tf
@@ -18,3 +18,8 @@ resource "aws_iam_role_policy_attachment" "ecs_task_exec_policy" {
   role       = aws_iam_role.ecs_task_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+resource "aws_iam_role_policy_attachment" "ecs_task_exec_policy" {
+  role       = aws_iam_role.ecs_task_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonSSMManagedInstanceCore"
+}
