5bb0c6da3b02d68da3c3bc40943db1b2da3435fc
daphnedemekas <daphnedemekas@gmail.com>
2025-05-08 11:40:06 -0700

    fix runs

--- run_evals.sh
+++ run_evals.sh
@@ -35,16 +35,11 @@ for i in "${!POLICIES[@]}"; do
         # sim.num_envs=20 \
         # sim.num_episodes=20
 
-    python3 -m tools.analyze +eval_db_uri=wandb://artifacts/multiagent_db run=multiagentrun3 ++analyzer.output_path=s3://softmax-public/policydash/multiagent.html \
-
-
     python3 -m tools.sim \
         sim=cards \
         run=cards$IDX \
         policy_uri=wandb://run/$POLICY_URI \
         +eval_db_uri=wandb://artifacts/cards_db \
-
-    python3 -m tools.analyze +eval_db_uri=wandb://artifacts/cards_db run=cardsrun3 ++analyzer.output_path=s3://softmax-public/policydash/cards.html \
 
     python3 -m tools.sim \
         sim=object_use \
@@ -54,16 +49,11 @@ for i in "${!POLICIES[@]}"; do
         # sim.num_envs=20 \
         # sim.num_episodes=20 \
 
-    python3 -m tools.analyze +eval_db_uri=wandb://artifacts/object_use_db run=object_userun3 ++analyzer.output_path=s3://softmax-public/policydash/object_use.html \
-
     python3 -m tools.sim \
         sim=memory \
         run=memory$IDX \
         policy_uri=wandb://run/$POLICY_URI \
         +eval_db_uri=wandb://artifacts/memory_db \
-
-    python3 -m tools.analyze +eval_db_uri=wandb://artifacts/memory_db run=multiagentrun3 ++analyzer.output_path=s3://softmax-public/policydash/memory.html \
-
 
     python3 -m tools.sim \
         sim=navigation \
@@ -72,6 +62,14 @@ for i in "${!POLICIES[@]}"; do
         +eval_db_uri=wandb://artifacts/navigation_db \
         # sim.num_envs=20 \
         # sim.num_episodes=20 \
+
+python3 -m tools.analyze +eval_db_uri=wandb://artifacts/multiagent_db run=multiagentrun3 ++analyzer.output_path=s3://softmax-public/policydash/multiagent.html \
+
+python3 -m tools.analyze +eval_db_uri=wandb://artifacts/cards_db run=cardsrun3 ++analyzer.output_path=s3://softmax-public/policydash/cards.html \
+
+python3 -m tools.analyze +eval_db_uri=wandb://artifacts/object_use_db run=object_userun3 ++analyzer.output_path=s3://softmax-public/policydash/object_use.html \
+
+python3 -m tools.analyze +eval_db_uri=wandb://artifacts/memory_db run=multiagentrun3 ++analyzer.output_path=s3://softmax-public/policydash/memory.html \
 
     python3 -m tools.analyze +eval_db_uri=wandb://artifacts/navigation_db run=navigationrun3 ++analyzer.output_path=s3://softmax-public/policydash/navigation.html \
 
