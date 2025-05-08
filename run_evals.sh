a66655b956c549f39ecc9f8d5d810d347e5d400a
daphnedemekas <daphnedemekas@gmail.com>
2025-05-08 11:36:02 -0700

    fix runs

--- run_evals.sh
+++ run_evals.sh
@@ -32,14 +32,12 @@ for i in "${!POLICIES[@]}"; do
         run=multiagent$IDX \
         policy_uri=wandb://run/$POLICY_URI \
         +eval_db_uri=wandb://artifacts/multiagent_db \
-        sim.num_envs=20 \
-        sim.num_episodes=20
+        +sim.eval_db_uri=wandb://artifacts/multiagent_db \
+        # sim.num_envs=20 \
+        # sim.num_episodes=20
 
-    python3 -m tools.sim \
-        sim=memory \
-        run=memory$IDX \
-        policy_uri=wandb://run/$POLICY_URI \
-        +eval_db_uri=wandb://artifacts/memory_db \
+    python3 -m tools.analyze +eval_db_uri=wandb://artifacts/multiagent_db run=multiagentrun3 ++analyzer.output_path=s3://softmax-public/policydash/multiagent.html \
+
 
     python3 -m tools.sim \
         sim=cards \
@@ -47,19 +45,35 @@ for i in "${!POLICIES[@]}"; do
         policy_uri=wandb://run/$POLICY_URI \
         +eval_db_uri=wandb://artifacts/cards_db \
 
+    python3 -m tools.analyze +eval_db_uri=wandb://artifacts/cards_db run=cardsrun3 ++analyzer.output_path=s3://softmax-public/policydash/cards.html \
+
     python3 -m tools.sim \
         sim=object_use \
         run=object_use$IDX \
         policy_uri=wandb://run/$POLICY_URI \
         +eval_db_uri=wandb://artifacts/object_use_db \
-        sim.num_envs=20 \
-        sim.num_episodes=20
+        # sim.num_envs=20 \
+        # sim.num_episodes=20 \
+
+    python3 -m tools.analyze +eval_db_uri=wandb://artifacts/object_use_db run=object_userun3 ++analyzer.output_path=s3://softmax-public/policydash/object_use.html \
+
+    python3 -m tools.sim \
+        sim=memory \
+        run=memory$IDX \
+        policy_uri=wandb://run/$POLICY_URI \
+        +eval_db_uri=wandb://artifacts/memory_db \
+
+    python3 -m tools.analyze +eval_db_uri=wandb://artifacts/memory_db run=multiagentrun3 ++analyzer.output_path=s3://softmax-public/policydash/memory.html \
+
 
     python3 -m tools.sim \
         sim=navigation \
         run=navigation$IDX \
         policy_uri=wandb://run/$POLICY_URI \
         +eval_db_uri=wandb://artifacts/navigation_db \
-        sim.num_envs=20 \
-        sim.num_episodes=20
+        # sim.num_envs=20 \
+        # sim.num_episodes=20 \
+
+    python3 -m tools.analyze +eval_db_uri=wandb://artifacts/navigation_db run=navigationrun3 ++analyzer.output_path=s3://softmax-public/policydash/navigation.html \
+
 done
