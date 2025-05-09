273775382686b5f21d91664d97627bbc6f3ab966
Jack Heart <jack@stem.ai>
2025-05-09 12:51:09 -0700

    make views available on the duckdb cli (#442)
    When we launch a CLI duck db, now the views for EvalStatsDB are present
    
    ---------
    
    Co-authored-by: Jack Heart <jack@softmax.com>
    Co-authored-by: Robb Walters <rjwalters@users.noreply.github.com>

--- tools/sweep_eval.py
+++ tools/sweep_eval.py
@@ -85,9 +85,7 @@ def main(cfg: DictConfig | ListConfig) -> int:
 
         results = eval.simulate()
         eval_time = time.time() - eval_start_time
-        stats_db = results.stats_db
-        stats_db.close()
-        eval_stats_db = EvalStatsDB(stats_db.path)
+        eval_stats_db = EvalStatsDB.from_sim_stats_db(results.stats_db)
         eval_metric = eval_stats_db.get_average_metric_by_filter(cfg.metric, policy_pr)
 
         # Get training stats from metadata if available
