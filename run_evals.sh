756155b04c83c4daeb9775aab9f413ac49f11113
daphnedemekas <daphnedemekas@gmail.com>
2025-05-09 09:46:14 -0700

    update rusn

--- run_evals.sh
+++ run_evals.sh
@@ -2,21 +2,30 @@
 
 # Define the list of policy URIs
 POLICIES=(
-    "b.daphne.multiagent_mix3"
-    "b.daphne.multiagent_c3"
-    "b.daphne.multiagent_nc3"
-    "b.daphne.multiagent_nc1"
-    "b.daphne.multiagent_mix1"
-    "b.daphne.multiagent_c1"
-    "george_sharing_48"
-    "george_sharing48_06"
-    "george_sharing48_03"
-    "george_48_no_sharing"
-    "b.daphne.navigation4"
-    "b.daphne.navigation1"
-    "b.daphne.navigation3"
-    "b.daphne.navigation0"
-    "b.daphne.navigation5"
+    "b.daphne.object_use_training_colors"
+    "b.daphne.object_use_training_colors2"
+    "b.daphne.object_use_training_colors_pretrained2"
+    "b.daphne.object_use_training_no_colors3"
+    "b.daphne.object_use_training_no_colors_pretrained3"
+    "georged_sharing48"
+    "georged_sharing48_03_pretrained"
+    "georged_sharing48_nosharing_pretrained"
+    "georged_24_no_sharing"
+    "georged_24_nosharing_pretrained"
+    "georged_sharing_48_range_pretrained"
+    "georged_sharing_48_06_pretrained"
+    "georged_sharing_24_03_pretrained"
+    "georged_sharing_24_03"
+    "georged_48_no_sharing"
+    "georged_extended_sequence_pretrained"
+    "georged_extended_sequence"
+    ""
+
+
+
+
+
+
 )
 
 for i in "${!POLICIES[@]}"; do
@@ -63,14 +72,14 @@ for i in "${!POLICIES[@]}"; do
         # sim.num_envs=20 \
         # sim.num_episodes=20 \
 
-python3 -m tools.analyze +eval_db_uri=wandb://artifacts/multiagent_db run=multiagentrun3 ++analyzer.output_path=s3://softmax-public/policydash/multiagent.html \
+python3 -m tools.analyze +eval_db_uri=wandb://artifacts/multiagent_db run=multiagentrun4 ++analyzer.output_path=s3://softmax-public/policydash/multiagent.html \
 
 # python3 -m tools.analyze +eval_db_uri=wandb://artifacts/cards_db run=cardsrun3 ++analyzer.output_path=s3://softmax-public/policydash/cards.html \
 
-python3 -m tools.analyze +eval_db_uri=wandb://artifacts/object_use_db run=object_userun3 ++analyzer.output_path=s3://softmax-public/policydash/object_use.html \
+python3 -m tools.analyze +eval_db_uri=wandb://artifacts/object_use_db run=object_userun4 ++analyzer.output_path=s3://softmax-public/policydash/object_use.html \
 
-python3 -m tools.analyze +eval_db_uri=wandb://artifacts/memory_db run=multiagentrun3 ++analyzer.output_path=s3://softmax-public/policydash/memory.html \
+python3 -m tools.analyze +eval_db_uri=wandb://artifacts/memory_db run=multiagentrun4 ++analyzer.output_path=s3://softmax-public/policydash/memory.html \
 
-python3 -m tools.analyze +eval_db_uri=wandb://artifacts/navigation_db run=navigationrun3 ++analyzer.output_path=s3://softmax-public/policydash/navigation.html \
+python3 -m tools.analyze +eval_db_uri=wandb://artifacts/navigation_db run=navigationrun4 ++analyzer.output_path=s3://softmax-public/policydash/navigation.html \
 
 done
