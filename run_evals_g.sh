a0ee923fb9b36c0aa8728aee1ae37a3a688508f9
daphnedemekas <daphnedemekas@gmail.com>
2025-05-09 13:14:49 -0700

    change for testing

--- run_evals_g.sh
+++ run_evals_g.sh
@@ -14,7 +14,7 @@ georged_sharing24_06_pretrained - unexpected keyword teams in terain_from_numpy
 POLICIES=(
 
     "georged_sharing_48"
-    # "georged_24_no_sharing"
+    "georged_24_no_sharing"
     # "georged_48_no_sharing"
     # "georged_sharing48_06_pretrained"
 
@@ -45,9 +45,8 @@ for i in "${!POLICIES[@]}"; do
         sim=navigation \
         run=navigation_sharing$IDX \
         policy_uri=wandb://run/$POLICY_URI \
-        +eval_db_uri=wandb://stats/navigation_db_sharing \
-        device=cpu \
+        +eval_db_uri=wandb://stats/navigation_db_sharing_test \
 
-python3 -m tools.dashboard +eval_db_uri=wandb://stats/navigation_db_sharing run=multiagentrugeorge ++analyzer.output_path=s3://softmax-public/policydash/navigation_sharing.html \
+python3 -m tools.dashboard +eval_db_uri=wandb://stats/navigation_db_sharing_test run=multiagentrugeorge ++analyzer.output_path=s3://softmax-public/policydash/navigation_sharing.html \
 
 done
