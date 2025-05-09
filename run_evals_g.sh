c10dc8bec874cbfd8dc0e9ef89ba4960ffbe8556
daphnedemekas <daphnedemekas@gmail.com>
2025-05-09 16:22:40 -0700

    run evals

--- run_evals_g.sh
+++ run_evals_g.sh
@@ -15,19 +15,19 @@ POLICIES=(
 
     "georged_sharing_48"
     "georged_24_no_sharing"
-    # "georged_48_no_sharing"
-    # "georged_sharing48_06_pretrained"
+    "georged_48_no_sharing"
+    "georged_sharing48_06_pretrained"
 
-    # "georged_24_nosharing_pretrained"
-    # "georged_48_nosharing_pretrained"
-    # "georged_sharing_48_range_pretrained"
-    # "georged_sharing_24_03"
-    # "georged_sharing24_03_pretrained"
-    # "georged_sharing48_03_pretrained"
+    "georged_24_nosharing_pretrained"
+    "georged_48_nosharing_pretrained"
+    "georged_sharing_48_range_pretrained"
+    "georged_sharing_24_03"
+    "georged_sharing24_03_pretrained"
+    "georged_sharing48_03_pretrained"
 
 
-    # "georged_sharing_48_06_pretrained"
-    # "georged_48_no_sharing"
+    "georged_sharing_48_06_pretrained"
+    "georged_48_no_sharing"
 
     ""
 
@@ -40,13 +40,15 @@ for i in "${!POLICIES[@]}"; do
     RANDOM_NUM=$((RANDOM % 1000))
     IDX="${IDX}_${RANDOM_NUM}"
 
+    DB=wandb://stats/navigation_sharing_db
+
 
     python3 -m tools.sim \
-        sim=navigation \
-        run=navigation_sharing$IDX \
+        sim=multiagent \
+        run=multiagent$IDX \
         policy_uri=wandb://run/$POLICY_URI \
-        +eval_db_uri=wandb://stats/navigation_db_sharing_test \
+        +eval_db_uri=$DB \
 
-python3 -m tools.dashboard +eval_db_uri=wandb://stats/navigation_db_sharing_test run=multiagentrugeorge ++analyzer.output_path=s3://softmax-public/policydash/navigation_sharing.html \
+python3 -m tools.dashboard +eval_db_uri=$DB run=multiagentrugeorge2 ++dashboard.output_path=s3://softmax-public/policydash/navigation_sharing.html \
 
 done
