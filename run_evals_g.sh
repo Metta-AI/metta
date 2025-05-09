6fd8837314c2ad110a2fc25ce9ee9150c20dc2ac
daphnedemekas <daphnedemekas@gmail.com>
2025-05-09 13:12:22 -0700

    update runs

--- run_evals_g.sh
+++ run_evals_g.sh
@@ -14,20 +14,20 @@ georged_sharing24_06_pretrained - unexpected keyword teams in terain_from_numpy
 POLICIES=(
 
     "georged_sharing_48"
-    "georged_24_no_sharing"
-    "georged_48_no_sharing"
-    "georged_sharing48_06_pretrained"
+    # "georged_24_no_sharing"
+    # "georged_48_no_sharing"
+    # "georged_sharing48_06_pretrained"
 
-    "georged_24_nosharing_pretrained"
-    "georged_48_nosharing_pretrained"
-    "georged_sharing_48_range_pretrained"
-    "georged_sharing_24_03"
-    "georged_sharing24_03_pretrained"
-    "georged_sharing48_03_pretrained"
+    # "georged_24_nosharing_pretrained"
+    # "georged_48_nosharing_pretrained"
+    # "georged_sharing_48_range_pretrained"
+    # "georged_sharing_24_03"
+    # "georged_sharing24_03_pretrained"
+    # "georged_sharing48_03_pretrained"
 
 
-    "georged_sharing_48_06_pretrained"
-    "georged_48_no_sharing"
+    # "georged_sharing_48_06_pretrained"
+    # "georged_48_no_sharing"
 
     ""
 
@@ -46,6 +46,7 @@ for i in "${!POLICIES[@]}"; do
         run=navigation_sharing$IDX \
         policy_uri=wandb://run/$POLICY_URI \
         +eval_db_uri=wandb://stats/navigation_db_sharing \
+        device=cpu \
 
 python3 -m tools.dashboard +eval_db_uri=wandb://stats/navigation_db_sharing run=multiagentrugeorge ++analyzer.output_path=s3://softmax-public/policydash/navigation_sharing.html \
 
