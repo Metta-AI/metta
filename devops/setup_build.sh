a8611eeb5af7df5babcb7c4e69cfaff4f54c529e
Robb Walters <robb@2amlogic.com>
2025-05-13 10:53:33 -0700

    update package name to match pybind

--- devops/setup_build.sh
+++ devops/setup_build.sh
@@ -93,9 +93,9 @@ python -c "from mettagrid.mettagrid_env import MettaGridEnv; print('✅ Found me
 }
 
 # Check for mettagrid.core.MettaGrid
-echo "Checking import for mettagrid.mettagrid_env.MettaGridEnv..."
-python -c "from mettagrid.core import MettaGrid; print('✅ Found mettagrid.core.MettaGrid')" || {
-  echo "❌ Failed to import mettagrid.core.MettaGrid"
+echo "Checking import for mettagrid.mettagrid_c.MettaGrid..."
+python -c "from mettagrid.mettagrid_c import MettaGrid; print('✅ Found mettagrid.core.MettaGrid')" || {
+  echo "❌ Failed to import mettagrid.mettagrid_c.MettaGrid"
   exit 1
 }
 
