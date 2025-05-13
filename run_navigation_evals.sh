15de0e5a63a346fea526846e3ab93950d3e56708
daphnedemekas <daphnedemekas@gmail.com>
2025-05-13 14:32:52 -0700

    run nav evals

---
+++ run_navigation_evals.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Define the list of policy URIs
+POLICIES=(
+    "b.daphne.navigation0"
+    "b.daphne.navigation4"
+    "b.daphne.navigation1"
+    "b.daphne.navigation3"
+    "navigation_training"
+    "daphne_navigation_train"
+)
+
+for i in "${!POLICIES[@]}"; do
+    POLICY_URI=${POLICIES[$i]}
+
+    echo "Running full sequence eval for policy $POLICY_URI"
+    RANDOM_NUM=$((RANDOM % 1000))
+    IDX="${IDX}_${RANDOM_NUM}"
+    python3 -m tools.sim \
+        sim=navigation \
+        run=navigation$IDX \
+        policy_uri=wandb://run/$POLICY_URI \
+        +eval_db_uri=wandb://artifacts/navigation_db \
+        ++device=cpu \
+
+done
