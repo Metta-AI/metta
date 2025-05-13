474cdccc50ab0185b04437ee70949f064b30f084
Vyacheslav Matyukhin <me@berekuk.ru>
2025-05-12 23:44:45 -0300

    fixes

--- devops/tf/eks/skypilot.tf
+++ devops/tf/eks/skypilot.tf
@@ -12,6 +12,8 @@ resource "random_password" "skypilot_password" {
 # Deployment for this chart is patched manually in production with `--host 0.0.0.0`
 # See also: https://skypilot-org.slack.com/archives/C03J2KQQZSS/p1747063075515989?thread_ts=1746545299.574249&cid=C03J2KQQZSS
 # (I triggered the same bug and used the same fix.)
+#
+# If you're modifying it and terraform wants to update or recreate, you might need to apply the patch with `kubectl edit` again.
 resource "helm_release" "skypilot" {
   name       = "skypilot"
   repository = "https://helm.skypilot.co"
@@ -41,5 +43,8 @@ data "kubernetes_service" "skypilot_ingress_nginx" {
 resource "aws_ssm_parameter" "skypilot_api_url" {
   name  = "/skypilot/api_url"
   type  = "String"
-  value = "https://skypilot:${random_password.skypilot_password.result}@${data.kubernetes_service.skypilot_ingress_nginx.status.load_balancer.ingress[0].hostname}"
+  
+  # Note: [0] for each element are necessary, even though in yaml status looks like an object, not a list.
+  # See https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs/data-sources/service#example-usage
+  value = "https://skypilot:${random_password.skypilot_password.result}@${data.kubernetes_service.skypilot_ingress_nginx.status[0].load_balancer[0].ingress[0].hostname}"
 }
