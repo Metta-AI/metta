94c35b14804e8328a580ce1dfb40e2d0c08076fd
Vyacheslav Matyukhin <me@berekuk.ru>
2025-05-13 16:20:35 -0300

    allow ECR

--- devops/tf/eks/skypilot/iam.tf
+++ devops/tf/eks/skypilot/iam.tf
@@ -92,7 +92,26 @@ resource "aws_iam_policy" "minimal" {
           "s3:*"
         ],
         "Resource" : "*"
-      }
+      },
+      # ECR read-only (identical to AmazonEC2ContainerRegistryReadOnly)
+      {
+          "Effect": "Allow",
+          "Action": [
+              "ecr:GetAuthorizationToken",
+              "ecr:BatchCheckLayerAvailability",
+              "ecr:GetDownloadUrlForLayer",
+              "ecr:GetRepositoryPolicy",
+              "ecr:DescribeRepositories",
+              "ecr:ListImages",
+              "ecr:DescribeImages",
+              "ecr:BatchGetImage",
+              "ecr:GetLifecyclePolicy",
+              "ecr:GetLifecyclePolicyPreview",
+              "ecr:ListTagsForResource",
+              "ecr:DescribeImageScanFindings"
+          ],
+          "Resource": "*"
+      },
     ]
   })
 }
