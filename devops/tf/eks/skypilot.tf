a7d67ae315fc03ee82da06ce7827fd261f6512db
Vyacheslav Matyukhin <me@berekuk.ru>
2025-05-12 23:11:45 -0300

    publish lb

--- devops/tf/eks/skypilot.tf
+++ devops/tf/eks/skypilot.tf
@@ -9,6 +9,9 @@ resource "random_password" "skypilot_password" {
   length = 40
 }
 
+# Deployment for this chart is patched manually in production with `--host 0.0.0.0`
+# See also: https://skypilot-org.slack.com/archives/C03J2KQQZSS/p1747063075515989?thread_ts=1746545299.574249&cid=C03J2KQQZSS
+# (I triggered the same bug and used the same fix.)
 resource "helm_release" "skypilot" {
   name       = "skypilot"
   repository = "https://helm.skypilot.co"
@@ -16,8 +19,13 @@ resource "helm_release" "skypilot" {
   devel = true
   namespace  = kubernetes_namespace.skypilot.metadata[0].name
 
-  set {
+  set_sensitive {
     name  = "ingress.authCredentials"
     value = "skypilot:${bcrypt(random_password.skypilot_password.result)}"
+  }
+
+  set {
+    name  = "ingress-nginx.controller.service.annotations"
+    value = "service.beta.kubernetes.io/aws-load-balancer-scheme: \"internet-facing\""
   }
 }
