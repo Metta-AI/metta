dee27778c92c100d8af153274e3e156015a7f434
treeform <starplant@gmail.com>
2025-05-13 17:04:42 -0700

    fixes

--- mettagrid/player/replays.py
+++ mettagrid/player/replays.py
@@ -7,7 +7,7 @@ import hydra
 
 from metta.agent.policy_store import PolicyStore
 from metta.sim.simulation import Simulation
-from metta.sim.simulation_config import SimulationSuiteConfig
+from metta.sim.simulation_config import SingleEnvSimulationConfig
 from metta.util.config import Config, setup_metta_environment
 from metta.util.logging import setup_mettagrid_logger
 from metta.util.runtime_configuration import setup_mettagrid_environment
@@ -16,14 +16,11 @@ from metta.util.wandb.wandb_context import WandbContext
 
 # TODO: This job can be replaced with sim now that Simulations create replays
 class ReplayJob(Config):
-    sim: SimulationSuiteConfig
+    sim: SingleEnvSimulationConfig
     policy_uri: str
     selector_type: str
-    metric: str
-    replay_dir: str = "s3://softmax-public/replays/local"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    replay_dir: str
+    stats_dir: str
 
 
 def create_simulation(cfg):
@@ -37,16 +34,21 @@ def create_simulation(cfg):
         policy_store = PolicyStore(cfg, wandb_run)
         replay_job = ReplayJob(cfg.replay_job)
         policy_record = policy_store.policy(replay_job.policy_uri)
+        sim_config = SingleEnvSimulationConfig(cfg.replay_job.sim)
 
+        sim_name = sim_config.env.split("/")[-1]
         replay_dir = f"{replay_job.replay_dir}/{cfg.run}"
         if cfg.trainer.get("replay_dry_run", False):
             replay_dir = None
 
         sim = Simulation(
-            name="replay",
-            config=replay_job.sim,
-            policy_pr=policy_record,
-            policy_store=policy_store,
+            sim_name,
+            sim_config,
+            policy_record,
+            policy_store,
+            device=cfg.device,
+            vectorization=cfg.vectorization,
+            stats_dir=replay_job.stats_dir,
             replay_dir=replay_dir,
         )
     return sim
