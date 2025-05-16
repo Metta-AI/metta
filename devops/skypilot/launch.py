83dadbd8c6b8c2f3b763bf0c361f772f55062f47
Vyacheslav Matyukhin <me@berekuk.ru>
2025-05-16 17:20:04 -0300

    skypilot sdk in launch; new resources

--- devops/skypilot/launch.py
+++ devops/skypilot/launch.py
@@ -1,9 +1,44 @@
 #!.venv/skypilot/bin/python3
 
 import argparse
+import copy
+import sys
 import subprocess
 
 import sky
+
+BLUE  = "\033[1;34m"
+RESET = "\033[0;0m"
+
+
+def print_tip(text: str):
+    print(f"{BLUE}{text}{RESET}", file=sys.stderr)
+
+
+def patch_task(task: sky.Task, cpus: int | None, gpus: int | None, nodes: int | None) -> sky.Task:
+    overrides = {}
+    if cpus:
+        overrides["cpus"] = cpus
+    if overrides:
+        task.set_resources_override(overrides)
+    if nodes:
+        task.num_nodes = nodes
+
+    if gpus:
+        new_resources_list = []
+        for res in list(task.resources):
+            if not isinstance(res.accelerators, dict):
+                # shouldn't happen with our current config
+                raise Exception(f"Unexpected accelerator type: {res.accelerators}, {type(res.accelerators)}")
+
+            patched_accelerators = copy.deepcopy(res.accelerators)
+            patched_accelerators = {gpu_type: gpus for gpu_type in patched_accelerators.keys()}
+            new_resources = res.copy(accelerators=patched_accelerators)
+            new_resources_list.append(new_resources)
+
+        task.set_resources(type(task.resources)(new_resources_list))
+
+    return task
 
 
 def main():
@@ -11,9 +46,9 @@ def main():
     parser.add_argument("cmd", help="Command to run")
     parser.add_argument("run", help="Run ID")
     parser.add_argument("--git-ref", type=str, default=None)
-    parser.add_argument("--gpus", type=int, default=1)
-    parser.add_argument("--nodes", type=int, default=1)
-    parser.add_argument("--cpus", type=int, default=8)
+    parser.add_argument("--gpus", type=int, default=None)
+    parser.add_argument("--nodes", type=int, default=None)
+    parser.add_argument("--cpus", type=int, default=None)
     parser.add_argument("--dry-run", action="store_true")
     (args, cmd_args) = parser.parse_known_args()
 
@@ -21,38 +56,25 @@ def main():
     if not git_ref:
         git_ref = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("utf-8").strip()
 
-    cmd = [
-        "sky",
-        "jobs",
-        "launch",
-        "--num-nodes", str(args.nodes),
-        "--cpus", f"{args.cpus}+",
-        "--name", args.run,
-        "./devops/skypilot/config/sk_train.yaml",
-        "--env", f"METTA_RUN_ID={args.run}",
-        "--env", f"METTA_CMD={args.cmd}",
-        "--env", f"METTA_CMD_ARGS={' '.join(cmd_args)}",
-        "--env", f"METTA_GIT_REF={git_ref}",
-        "--detach-run",
-        "--async",
-        "--yes",
-    ]
+    task = sky.Task.from_yaml("./devops/skypilot/config/sk_train.yaml")
+    task = task.update_envs(dict(
+        METTA_RUN_ID=args.run,
+        METTA_CMD=args.cmd,
+        METTA_CMD_ARGS=" ".join(cmd_args),
+        METTA_GIT_REF=git_ref,
+    ))
+
+    task = patch_task(task, cpus=args.cpus, gpus=args.gpus, nodes=args.nodes)
 
     if args.dry_run:
-        print("DRY RUN:")
-        print(" ".join(cmd))
-    else:
-        result = subprocess.run(cmd)
-        exit(result.returncode)
-        # task = sky.Task.from_yaml("./devops/skypilot/config/sk_train.yaml")
-        # task = task.update_env(dict(
-        #     METTA_RUN_ID=args.run,
-        #     METTA_CMD=args.cmd,
-        #     METTA_CMD_ARGS=" ".join(cmd_args),
-        #     METTA_GIT_REF=git_ref,
-        # ))
-        # result = sky.jobs.launch(task)
-        # print(result)
+        print_tip("DRY RUN.")
+        print_tip("Tip: Pipe this command to `| yq -P .` to get the pretty yaml config.\n")
+        print(task.to_yaml_config())
+        return
+
+    result = sky.jobs.launch(task)
+
+    print(result)
 
 
 if __name__ == "__main__":
