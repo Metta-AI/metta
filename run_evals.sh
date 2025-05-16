5827a51460735f2dfcd12adf0bd71405599a1387
S. Alex Smith <sasmith@stem.ai>
2025-05-16 00:30:55 -0700

    wip

--- run_evals.sh
+++ run_evals.sh
@@ -2,7 +2,42 @@
 
 # Define the list of policy URIs to evaluate on a normal run.
 POLICIES=(
-  "b.daphne.navigation0"
+    "daveey.dist.2x4"
+    "navigation_training:v35"
+    "b.daphne.navigation0"
+    "daphne_navigation_train"
+    "b.daphne.navigation1"
+    "b.daphne.navigation3"
+    "b.daphne.navigation4"
+    "gd2_sharing24_03"
+    "gd2_sharing24_06"
+    "gd2_sharing_48"
+    "gd2_sharing_24"
+    "gd2_sharing48_03"
+    "gd2_sharing48_06"
+    "MRM_test_mettabox"
+    "georged_48_no_sharing"
+    "georged_24_no_sharing"
+    "dd_object_use_easy2"
+    "daphne.3object_use_no_colors"
+    "daphne.3object_use_colors"
+    "daphne.2object_use_colors_pretrained"
+    "b.daphne.USER.navigation_before_refactor"
+
+    "b.daphne.object_use_colored_converters"
+    "b.daphne.object_use_onlyred"
+    "b.daphne.object_use_colored_converters_ent0.05"
+    "b.daphne.object_use_onlyred_ent0.05"
+
+    "b.daphne.object_use_colored_converters2"
+    "b.daphne.object_use_onlyred2"
+    "b.daphne.object_use_colored_converters_ent0.052"
+    "b.daphne.object_use_onlyred_ent0.052"
+
+    "b.georgedeane.george_sequence_no_increment"
+    "b.georgedeane.george_sequence_incremental"
+    "george_sequence_incremental"
+    "george2_multienv_noincrement"
 )
 MESSAGE="Running full sequence eval"
 MAYBE_SMOKE_TEST=""
@@ -42,18 +77,20 @@ for i in "${!POLICIES[@]}"; do
   # Tests below this line aren't part of smoke tests, since we either
   # aren't scoring well enough to include them, or have some other reason.
 
-  python3 -m tools.sim \
-    sim=multiagent \
-    run=multiagent$IDX \
-    policy_uri=wandb://run/$POLICY_URI \
-    +eval_db_uri=wandb://artifacts/multiagent_db \
-    $MAYBE_SMOKE_TEST
+  # We also don't want to upload to dashboards for these.
 
-    python3 -m tools.sim \
-    sim=memory \
-    run=memory$IDX \
-    policy_uri=wandb://run/$POLICY_URI \
-    +eval_db_uri=wandb://artifacts/memory_db \
-    $MAYBE_SMOKE_TEST
+  python3 -m tools.sim \
+      sim=memory \
+      run=memory$IDX \
+      policy_uri=wandb://run/$POLICY_URI \
+      sim_job.stats_db_uri=wandb://stats/memory_db \
+      $MAYBE_SMOKE_TEST
+
+  python3 -m tools.sim \
+      sim=object_use \
+      run=objectuse$IDX \
+      policy_uri=wandb://run/$POLICY_URI \
+      sim_job.stats_db_uri=wandb://stats/objectuse_db \
+      $MAYBE_SMOKE_TEST
 
 done
