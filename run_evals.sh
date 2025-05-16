c2f96d439bc071e154f2941eb2b4ecd2b3fadaa9
George Deane <george@stema.ai>
2025-05-16 15:14:28 -0700

    run evals

--- run_evals.sh
+++ run_evals.sh
@@ -2,29 +2,35 @@
 
 # Define the list of policy URIs
 POLICIES=(
-  "b.daphne.terrain_prioritized_styles_pretrained_r"
-  "b.daphne.terrain_prioritized_styles2"
-  "terrain_prioritized_styles_pretrained_mpmc"
-  "terrain_prioritized_styles_pretrained"
-  "b.terrain_prioritized_styles_nb"
-  "b.terrain_prioritized_styles_pretrained_nb"
-  "b.terrain_prioritized_styles"
-  "b.terrain_prioritized_styles_pretrained"
-  "b.georgedeane.terrain_multienv"
-  "b.daphne.terrain_multienv_3_no_blocks3"
-  "terrain_multienv_3_single_agent"
-  "b.daphne.terrain_multienv_prioritized_multienv_cylinders2"
-  "b.daphne.terrain_multienv_prioritized_multienv_cylinders"
-  "b.georgedeane.terrain_massive_empty_world_pretrained"
-  "b.georgedeane.terrain_extra_hard:v1"
-  "b.daphne.terrain_varied_cyl_lab_pretrained"
-  "b.daphne.terrain_prioritized_styles"
-  "b.daphne.terrain_prioritized_styles_pretrained"
-  "george_memory_pretrained"
-  "b.daphne.terrain_multiagent_48_norewardsharing"
-  "b.daphne.terrain_multiagent_24_norewardsharing"
-  "b.daphne.terrain_multiagent_24_rewardsharing"
-  "b.daphne.terrain_multiagent_48_rewardsharing"
+#   "b.daphne.terrain_prioritized_styles_pretrained_r"
+#   "b.daphne.terrain_prioritized_styles2"
+#   "terrain_prioritized_styles_pretrained_mpmc"
+#   "terrain_prioritized_styles_pretrained"
+#   "b.terrain_prioritized_styles_nb"
+#   "b.terrain_prioritized_styles_pretrained_nb"
+#   "b.terrain_prioritized_styles"
+#   "b.terrain_prioritized_styles_pretrained"
+#   "b.georgedeane.terrain_multienv"
+#   "b.daphne.terrain_multienv_3_no_blocks3"
+#   "terrain_multienv_3_single_agent"
+#   "b.daphne.terrain_multienv_prioritized_multienv_cylinders2"
+#   "b.daphne.terrain_multienv_prioritized_multienv_cylinders"
+#   "b.georgedeane.terrain_massive_empty_world_pretrained"
+#   "b.georgedeane.terrain_extra_hard:v1"
+#   "b.daphne.terrain_varied_cyl_lab_pretrained"
+#   "b.daphne.terrain_prioritized_styles"
+#   "b.daphne.terrain_prioritized_styles_pretrained"
+#   "george_memory_pretrained"
+#   "b.daphne.terrain_multiagent_48_norewardsharing"
+#   "b.daphne.terrain_multiagent_24_norewardsharing"
+#   "b.daphne.terrain_multiagent_24_rewardsharing"
+#   "b.daphne.terrain_multiagent_48_rewardsharing"
+  "objectuse_no_colors"
+  "george_sequence_varied"
+  "george_sequence_meta"
+  "george3_multienv_noincrement"
+  "george_sequence_incremental"
+  "george_multienv_incremental"
 )
 
 for i in "${!POLICIES[@]}"; do
@@ -34,20 +40,27 @@ for i in "${!POLICIES[@]}"; do
   RANDOM_NUM=$((RANDOM % 1000))
   IDX="${IDX}_${RANDOM_NUM}"
   python3 -m tools.sim \
+    sim=simple_sequence \
+    run=navigation$IDX \
+    policy_uri=wandb://run/$POLICY_URI \
+    sim_job.stats_db_uri=wandb://stats/simple_sequence \
+
+  python3 -m tools.sim \
+    sim=extended_sequence \
+    run=extended_sequence$IDX \
+    policy_uri=wandb://run/$POLICY_URI \
+    sim_job.stats_db_uri=wandb://stats/extended_sequence \
+
+
+  python3 -m tools.sim \
     sim=navigation \
     run=navigation$IDX \
     policy_uri=wandb://run/$POLICY_URI \
-    +eval_db_uri=wandb://artifacts/navigation_db
+    sim_job.stats_db_uri=wandb://stats/navigation_new \
+    
 
-  python3 -m tools.sim \
-    sim=multiagent \
-    run=multiagent$IDX \
-    policy_uri=wandb://run/$POLICY_URI \
-    +eval_db_uri=wandb://artifacts/multiagent_db
+    python3 -m tools.dashboard +eval_db_uri=wandb://stats/simple_sequence run=simpleseq ++dashboard.output_path=s3://softmax-public/policydash/simpleseq.html \
+    python3 -m tools.dashboard +eval_db_uri=wandb://stats/extended_sequence run=extended_sequence ++dashboard.output_path=s3://softmax-public/policydash/extended_sequence.html \
+    python3 -m tools.dashboard +eval_db_uri=wandb://stats/navigation_new run=navigation_new ++dashboard.output_path=s3://softmax-public/policydash/navigation_new.html \
 
-  python3 -m tools.sim \
-    sim=memory \
-    run=memory$IDX \
-    policy_uri=wandb://run/$POLICY_URI \
-    +eval_db_uri=wandb://artifacts/memory_db
 done
