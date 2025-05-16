5d6046253782f6deea4152c7d5c353770fa78ee2
daphnedemekas <daphnedemekas@gmail.com>
2025-05-16 15:59:50 -0700

    sequence evals

--- metta/util/config.py
+++ metta/util/config.py
@@ -161,7 +161,7 @@ def check_wandb_credentials() -> bool:
         return False
 
 
-def setup_metta_environment(cfg: ListConfig | DictConfig, require_aws: bool = True, require_wandb: bool = True):
+def setup_metta_environment(cfg: ListConfig | DictConfig, require_aws: bool = False, require_wandb: bool = True):
     if require_aws:
         # Check that AWS is good to go.
         if not check_aws_credentials():
