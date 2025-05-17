f0fec99abf031ac7c6c9f49c826f8b4f51674841
Vyacheslav Matyukhin <me@berekuk.ru>
2025-05-17 16:55:27 -0300

    tailscale auth key for mettaboxes

---
+++ devops/tf/shared-efs/mettabox.tf
@@ -0,0 +1,11 @@
+resource "tailscale_tailnet_key" "mettabox" {
+  reusable      = true
+  preauthorized = true
+  description   = "Mettabox key"
+  tags          = ["tag:mettabox"]
+  depends_on    = [tailscale_acl.acl] # won't work until the tag is mentioned in ACL
+}
+
+output "mettabox_tailscale_key" {
+  value = tailscale_tailnet_key.mettabox.key
+}
