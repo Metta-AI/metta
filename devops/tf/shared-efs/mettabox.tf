e5ea9a766cdcaa7e722226c9a2e1690206d1655d
Vyacheslav Matyukhin <me@berekuk.ru>
2025-05-17 17:17:40 -0300

    disable tailscale key expiry on mettaboxes

--- devops/tf/shared-efs/mettabox.tf
+++ devops/tf/shared-efs/mettabox.tf
@@ -7,5 +7,19 @@ resource "tailscale_tailnet_key" "mettabox" {
 }
 
 output "mettabox_tailscale_key" {
+  # make this available through spacelift UI - we apply it to mettaboxes manually
   value = nonsensitive(tailscale_tailnet_key.mettabox.key)
+}
+
+data "tailscale_devices" "mettaboxes" {
+  name_prefix = "metta"
+}
+
+resource "tailscale_device_key" "disable_mettabox_expiry" {
+  for_each = {
+    for device in data.tailscale_devices.mettaboxes.devices :
+    device.id => device if contains(device.tags, "tag:mettabox")
+  }
+  device_id           = each.value.id
+  key_expiry_disabled = true
 }
