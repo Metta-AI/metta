841a7d85d0ba90a4fc65a35edbd01f132a59efcf
daphnedemekas <daphnedemekas@gmail.com>
2025-05-19 14:05:13 -0700

    fix script to run evals

--- run_evaluation.sh
+++ run_evaluation.sh
@@ -4,54 +4,45 @@ set -e
 
 # Define the list of policy URIs to evaluate on a normal run.
 POLICIES=(
-    "b.georgedeane.george_sequence_no_increment"
-    "b.georgedeane.george_sequence_incremental"
-    "george_sequence_incremental"
-    "george2_multienv_noincrement"
-
-    "objectuse_nocolors"
-
-    "george_sequence_varied"
-    "george3_multienv_noincrement"
-
-    "daphne_objectuse_allobjs_multienv"
-    "daphne_objectuse_allobjs"
-    "b.daphne.object_use_mulitenv_pretrained"
-    "b.daphne.object_use_all_easy"
-    "b.daphne.object_use_multienv"
+    "b.daphne.object_use_multienv_pretrained"
     "b.daphne.object_use_multienv2"
+    "daphne_objectuse_bigandsmall"
 )
 #!/bin/bash
 
 
+
 for i in "${!POLICIES[@]}"; do
-  POLICY_URI=${POLICIES[$i]}
+    POLICY_URI=${POLICIES[$i]}
 
-  echo "Running full sequence eval for policy $POLICY_URI"
-  RANDOM_NUM=$((RANDOM % 1000))
-  IDX="${IDX}_${RANDOM_NUM}"
-  python3 -m tools.sim \
-    sim=simple_sequence \
-    run=navigation$IDX \
-    policy_uri=wandb://run/$POLICY_URI \
-    sim_job.stats_db_uri=wandb://stats/simple_sequence \
+    echo "Running full sequence eval for policy $POLICY_URI"
+    RANDOM_NUM=$((RANDOM % 1000))
+    IDX="${IDX}_${RANDOM_NUM}"
+    python3 -m tools.sim \
+        sim=navigation \
+        run=navigation$IDX \
+        policy_uri=wandb://run/$POLICY_URI \
+        sim_job.stats_db_uri=wandb://stats/navigation_db \
+        device=cpu \
 
-  python3 -m tools.sim \
-    sim=extended_sequence \
-    run=extended_sequence$IDX \
-    policy_uri=wandb://run/$POLICY_URI \
-    sim_job.stats_db_uri=wandb://stats/extended_sequence \
+    python3 -m tools.sim \
+        sim=memory \
+        run=memory$IDX \
+        policy_uri=wandb://run/$POLICY_URI \
+        sim_job.stats_db_uri=wandb://stats/memory_db \
+        device=cpu \
+
+    python3 -m tools.sim \
+        sim=object_use \
+        run=objectuse$IDX \
+        policy_uri=wandb://run/$POLICY_URI \
+        sim_job.stats_db_uri=wandb://stats/objectuse_db \
 
 
-  python3 -m tools.sim \
-    sim=navigation \
-    run=navigation$IDX \
-    policy_uri=wandb://run/$POLICY_URI \
-    sim_job.stats_db_uri=wandb://stats/navigation_new \
+    python3 -m tools.dashboard +eval_db_uri=wandb://stats/navigation_db run=navigation_db ++dashboard.output_path=s3://softmax-public/policydash/navigation.html \
 
+    python3 -m tools.dashboard +eval_db_uri=wandb://stats/memory_db run=memory_db ++dashboard.output_path=s3://softmax-public/policydash/memory.html \
 
-    python3 -m tools.dashboard +eval_db_uri=wandb://stats/simple_sequence run=simpleseq ++dashboard.output_path=s3://softmax-public/policydash/simpleseq.html \
-    python3 -m tools.dashboard +eval_db_uri=wandb://stats/extended_sequence run=extended_sequence ++dashboard.output_path=s3://softmax-public/policydash/extended_sequence.html \
-    python3 -m tools.dashboard +eval_db_uri=wandb://stats/navigation_new run=navigation_new ++dashboard.output_path=s3://softmax-public/policydash/navigation_new.html \
+    python3 -m tools.dashboard +eval_db_uri=wandb://stats/objectuse_db run=objectuse_db ++dashboard.output_path=s3://softmax-public/policydash/objectuse.html \
 
 done
