caf2913d9a6c2d5ea563b102588d176ea1ccfa60
Daphne <45669133+daphnedemekas@users.noreply.github.com>
2025-05-19 16:41:06 -0700

    Object use evaluation code (#505)
    Currently commenting out the group metric code as that is breaking some
    of the evals.
    Updated the object use evals so the rooms are smaller, and added in
    additional objects into the training environment.
    
    ---------
    
    Co-authored-by: David Bloomin <daveey@gmail.com>

---
+++ run_evaluation.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -e
+
+# Define the list of policy URIs to evaluate on a normal run.
+POLICIES=(
+    "b.daphne.object_use_multienv_pretrained"
+    "b.daphne.object_use_multienv2"
+    "daphne_objectuse_bigandsmall"
+)
+#!/bin/bash
+
+
+
+for i in "${!POLICIES[@]}"; do
+    POLICY_URI=${POLICIES[$i]}
+
+    echo "Running full sequence eval for policy $POLICY_URI"
+    RANDOM_NUM=$((RANDOM % 1000))
+    IDX="${IDX}_${RANDOM_NUM}"
+    python3 -m tools.sim \
+        sim=navigation \
+        run=navigation$IDX \
+        policy_uri=wandb://run/$POLICY_URI \
+        sim_job.stats_db_uri=wandb://stats/navigation_db \
+        device=cpu \
+
+    python3 -m tools.sim \
+        sim=memory \
+        run=memory$IDX \
+        policy_uri=wandb://run/$POLICY_URI \
+        sim_job.stats_db_uri=wandb://stats/memory_db \
+        device=cpu \
+
+    python3 -m tools.sim \
+        sim=object_use \
+        run=objectuse$IDX \
+        policy_uri=wandb://run/$POLICY_URI \
+        sim_job.stats_db_uri=wandb://stats/objectuse_db \
+
+
+    python3 -m tools.dashboard +eval_db_uri=wandb://stats/navigation_db run=navigation_db ++dashboard.output_path=s3://softmax-public/policydash/navigation.html \
+
+    python3 -m tools.dashboard +eval_db_uri=wandb://stats/memory_db run=memory_db ++dashboard.output_path=s3://softmax-public/policydash/memory.html \
+
+    python3 -m tools.dashboard +eval_db_uri=wandb://stats/objectuse_db run=objectuse_db ++dashboard.output_path=s3://softmax-public/policydash/objectuse.html \
+
+done
