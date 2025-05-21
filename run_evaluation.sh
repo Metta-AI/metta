ac32498c87e8a733f06f60aadd3939db5ce37cce
daphnedemekas <daphnedemekas@gmail.com>
2025-05-21 10:24:29 -0700

    update runs

--- run_evaluation.sh
+++ run_evaluation.sh
@@ -4,11 +4,11 @@ set -e
 
 # Define the list of policy URIs to evaluate on a normal run.
 POLICIES=(
-    "b.daphne.object_use_multienv_pretrained"
-    "b.daphne.object_use_multienv2"
-    "daphne_objectuse_bigandsmall"
-    "mrazo_object-use_allobjs_large-multienv_v01"
-    "daphne_objectuse_allobjs_multienv"
+    # "b.daphne.object_use_multienv_pretrained"
+    # "b.daphne.object_use_multienv2"
+    # "daphne_objectuse_bigandsmall"
+    # "mrazo_object-use_allobjs_large-multienv_v01"
+    # "daphne_objectuse_allobjs_multienv"
     "training_regular_envset"
     "training_prioritized_envset"
     "b.daphne.navigation_prioritized_envset"
@@ -16,9 +16,12 @@ POLICIES=(
     "b.daphne.objectuse_prioritized_envset"
     "b.daphne.objectuse_regular_envset"
     "b.daphne.prioritized_envset"
+    "b.daphne.uniform_envset_nb"
+    "b.daphne.regular_envset_nb"
+    "b.daphne.prioritized_envset_nb"
     "b.daphne.regular_envset"
-    # "training_regular_envset_nb"
-    # "training_prioritized_envset_nb"
+    "training_regular_envset_nb"
+    "training_uniform_envset_nb"
 
 )
 #!/bin/bash
@@ -36,20 +39,21 @@ for i in "${!POLICIES[@]}"; do
         run=navigation$IDX \
         policy_uri=wandb://run/$POLICY_URI \
         sim_job.stats_db_uri=wandb://stats/navigation_db \
-        # device=cpu \
+        device=cpu \
 
     python3 -m tools.sim \
         sim=memory \
         run=memory$IDX \
         policy_uri=wandb://run/$POLICY_URI \
         sim_job.stats_db_uri=wandb://stats/memory_db \
-        # device=cpu \
+        device=cpu \
 
     python3 -m tools.sim \
         sim=object_use \
         run=objectuse$IDX \
         policy_uri=wandb://run/$POLICY_URI \
         sim_job.stats_db_uri=wandb://stats/objectuse_db \
+        device=cpu \
 
 
     python3 -m tools.dashboard +eval_db_uri=wandb://stats/navigation_db run=navigation_db ++dashboard.output_path=s3://softmax-public/policydash/navigation.html \
