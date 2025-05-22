4b4cdf78103a1a800249e6c6c96801609ea8903e
Robb Walters <robb@2amlogic.com>
2025-05-21 19:51:55 -0700

    turn off asserts

--- devops/env.sh
+++ devops/env.sh
@@ -2,7 +2,7 @@
 # env.sh - Environment configuration
 export PYTHONUNBUFFERED=1
 export PYTHONPATH=$PYTHONPATH:$(pwd)
-export PYTHONOPTIMIZE=0
+export PYTHONOPTIMIZE=1
 export HYDRA_FULL_ERROR=1
 export WANDB_CONSOLE=off
 export WANDB_DIR="./wandb"
