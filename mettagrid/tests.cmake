c3294d5c827ed7a62955383ad05b90ff15bd2b8a
Vyacheslav Matyukhin <me@berekuk.ru>
2025-05-23 03:30:47 -0300

    changes

--- mettagrid/tests.cmake
+++ mettagrid/tests.cmake
@@ -3,20 +3,22 @@
 FetchContent_Declare(
   googletest
   GIT_REPOSITORY https://github.com/google/googletest.git
-  GIT_TAG v1.17.0
-)
+  GIT_TAG v1.17.0)
 
 FetchContent_MakeAvailable(googletest)
 
 # Disable building Benchmark's own tests
-set(BENCHMARK_ENABLE_TESTING    OFF CACHE BOOL "" FORCE)
-set(BENCHMARK_ENABLE_GTEST_TESTS OFF CACHE BOOL "" FORCE)
+set(BENCHMARK_ENABLE_TESTING
+    OFF
+    CACHE BOOL "" FORCE)
+set(BENCHMARK_ENABLE_GTEST_TESTS
+    OFF
+    CACHE BOOL "" FORCE)
 
 FetchContent_Declare(
   googlebenchmark
   GIT_REPOSITORY https://github.com/google/benchmark.git
-  GIT_TAG v1.9.4
-)
+  GIT_TAG v1.9.4)
 
 FetchContent_MakeAvailable(googlebenchmark)
 
@@ -26,8 +28,7 @@ FetchContent_MakeAvailable(googlebenchmark)
 enable_testing()
 
 # 2) Compile & register all tests
-file(GLOB_RECURSE TEST_SOURCES CONFIGURE_DEPENDS
-     "${CMAKE_CURRENT_SOURCE_DIR}/tests/*.cpp")
+file(GLOB_RECURSE TEST_SOURCES CONFIGURE_DEPENDS "tests/*.cpp")
 
 set(PYBIND11_TESTS test_mettagrid)
 
@@ -37,28 +38,21 @@ foreach(test_src IN LISTS TEST_SOURCES)
 
   add_executable(${test_name} ${test_src} $<TARGET_OBJECTS:mettagrid_obj>)
 
-  target_include_directories(
-    ${test_name} PRIVATE "${CMAKE_CURRENT_SOURCE_DIR}/mettagrid"
-                         ${NUMPY_INCLUDE_DIR})
+  target_include_directories(${test_name} PRIVATE "${CMAKE_CURRENT_SOURCE_DIR}/mettagrid" ${NUMPY_INCLUDE_DIR})
 
-  # All tests need Python linking since they use mettagrid_obj which contains
-  # pybind11 code
-  target_link_libraries(${test_name} PRIVATE pybind11::pybind11 Python3::Python
-                                             GTest::gtest GTest::gtest_main)
+  # All tests need Python linking since they use mettagrid_obj which contains pybind11 code
+  target_link_libraries(${test_name} PRIVATE pybind11::pybind11 Python3::Python GTest::gtest GTest::gtest_main)
 
   # Pass Python base prefix for runtime configuration
-  target_compile_definitions(${test_name}
-                             PRIVATE PYTHON_BASE_PREFIX="${PYTHON_BASE_PREFIX}")
+  target_compile_definitions(${test_name} PRIVATE PYTHON_BASE_PREFIX="${PYTHON_BASE_PREFIX}")
 
   add_test(NAME ${test_name} COMMAND ${test_name} --gtest_color=yes)
 
-  set_target_properties(${test_name} PROPERTIES RUNTIME_OUTPUT_DIRECTORY
-                                                "${test_src_dir}")
+  set_target_properties(${test_name} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${test_src_dir}")
 endforeach()
 
 # 3) Compile all benchmarks under benchmarks/
-file(GLOB_RECURSE BENCH_SOURCES CONFIGURE_DEPENDS
-     "${CMAKE_CURRENT_SOURCE_DIR}/benchmarks/*.cpp")
+file(GLOB_RECURSE BENCH_SOURCES CONFIGURE_DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/benchmarks/*.cpp")
 
 foreach(bench_src IN LISTS BENCH_SOURCES)
 
@@ -67,15 +61,11 @@ foreach(bench_src IN LISTS BENCH_SOURCES)
 
   add_executable(${bench_name} ${bench_src} $<TARGET_OBJECTS:mettagrid_obj>)
 
-  target_include_directories(
-    ${bench_name} PRIVATE "${CMAKE_CURRENT_SOURCE_DIR}/mettagrid"
-                          ${NUMPY_INCLUDE_DIR})
+  target_include_directories(${bench_name} PRIVATE "${CMAKE_CURRENT_SOURCE_DIR}/mettagrid" ${NUMPY_INCLUDE_DIR})
 
-  target_link_libraries(
-    ${bench_name} PRIVATE pybind11::pybind11 Python3::Python
-                          benchmark::benchmark benchmark::benchmark_main)
+  target_link_libraries(${bench_name} PRIVATE pybind11::pybind11 Python3::Python benchmark::benchmark
+                                              benchmark::benchmark_main)
 
-  set_target_properties(${bench_name} PROPERTIES RUNTIME_OUTPUT_DIRECTORY
-                                                 "${bench_src_dir}")
+  set_target_properties(${bench_name} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${bench_src_dir}")
 
 endforeach()
