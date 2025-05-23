34d7692276bbd4fd48a16f92b26ddcd179c00887
daphnedemekas <daphnedemekas@gmail.com>
2025-05-23 09:52:12 -0700

    update runs

--- run_evaluation.sh
+++ run_evaluation.sh
@@ -4,9 +4,25 @@ set -e
 
 # Define the list of policy URIs to evaluate on a normal run.
 POLICIES=(
-    "b.daphne.object_use_multienv_pretrained"
-    "b.daphne.object_use_multienv2"
-    "daphne_objectuse_bigandsmall"
+    "breadcrumb_daphne_multiagent_training"
+    "b.daphne.multiagent_training_pretrained"
+    "b.daphne.multiagent_training2"
+    "onlyred_daphne_multiagent_training"
+    "b.daphne.multiagent_training_8agents_onlyred"
+    "b.daphne.multiagent_training_4agents_onlyred"
+    "b.daphne.multiagent_training_4agents"
+    "4agents_daphne_multiagent_training"
+    "8agents_daphne_multiagent_training"
+    "b.daphne.multiagent_training"
+    "b.daphne.multiagent_training_8agents"
+    "b.daphne.objectuse_training_breadcrumb"
+    "b.daphne.objectuse_training_breadcrumb_sm"
+    "b.daphne.objectuse_training_breadcrumb_pretrained_sm"
+    "b.daphne.objectuse_training_breadcrumb_pretrained"
+    "b.daphne.multiagent_training_breadcrumb"
+    "b.daphne.multiagent_training_breadcrumb_pretrained"
+    "b.daphne.multiagent_training_breadcrumb_sm"
+    "b.daphne.multiagent_training_breadcrumb_pretrained_sm"
 )
 #!/bin/bash
 
@@ -23,20 +39,28 @@ for i in "${!POLICIES[@]}"; do
         run=navigation$IDX \
         policy_uri=wandb://run/$POLICY_URI \
         sim_job.stats_db_uri=wandb://stats/navigation_db \
-        device=cpu \
+        # device=cpu \
 
     python3 -m tools.sim \
         sim=memory \
         run=memory$IDX \
         policy_uri=wandb://run/$POLICY_URI \
         sim_job.stats_db_uri=wandb://stats/memory_db \
-        device=cpu \
+        # device=cpu \
 
     python3 -m tools.sim \
         sim=object_use \
         run=objectuse$IDX \
         policy_uri=wandb://run/$POLICY_URI \
         sim_job.stats_db_uri=wandb://stats/objectuse_db \
+        # device=cpu \
+
+    python3 -m tools.sim \
+        sim=nav_sequence \
+        run=nav_sequence$IDX \
+        policy_uri=wandb://run/$POLICY_URI \
+        sim_job.stats_db_uri=wandb://stats/nav_sequence_db \
+        # device=cpu \
 
 
     python3 -m tools.dashboard +eval_db_uri=wandb://stats/navigation_db run=navigation_db ++dashboard.output_path=s3://softmax-public/policydash/navigation.html \
@@ -44,5 +68,7 @@ for i in "${!POLICIES[@]}"; do
     python3 -m tools.dashboard +eval_db_uri=wandb://stats/memory_db run=memory_db ++dashboard.output_path=s3://softmax-public/policydash/memory.html \
 
     python3 -m tools.dashboard +eval_db_uri=wandb://stats/objectuse_db run=objectuse_db ++dashboard.output_path=s3://softmax-public/policydash/objectuse.html \
+
+    python3 -m tools.dashboard +eval_db_uri=wandb://stats/nav_sequence_db run=nav_sequence_db ++dashboard.output_path=s3://softmax-public/policydash/nav_sequence.html \
 
 done
