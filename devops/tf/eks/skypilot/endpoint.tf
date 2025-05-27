7f8b765dc2655b4b2e4014e6d03192fffcd52406
Vyacheslav Matyukhin <me@berekuk.ru>
2025-05-26 21:30:22 -0300

    remove accidental extra zone

--- devops/tf/eks/skypilot/endpoint.tf
+++ devops/tf/eks/skypilot/endpoint.tf
@@ -5,27 +5,27 @@ locals {
 # get load balancer from skypilot chart
 data "kubernetes_service" "skypilot_ingress_nginx" {
   metadata {
-    name = "skypilot-ingress-nginx-controller"
+    name      = "skypilot-ingress-nginx-controller"
     namespace = "skypilot"
   }
   depends_on = [helm_release.skypilot]
 }
 
 resource "aws_ssm_parameter" "skypilot_api_url" {
-  name  = "/skypilot/api_url"
-  type  = "String"
-  
+  name = "/skypilot/api_url"
+  type = "String"
+
   # Note: [0] for each element are necessary, even though in yaml status looks like an object, not a list.
   # See https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs/data-sources/service#example-usage
   value = "https://skypilot:${random_password.skypilot_password.result}@${var.subdomain}.${var.zone_domain}"
 }
 
-resource "aws_route53_zone" "main" {
-  name         = var.zone_domain
+data "aws_route53_zone" "main" {
+  name = var.zone_domain
 }
 
 resource "aws_route53_record" "skypilot_api" {
-  zone_id = aws_route53_zone.main.zone_id
+  zone_id = data.aws_route53_zone.main.zone_id
   name    = var.subdomain
   type    = "CNAME"
   ttl     = 60
