e08705d923dcda2aab3e128ed70eead722cf823d
Vyacheslav Matyukhin <me@berekuk.ru>
2025-05-26 21:32:44 -0300

    fix

--- devops/tf/eks/skypilot/endpoint.tf
+++ devops/tf/eks/skypilot/endpoint.tf
@@ -22,6 +22,7 @@ resource "aws_ssm_parameter" "skypilot_api_url" {
 
 data "aws_route53_zone" "main" {
   name = var.zone_domain
+  id   = "Z03090461BEBKPEGZIZ63" # tmp
 }
 
 resource "aws_route53_record" "skypilot_api" {
