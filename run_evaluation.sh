5b613c663a91cbf6af722035f6a18b1250b23676
daphnedemekas <daphnedemekas@gmail.com>
2025-05-28 16:06:56 -0700

    update runs

--- run_evaluation.sh
+++ run_evaluation.sh
@@ -38,27 +38,6 @@ POLICIES=(
 
 
 )
-#!/bin/bash
-
-    # "daphne_objectuse_allobjs_multienv:v94"
-    # "b.daphne.object_use_multienv2:v65"
-    # "training_regular_envset_nb:v76"
-    # "daphne_objectuse_bigandsmall:v67"
-    # "navigation_training:v35"
-    # "training_regular_envset"
-    # "training_prioritized_envset"
-    # "b.daphne.navigation_prioritized_envset"
-    # "b.daphne.navigation_regular_envset"
-    # "b.daphne.objectuse_prioritized_envset"
-    # "b.daphne.objectuse_regular_envset"
-    # "b.daphne.prioritized_envset"
-    # "b.daphne.uniform_envset_nb"
-    # "b.daphne.regular_envset_nb"
-    # "b.daphne.prioritized_envset_nb"
-    # "b.daphne.regular_envset"
-    # "training_regular_envset_nb"
-    # "training_uniform_envset_nb"
-
 
 
 for i in "${!POLICIES[@]}"; do
@@ -71,21 +50,21 @@ for i in "${!POLICIES[@]}"; do
         sim=navigation \
         run=navigation$IDX \
         policy_uri=wandb://run/$POLICY_URI \
-        sim_job.stats_db_uri=wandb://stats/navigation_db2 \
+        sim_job.stats_db_uri=wandb://stats/navigation_db \
         # device=cpu \
 
     python3 -m tools.sim \
         sim=memory \
         run=memory$IDX \
         policy_uri=wandb://run/$POLICY_URI \
-        sim_job.stats_db_uri=wandb://stats/memory_db2 \
+        sim_job.stats_db_uri=wandb://stats/memory_db \
         # device=cpu \
 
     python3 -m tools.sim \
         sim=object_use \
         run=objectuse$IDX \
         policy_uri=wandb://run/$POLICY_URI \
-        sim_job.stats_db_uri=wandb://stats/objectuse_db2 \
+        sim_job.stats_db_uri=wandb://stats/objectuse_db \
         # device=cpu \
 
 
@@ -93,24 +72,24 @@ for i in "${!POLICIES[@]}"; do
         sim=nav_sequence \
         run=nav_sequence$IDX \
         policy_uri=wandb://run/$POLICY_URI \
-        sim_job.stats_db_uri=wandb://stats/nav_sequence_db2 \
+        sim_job.stats_db_uri=wandb://stats/nav_sequence_db \
         # device=cpu \
 
    python3 -m tools.sim \
         sim=multiagent \
         run=multi_agent$IDX \
         policy_uri=wandb://run/$POLICY_URI \
-        sim_job.stats_db_uri=wandb://stats/multi_agent_db2 \
+        sim_job.stats_db_uri=wandb://stats/multi_agent_db \
         # device=cpu \
 
-  python3 -m tools.dashboard +eval_db_uri=wandb://stats/navigation_db2 run=navigation_db2 ++dashboard.output_path=s3://softmax-public/policydash/navigation.html
+  python3 -m tools.dashboard +eval_db_uri=wandb://stats/navigation_db run=navigation_db2 ++dashboard.output_path=s3://softmax-public/policydash/navigation.html
 
-  python3 -m tools.dashboard +eval_db_uri=wandb://stats/memory_db2 run=memory_db2 ++dashboard.output_path=s3://softmax-public/policydash/memory.html
+  python3 -m tools.dashboard +eval_db_uri=wandb://stats/memory_db run=memory_db2 ++dashboard.output_path=s3://softmax-public/policydash/memory.html
 
-  python3 -m tools.dashboard +eval_db_uri=wandb://stats/objectuse_db2 run=objectuse_db2 ++dashboard.output_path=s3://softmax-public/policydash/objectuse.html
+  python3 -m tools.dashboard +eval_db_uri=wandb://stats/objectuse_db run=objectuse_db2 ++dashboard.output_path=s3://softmax-public/policydash/objectuse.html
 
-  python3 -m tools.dashboard +eval_db_uri=wandb://stats/nav_sequence_db2 run=nav_sequence_db2 ++dashboard.output_path=s3://softmax-public/policydash/nav_sequence.html
+  python3 -m tools.dashboard +eval_db_uri=wandb://stats/nav_sequence_db run=nav_sequence_db2 ++dashboard.output_path=s3://softmax-public/policydash/nav_sequence.html
 
-  python3 -m tools.dashboard +eval_db_uri=wandb://stats/multi_agent_db2 run=multi_agent_db2 ++dashboard.output_path=s3://softmax-public/policydash/multiagent.html
+  python3 -m tools.dashboard +eval_db_uri=wandb://stats/multi_agent_db run=multi_agent_db2 ++dashboard.output_path=s3://softmax-public/policydash/multiagent.html
 
 done
