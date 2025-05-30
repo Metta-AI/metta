b95d1183370180dd1c486f0d0783edbdf2f6a55e
Vyacheslav Matyukhin <me@berekuk.ru>
2025-05-30 16:00:26 -0300

    fix tests

--- tests/map/scenes/test_multi_left_and_right.py
+++ tests/map/scenes/test_multi_left_and_right.py
@@ -3,7 +3,7 @@ from tests.map.scenes.utils import scene_to_node
 
 
 def test_basic():
-    scene = MultiLeftAndRight(rows=3, columns=2)
+    scene = MultiLeftAndRight(rows=3, columns=2, altar_ratio=0.75, total_altars=4)
     node = scene_to_node(scene, (20, 20))
 
     assert (node.grid == "wall").sum() > 0
