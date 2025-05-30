541ff6d473edf13cfafbcb8efb62e15018e7a6f2
Vyacheslav Matyukhin <me@berekuk.ru>
2025-05-30 01:49:15 -0300

    update all scene files

--- metta/map/utils/make_scene_config.py
+++ metta/map/utils/make_scene_config.py
@@ -1,7 +1,5 @@
 from typing import Optional
 
-from omegaconf import DictConfig, OmegaConf
-
 from metta.map.mapgen import MapGen
 
 
@@ -9,29 +7,30 @@ def with_boundaries(pattern: str):
     return "\n".join([f"|{line}|" for line in pattern.split("\n")])
 
 
-def make_convchain_config_from_pattern(pattern: str) -> DictConfig:
+def make_convchain_config_from_pattern(pattern: str) -> dict:
     pattern = with_boundaries(pattern)
-    config = OmegaConf.create(
-        {
-            "_target_": "metta.map.scenes.convchain.ConvChain",
+    config = {
+        "type": "metta.map.scenes.convchain.ConvChain",
+        "params": {
             "pattern_size": 3,
             "iterations": 10,
             "temperature": 1,
             "pattern": pattern,
-        }
-    )
+        },
+    }
+
     return config
 
 
-def make_wfc_config_from_pattern(pattern: str) -> Optional[DictConfig]:
+def make_wfc_config_from_pattern(pattern: str) -> Optional[dict]:
     pattern = with_boundaries(pattern)
-    config = OmegaConf.create(
-        {
-            "_target_": "metta.map.scenes.wfc.WFC",
+    config = {
+        "type": "metta.map.scenes.wfc.WFC",
+        "params": {
             "pattern_size": 3,
             "pattern": pattern,
-        }
-    )
+        },
+    }
 
     # Some WFC patterns are invalid, so we need to check that they are valid.
     # This is the slowest part of import, 2-20 seconds per map.
