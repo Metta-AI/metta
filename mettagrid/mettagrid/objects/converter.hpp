5ca27866c18601b28bc7d0d4e4929dc46ee0b6e9
Alex Smith <sasmith@gmail.com>
2025-05-30 10:18:34 -0700

    Remove 1-hot encoding for type_ids (#708)
    Replaced object type one-hot encodings with `agent`, `converter`, and `type_id` feature in observations.
    
    This removes the 1-hot encoding of general type_ids, which mostly meant we were 1-hot encoding converter types. This makes our observation space more robust to changing converter types, and makes the CNN closer to what we're doing with tokens.
    
    In basic smoke tests, this appeared to learn as fast as pre-ablation; though I haven't studied this thoroughly. The change is primarily on the principle of normalizing the data we're sending.

--- mettagrid/mettagrid/objects/converter.hpp
+++ mettagrid/mettagrid/objects/converter.hpp
@@ -150,20 +150,22 @@ public:
 
   void obs(ObsType* obs, const std::vector<uint8_t>& offsets) const override {
     obs[offsets[0]] = 1;
-    obs[offsets[1]] = this->hp;
-    obs[offsets[2]] = this->color;
-    obs[offsets[3]] = this->converting || this->cooling_down;
+    obs[offsets[1]] = _type_id;
+    obs[offsets[2]] = this->hp;
+    obs[offsets[3]] = this->color;
+    obs[offsets[4]] = this->converting || this->cooling_down;
     for (unsigned int i = 0; i < InventoryItem::InventoryItemCount; i++) {
-      obs[offsets[4 + i]] = this->inventory[i];
+      obs[offsets[5 + i]] = this->inventory[i];
     }
   }
 
-  static std::vector<std::string> feature_names(TypeId type_id) {
+  static std::vector<std::string> feature_names() {
     std::vector<std::string> names;
     // We use the same feature names for all converters, since this compresses
     // the observation space. At the moment we don't expose the recipe, since
     // we expect converters to be hard coded.
-    names.push_back(ObjectTypeNames[type_id]);
+    names.push_back("converter");
+    names.push_back("type_id");
     names.push_back("hp");
     names.push_back("color");
     names.push_back("converting");
