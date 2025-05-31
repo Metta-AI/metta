6a890dd3e14d2e8a58ee59eda4a60709e24a58cf
daphnedemekas <daphnedemekas@gmail.com>
2025-05-30 17:03:54 -0700

    more attemps

--- devops/aws/batch/launch_task.py
+++ devops/aws/batch/launch_task.py
@@ -293,7 +293,7 @@ def submit_batch_job(args, task_args):
         "jobQueue": job_queue,
         "jobDefinition": job_definition,
         "containerOverrides": container_config(args, task_args, job_name),
-        "retryStrategy": {"attempts": 1},
+        "retryStrategy": {"attempts": 10},
     }
 
     if args.num_nodes > 1:
