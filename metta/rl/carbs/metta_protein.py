34c306be1b12ba76031afda3a136054655410a2c
Axel Kerbec <akerbec@umich.edu>
2025-06-02 13:46:00 -0700

    Added Protein puffer code

---
+++ metta/rl/carbs/metta_protein.py
@@ -0,0 +1,12 @@
+from metta.rl.protein import (
+    Protein,
+)
+
+
+class MettaProtein(Protein):
+    """
+    MettaProtein is a thin wrapper around the Protein sweep class for use in the metta project.
+    You can extend this class with metta-specific logic if needed.
+    """
+
+    pass
