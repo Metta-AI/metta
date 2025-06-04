76cb66ec1d3d1e5304459898f73db3e6070adacf
daphnedemekas <daphnedemekas@gmail.com>
2025-06-04 09:47:31 -0700

    update runs

--- run_evaluation.sh
+++ run_evaluation.sh
@@ -46,12 +46,12 @@ for i in "${!POLICIES[@]}"; do
     echo "Running full sequence eval for policy $POLICY_URI"
     RANDOM_NUM=$((RANDOM % 1000))
     IDX="${IDX}_${RANDOM_NUM}"
-    python3 -m tools.sim \
-        sim=navigation \
-        run=navigation$IDX \
-        policy_uri=wandb://run/$POLICY_URI \
-        sim_job.stats_db_uri=wandb://stats/stats_db \
-        device=cpu \
+    # python3 -m tools.sim \
+    #     sim=navigation \
+    #     run=navigation$IDX \
+    #     policy_uri=wandb://run/$POLICY_URI \
+    #     sim_job.stats_db_uri=wandb://stats/stats_db \
+    #     # device=cpu \
 
 
     python3 -m tools.sim \
@@ -59,14 +59,14 @@ for i in "${!POLICIES[@]}"; do
         run=memory$IDX \
         policy_uri=wandb://run/$POLICY_URI \
         sim_job.stats_db_uri=wandb://stats/stats_db \
-        device=cpu \
+        # device=cpu \
 
     python3 -m tools.sim \
         sim=object_use \
         run=objectuse$IDX \
         policy_uri=wandb://run/$POLICY_URI \
         sim_job.stats_db_uri=wandb://stats/stats_db \
-        device=cpu \
+        # device=cpu \
 
 
     python3 -m tools.sim \
@@ -74,14 +74,14 @@ for i in "${!POLICIES[@]}"; do
         run=nav_sequence$IDX \
         policy_uri=wandb://run/$POLICY_URI \
         sim_job.stats_db_uri=wandb://stats/stats_db \
-        device=cpu \
+        # device=cpu \
 
-   python3 -m tools.sim \
-        sim=multiagent \
-        run=multi_agent$IDX \
-        policy_uri=wandb://run/$POLICY_URI \
-        sim_job.stats_db_uri=wandb://stats/stats_db \
-        device=cpu \
+  #  python3 -m tools.sim \
+  #       sim=multiagent \
+  #       run=multi_agent$IDX \
+  #       policy_uri=wandb://run/$POLICY_URI \
+  #       sim_job.stats_db_uri=wandb://stats/stats_db \
+  #       # device=cpu \
 
   python3 -m tools.dashboard +eval_db_uri=wandb://stats/stats_db run=makestatsdb ++dashboard.output_path=s3://softmax-public/policydash/results.html
 
