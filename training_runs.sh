ead10a614d18b8b132b8475fb21701ac078ba393
George Deane <george@stema.ai>
2025-06-03 17:32:23 -0700

    training runs

--- training_runs.sh
+++ training_runs.sh
@@ -1,2 +1,15 @@
 
-./devops/skypilot/launch.py train dd_multiagent trainer.curriculum=env/mettagrid/curriculum/multiagent \
+./devops/skypilot/launch.py train gd_backchain_kitchen3 trainer.curriculum=env/mettagrid/curriculum/backchain_kitchen \
+
+./devops/skypilot/launch.py train gd_easy_sequence3 trainer.curriculum=env/mettagrid/curriculum/easy_sequence \
+
+./devops/skypilot/launch.py train gd_sequence_stripped3 trainer.curriculum=env/mettagrid/curriculum/sequence_stripped \
+
+./devops/skypilot/launch.py train gd_kitchen_sink3 trainer.curriculum=env/mettagrid/curriculum/kitchen_sink \
+
+./devops/skypilot/launch.py train gd_backchain_none3 trainer.curriculum=env/mettagrid/curriculum/backchain_none \
+
+./devops/skypilot/launch.py train gd_backchain_seq3 trainer.curriculum=env/mettagrid/curriculum/backchain_seq \
+
+./devops/skypilot/launch.py train gd_backchain3 trainer.curriculum=env/mettagrid/curriculum/backchain \
+
