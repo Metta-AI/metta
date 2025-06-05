a181f4de98a5bbb4cf0b4214cdb53cd342541d41
Robb Walters <robb@2amlogic.com>
2025-06-05 12:47:32 -0700

    update docker

--- devops/docker/build.sh
+++ devops/docker/build.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+echo "Building Docker images..."
 branch=$(git rev-parse --abbrev-ref HEAD)
 docker build -f devops/docker/Dockerfile.base --build-arg CACHE_DATE=$(date +%Y%m%d_%H%M%S) --build-arg BRANCH=$branch -t mettaai/metta-base:latest .
-docker build -f devops/docker/Dockerfile --build-arg CACHE_DATE=$(date +%Y%m%d_%H%M%S) --build-arg BRANCH=$branch -t mettaai/metta:latest .
+docker build -f devops/docker/Dockerfile --build-arg CACHE_DATE=$(date +%Y%m%d_%H%M
