186812b9199e5613246a35946993396454990207
George Deane <george@stema.ai>
2025-06-06 11:33:04 -0700

    policies

--- training_runs.sh
+++ training_runs.sh
@@ -19,13 +19,13 @@
 
  ./devops/skypilot/launch.py train gd_backchain_scratch_hard trainer.curriculum=env/mettagrid/curriculum/backchain_scratch_hard \
 
- ./devops/skypilot/launch.py train gd_hard_mem_seq_pretrained trainer.curriculum=env/mettagrid/curriculum/hard_mem_seq_pretrained ***this needs to be pretrained on 'gd_backchain_mem_pretrained'***\
+ ./devops/skypilot/launch.py train gd_hard_mem_seq_pretrained trainer.curriculum=env/mettagrid/curriculum/hard_mem_seq_pretrained trainer.initial_policy.uri=wandb://run/gd_backchain_mem_pretrained \
 
  ./devops/skypilot/launch.py train gd_pure_mem_backchain trainer.curriculum=env/mettagrid/curriculum/pure_mem_backchain \
 
  ./devops/skypilot/launch.py train gd_pure_seq_backchain trainer.curriculum=env/mettagrid/curriculum/pure_seq_backchain \
 
- ./devops/skypilot/launch.py train gd_kitchen_hard_pretrained trainer.curriculum=env/mettagrid/curriculum/all ***this needs to be pretrained on 'gd_backchain_mem_pretrained'*** \
+ ./devops/skypilot/launch.py train gd_kitchen_hard_pretrained trainer.curriculum=env/mettagrid/curriculum/all trainer.initial_policy.uri=wandb://run/gd_backchain_mem_pretrained \
 
 
 
