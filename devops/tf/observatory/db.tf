d6dd46039ebc92b36f953b61ae354772f509256d
Vyacheslav Matyukhin <me@berekuk.ru>
2025-06-13 19:25:18 -0300

    improve

--- devops/tf/observatory/db.tf
+++ devops/tf/observatory/db.tf
@@ -1,6 +1,3 @@
-############################
-#  Look-ups from the EKS VPC
-############################
 data "aws_eks_cluster" "eks" {
   name = var.eks_cluster_name
 }
@@ -8,9 +5,8 @@ data "aws_eks_cluster" "eks" {
 # Workers usually share the *cluster* security group; that is enough
 # to scope DB access to the nodes.
 locals {
-  vpc_id           = data.aws_eks_cluster.eks.vpc_config[0].vpc_id
-  eks_sg_id        = data.aws_eks_cluster.eks.vpc_config[0].cluster_security_group_id
-  eks_private_cidr = data.aws_eks_cluster.eks.vpc_config[0].cluster_security_group_id
+  vpc_id    = data.aws_eks_cluster.eks.vpc_config[0].vpc_id
+  eks_sg_id = data.aws_eks_cluster.eks.vpc_config[0].cluster_security_group_id
 }
 
 data "aws_subnets" "private" {
@@ -26,22 +22,11 @@ data "aws_subnets" "private" {
   }
 }
 
-###########################
-#  Password & subnet group
-###########################
-resource "random_password" "db" {
-  length  = 20
-  special = true
-}
-
 resource "aws_db_subnet_group" "this" {
   name       = "${var.eks_cluster_name}-db"
   subnet_ids = data.aws_subnets.private.ids
 }
 
-###################
-#  RDS - Postgres
-###################
 resource "aws_security_group" "db" {
   name        = "${var.eks_cluster_name}-postgres-sg"
   description = "Allow Postgres from EKS worker nodes"
@@ -62,28 +47,26 @@ resource "aws_security_group" "db" {
   }
 }
 
+resource "random_password" "db" {
+  length  = 20
+  special = true
+}
+
 resource "aws_db_instance" "postgres" {
-  identifier             = "${var.eks_cluster_name}-pg"
-  engine                 = "postgres"
-  engine_version         = var.db_postgres_version
-  instance_class         = var.db_instance_class
-  allocated_storage      = var.db_allocated_storage
-  username               = "metta"
-  password               = random_password.db.result
+  identifier     = "${var.eks_cluster_name}-pg"
+  engine         = "postgres"
+  engine_version = var.db_postgres_version
+
+  instance_class    = var.db_instance_class
+  allocated_storage = var.db_allocated_storage
+  multi_az          = true
+
   db_subnet_group_name   = aws_db_subnet_group.this.name
   vpc_security_group_ids = [aws_security_group.db.id]
   publicly_accessible    = false # stays inside the VPC
-  db_name                = "metta"
-}
 
-###################
-#  Helpful outputs
-###################
-output "postgres_endpoint" {
-  value = aws_db_instance.postgres.endpoint
-}
+  db_name  = "metta"
+  username = "metta"
+  password = random_password.db.result
 
-output "postgres_password" {
-  value     = random_password.db.result
-  sensitive = true
 }
