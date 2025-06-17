530ce49b8bb525800e2395585abcae653fd03889
Slava Matyukhin <me@berekuk.ru>
2025-06-17 02:31:32 -0300

    Build docker image in CI (#966)
    Scripts for manual building and pushing are still in repo but warn that
    CI should be used instead.
    
    For the discussion of tradeoffs, see
    https://app.asana.com/1/1209016784099267/project/1210131926580024/task/1209185339447610?focus=true

--- devops/skypilot/launch.py
+++ devops/skypilot/launch.py
@@ -10,9 +10,9 @@ from devops.skypilot.utils import (
     check_config_files,
     check_git_state,
     display_job_summary,
-    get_user_confirmation,
     launch_task,
 )
+from metta.util.cli import get_user_confirmation
 from metta.util.colorama import red
 from metta.util.fs import cd_repo_root
 from metta.util.git import get_current_commit, validate_git_ref
