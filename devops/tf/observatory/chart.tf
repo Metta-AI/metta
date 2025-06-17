ee4b1998f7dff0bb76653ba70e28d4c96623d661
Vyacheslav Matyukhin <me@berekuk.ru>
2025-06-17 20:00:53 -0300

    oauth proxy

--- devops/tf/observatory/chart.tf
+++ devops/tf/observatory/chart.tf
@@ -1,9 +1,33 @@
+data "aws_secretsmanager_secret" "google_service_account_secret" {
+  arn = var.google_service_account_secret_arn
+}
+
+data "aws_secretsmanager_secret_version" "google_service_account_secret_version" {
+  secret_id = data.aws_secretsmanager_secret.google_service_account_secret
+}
+
+resource "kubernetes_namespace" "observatory" {
+  metadata {
+    name = "observatory"
+  }
+}
+
+resource "kubernetes_secret" "observatory_secrets" {
+  metadata {
+    name      = "observatory-secrets"
+    namespace = kubernetes_namespace.observatory.metadata[0].name
+  }
+
+  data = {
+    "google-service-account.json" = data.aws_secretsmanager_secret_version.google_service_account_secret_version.secret_string
+  }
+}
+
 resource "helm_release" "observatory" {
   name  = "observatory"
   chart = "./chart"
 
-  namespace        = "observatory"
-  create_namespace = true
+  namespace = kubernetes_namespace.observatory.metadata[0].name
 
   set {
     name  = "db_uri"
