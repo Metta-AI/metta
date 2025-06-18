ae93903b8e869d74d9915d90f601775c3a29c8ae
Vyacheslav Matyukhin <me@berekuk.ru>
2025-06-17 21:56:44 -0300

    better secret

--- devops/tf/observatory/chart.tf
+++ devops/tf/observatory/chart.tf
@@ -4,9 +4,6 @@ resource "kubernetes_namespace" "observatory" {
   }
 }
 
-# `observatory-secrets` secret was created manually
-# (it's not possible to automate it because it's not possible to terraform oauth clients in GCP)
-
 resource "helm_release" "observatory" {
   name  = "observatory"
   chart = "./chart"
