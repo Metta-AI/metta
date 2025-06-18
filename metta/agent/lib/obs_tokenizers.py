6b12f63dbd64b642bdc69d4e6233604125fc3d3a
Alex Vardakostas <avardakostas@gmail.com>
2025-06-17 18:35:29 -0700

    sp

--- metta/agent/lib/obs_tokenizers.py
+++ metta/agent/lib/obs_tokenizers.py
@@ -161,12 +161,12 @@ class ObsAttrCoordEmbed(LayerBase):
         self._out_tensor_shape = [0, self._feat_dim]
 
         # ------------------------------------------------------------
-        self._atr_embeds = nn.Embedding(self._max_embeds, self._atr_embed_dim, padding_idx=255)
+        # self._atr_embeds = nn.Embedding(self._max_embeds, self._atr_embed_dim, padding_idx=255)
 
-        # self._atr_embeds = nn.Parameter(torch.Tensor(256, self._atr_embed_dim))
-        # nn.init.trunc_normal_(self._atr_embeds, std=0.02)
-        # with torch.no_grad():
-        #     self._atr_embeds[255] = 0.0
+        self._atr_embeds = nn.Parameter(torch.Tensor(256, self._atr_embed_dim))
+        nn.init.trunc_normal_(self._atr_embeds, std=0.02)
+        with torch.no_grad():
+            self._atr_embeds[255] = 0.0
         # ------------------------------------------------------------
 
         return None
@@ -179,8 +179,8 @@ class ObsAttrCoordEmbed(LayerBase):
         atr_indices = observations[..., 1].long()  # Shape: [B_TT, M], ready for embedding
 
         # ------------------------------------------------------------
-        embeddings = self._atr_embeds(atr_indices)
-        # embeddings = self._atr_embeds[atr_indices]
+        # embeddings = self._atr_embeds(atr_indices)
+        embeddings = self._atr_embeds[atr_indices]
 
         # ------------------------------------------------------------
 
