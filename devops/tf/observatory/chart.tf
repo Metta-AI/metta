6d48861f26dc89b764ca2cd53323d3419a9f57fb
Vyacheslav Matyukhin <me@berekuk.ru>
2025-06-17 22:06:50 -0300

    force update

--- devops/tf/observatory/chart.tf
+++ devops/tf/observatory/chart.tf
@@ -10,6 +10,9 @@ resource "helm_release" "observatory" {
 
   namespace = kubernetes_namespace.observatory.metadata[0].name
 
+
+  force_update = true
+
   set {
     name  = "db_uri"
     value = "postgresql://${aws_db_instance.postgres.username}:${aws_db_instance.postgres.password}@${aws_db_instance.postgres.endpoint}/${aws_db_instance.postgres.db_name}"
