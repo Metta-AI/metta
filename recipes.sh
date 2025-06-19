afdf139a3bfa45e0ac5b23b9dd443437f005316c
Daphne <45669133+daphnedemekas@users.noreply.github.com>
2025-06-19 16:56:13 -0700

    Small config changes (#1058)
    [Asana
    Task](https://app.asana.com/1/1209016784099267/project/1210348820405981/task/1210595995099140)
    
    [Asana
    Task](https://app.asana.com/1/1209016784099267/project/1210348820405981/task/1210596033218493)
    
    [Asana
    Task](https://app.asana.com/1/1209016784099267/project/1210348820405981/task/1210595995659788)

--- recipes.sh
+++ recipes.sh
@@ -1,11 +1,11 @@
 #NAVIGATION
-./devops/skypilot/launch.py train run=george.deane.navigation.baseline.06-19  trainer.curriculum=env/mettagrid/curriculum/navigation --no-spot --gpus=4 --skip-git-check \
+./devops/skypilot/launch.py train run=USER.navigation.baseline  trainer.curriculum=env/mettagrid/curriculum/navigation --no-spot --gpus=4 --skip-git-check \
 
 #LEARNING THE SEQUENCE
-./devops/skypilot/launch.py train run=georgdeane.sequence.baseline.128tokens.06-19.2  trainer.curriculum=env/mettagrid/curriculum/sequence --no-spot --gpus=4 --skip-git-check \
+./devops/skypilot/launch.py train run=USER.sequence.baseline  trainer.curriculum=env/mettagrid/curriculum/sequence --no-spot --gpus=4 --skip-git-check \
 
 #OBJECT USE
-./devops/skypilot/launch.py train run=georgdeane.object_use.baseline.128tokens.06-19  trainer.curriculum=env/mettagrid/curriculum/object_use --no-spot --gpus=4 --skip-git-check \
+./devops/skypilot/launch.py train run=USER.object_use.baseline  trainer.curriculum=env/mettagrid/curriculum/object_use --no-spot --gpus=4 --skip-git-check \
 
 #NAV-MEMORY-SEQUENCE
-./devops/skypilot/launch.py train run=georgdeane.nav_memory_sequence.baseline.128tokens.06-19  trainer.curriculum=env/mettagrid/curriculum/nav_memory_sequence --no-spot --gpus=4 --skip-git-check \
+./devops/skypilot/launch.py train run=USER.nav_memory_sequence.baseline  trainer.curriculum=env/mettagrid/curriculum/nav_memory_sequence --no-spot --gpus=4 --skip-git-check \
