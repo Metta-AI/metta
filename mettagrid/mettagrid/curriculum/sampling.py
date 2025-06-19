e3e7a2ef25bd89c058b603114ddafba11a2f6352
daphnedemekas <daphnedemekas@gmail.com>
2025-06-18 17:10:34 -0700

    completed tasks

--- mettagrid/mettagrid/curriculum/sampling.py
+++ mettagrid/mettagrid/curriculum/sampling.py
@@ -17,6 +17,7 @@ logger = logging.getLogger(__name__)
 class SamplingCurriculum(Curriculum):
     def __init__(self, env_cfg_template: str, env_overrides: Optional[DictConfig] = None):
         self._cfg_template = config_from_path(env_cfg_template, env_overrides)
+        self.completed_tasks = None
 
     def get_task(self) -> Task:
         cfg = OmegaConf.create(copy.deepcopy(self._cfg_template))
