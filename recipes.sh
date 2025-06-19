6fd0d1cbf8afd19c6d86e78c4b4857abe0fe5636
daphnedemekas <daphnedemekas@gmail.com>
2025-06-19 15:56:42 -0700

    recipes

--- recipes.sh
+++ recipes.sh
@@ -9,20 +9,3 @@
 
 #NAV-MEMORY-SEQUENCE
 ./devops/skypilot/launch.py train run=georgdeane.nav_memory_sequence.baseline.128tokens.06-19  trainer.curriculum=env/mettagrid/curriculum/nav_memory_sequence --no-spot --gpus=4 --skip-git-check \
-
-
-
-#SWEEPS
-
-
-# #NAVIGATION
-# ./devops/skypilot/launch.py sweep run=daphne.navigation.sweep.06-19.2  trainer.curriculum=env/mettagrid/curriculum/navigation --no-spot --gpus=4 --skip-git-check \
-
-# #LEARNING THE SEQUENCE
-# ./devops/skypilot/launch.py sweep run=daphne.sequence.sweep.06-19  trainer.curriculum=env/mettagrid/curriculum/sequence --no-spot --gpus=4 --skip-git-check \
-
-# #OBJECT USE
-# # ./devops/skypilot/launch.py sweep run=daphne.object_use.sweep.06-19  trainer.curriculum=env/mettagrid/curriculum/object_use --no-spot --gpus=4 --skip-git-check \
-
-# #NAV-MEMORY-SEQUENCE
-# ./devops/skypilot/launch.py sweep run=daphne.nav_memory_sequence.sweep.06-19  trainer.curriculum=env/mettagrid/curriculum/nav_memory_sequence --no-spot --gpus=4 --skip-git-check \
