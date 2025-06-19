474429023104e407159bd4cbd1e76463ab7763fb
Richard Higgins <richard@softmax.com>
2025-06-19 10:27:55 -0700

    refactored

--- tools/replay.py
+++ tools/replay.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env -S uv run
 # Generate a replay file that can be used in MettaScope to visualize a single run.
 
-import os
 import platform
 import webbrowser
 
@@ -39,37 +38,28 @@ def main(cfg):
     with WandbContext(cfg.wandb, cfg) as wandb_run:
         policy_store = PolicyStore(cfg, wandb_run)
         replay_job = ReplayJob(cfg.replay_job)
-        ma = policy_store.policy(cfg.replay_job.policy_uri)
-        logger.info(f"Generating replays for policy: {ma.name}")
-
-        if not os.path.exists(cfg.replay_job.replay_dir):
-            os.makedirs(cfg.replay_job.replay_dir, exist_ok=True)
-        if not os.path.exists(cfg.replay_job.stats_dir):
-            os.makedirs(cfg.replay_job.stats_dir, exist_ok=True)
-
+        policy_ma = policy_store.policy(replay_job.policy_uri)
         sim_config = SingleEnvSimulationConfig(cfg.replay_job.sim)
 
+        sim_name = sim_config.env.split("/")[-1]
+        replay_dir = f"{replay_job.replay_dir}/{sim_name}"
+
         sim = Simulation(
-            name=f"replay_{ma.name.replace('/', '_')}",
-            config=sim_config,
-            policy_ma=ma,
-            policy_store=policy_store,
+            sim_name,
+            sim_config,
+            policy_ma,
+            policy_store,
             device=cfg.device,
             vectorization=cfg.vectorization,
-            replay_dir=cfg.replay_job.replay_dir,
-            stats_dir=cfg.replay_job.stats_dir,
+            stats_dir=replay_job.stats_dir,
+            replay_dir=replay_dir,
         )
-        results = sim.simulate()
-
-        replay_urls = results.stats_db.get_replay_urls(policy_key=ma.key(), policy_version=ma.version())
-        if len(replay_urls) > 0:
-            logger.info("Replay URLs:")
-            for url in replay_urls:
-                logger.info(f"  {url}")
+        result = sim.simulate()
+        replay_url = result.stats_db.get_replay_urls(policy_key=policy_ma.key(), policy_version=policy_ma.version())[0]
 
         # Only on macos open a browser to the replay
         if platform.system() == "Darwin":
-            webbrowser.open(f"https://metta-ai.github.io/metta/?replayUrl={http_url(replay_urls[0])}")
+            webbrowser.open(f"https://metta-ai.github.io/metta/?replayUrl={http_url(replay_url)}")
 
 
 if __name__ == "__main__":
