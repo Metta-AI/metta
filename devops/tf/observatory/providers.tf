fcf9a3aedaeb8acb677c94a39f389d8b18e740b7
Vyacheslav Matyukhin <me@berekuk.ru>
2025-06-19 00:55:14 -0300

    route53 fixes

--- devops/tf/observatory/providers.tf
+++ devops/tf/observatory/providers.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
