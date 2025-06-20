7e43d8294abbdf085e8d7deb0c05a16e7828db3f
daphnedemekas <daphnedemekas@gmail.com>
2025-06-20 12:10:35 -0700

    recipes

--- recipes.sh
+++ recipes.sh
@@ -1,11 +1,29 @@
 #NAVIGATION
-./devops/skypilot/launch.py train run=USER.navigation.baseline  trainer.curriculum=env/mettagrid/curriculum/navigation --no-spot --gpus=4 --skip-git-check \
+./devops/skypilot/launch.py train run=georgdeane.navigation.baseline.06-20  trainer.curriculum=env/mettagrid/curriculum/navigation --no-spot --gpus=4 --skip-git-check \
 
 #LEARNING THE SEQUENCE
-./devops/skypilot/launch.py train run=USER.sequence.baseline  trainer.curriculum=env/mettagrid/curriculum/sequence --no-spot --gpus=4 --skip-git-check \
+./devops/skypilot/launch.py train run=georgdeane.sequence.baseline.06-20  trainer.curriculum=env/mettagrid/curriculum/sequence --no-spot --gpus=4 --skip-git-check \
 
 #OBJECT USE
-./devops/skypilot/launch.py train run=USER.object_use.baseline  trainer.curriculum=env/mettagrid/curriculum/object_use --no-spot --gpus=4 --skip-git-check \
+./devops/skypilot/launch.py train run=georgdeane.object_use.baseline.06-20  trainer.curriculum=env/mettagrid/curriculum/object_use --no-spot --gpus=4 --skip-git-check \
 
 #NAV-MEMORY-SEQUENCE
-./devops/skypilot/launch.py train run=USER.nav_memory_sequence.baseline  trainer.curriculum=env/mettagrid/curriculum/nav_memory_sequence --no-spot --gpus=4 --skip-git-check \
+./devops/skypilot/launch.py train run=georgdeane.nav_memory_sequence.baseline.06-20  trainer.curriculum=env/mettagrid/curriculum/nav_memory_sequence --no-spot --gpus=4 --skip-git-check \
+
+
+# ./devops/skypilot/launch.py train run=daphne.progressive.06-20 trainer.curriculum=env/mettagrid/curriculum/progressive --no-spot --gpus=4 --skip-git-check \
+
+#SWEEPS
+
+
+# #NAVIGATION
+# ./devops/skypilot/launch.py sweep run=daphne.navigation.sweep.06-19.2  trainer.curriculum=env/mettagrid/curriculum/navigation --no-spot --gpus=4 --skip-git-check \
+
+# #LEARNING THE SEQUENCE
+# ./devops/skypilot/launch.py sweep run=daphne.sequence.sweep.06-19  trainer.curriculum=env/mettagrid/curriculum/sequence --no-spot --gpus=4 --skip-git-check \
+
+# #OBJECT USE
+# # ./devops/skypilot/launch.py sweep run=daphne.object_use.sweep.06-19  trainer.curriculum=env/mettagrid/curriculum/object_use --no-spot --gpus=4 --skip-git-check \
+
+# #NAV-MEMORY-SEQUENCE
+# ./devops/skypilot/launch.py sweep run=daphne.nav_memory_sequence.sweep.06-19  trainer.curriculum=env/mettagrid/curriculum/nav_memory_sequence --no-spot --gpus=4 --skip-git-check \
