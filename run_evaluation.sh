d9c85968c24b9411589455da6db7113e98e91c9b
daphnedemekas <daphnedemekas@gmail.com>
2025-06-20 12:24:44 -0700

    update runs

--- run_evaluation.sh
+++ run_evaluation.sh
@@ -33,28 +33,28 @@ for i in "${!POLICIES[@]}"; do
     run=navigation$IDX \
     policy_uri=wandb://run/$POLICY_URI \
     sim_job.stats_db_uri=wandb://stats/navigation_db2 \
-    device=cpu \
+    # device=cpu \
 
   python3 -m tools.sim \
     sim=memory \
     run=memory$IDX \
     policy_uri=wandb://run/$POLICY_URI \
     sim_job.stats_db_uri=wandb://stats/memory_db2 \
-    device=cpu \
+    # device=cpu \
 
   python3 -m tools.sim \
     sim=object_use \
     run=objectuse$IDX \
     policy_uri=wandb://run/$POLICY_URI \
     sim_job.stats_db_uri=wandb://stats/objectuse_db2 \
-    device=cpu \
+    # device=cpu \
 
   python3 -m tools.sim \
     sim=nav_sequence \
     run=nav_sequence$IDX \
     policy_uri=wandb://run/$POLICY_URI \
     sim_job.stats_db_uri=wandb://stats/nav_sequence_db2 \
-    device=cpu \
+    # device=cpu \
 
   python3 -m tools.dashboard +eval_db_uri=wandb://stats/navigation_db2 run=navigation_db ++dashboard.output_path=s3://softmax-public/policydash/navigation.html
 
