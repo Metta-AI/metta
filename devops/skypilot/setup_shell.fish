c231e3ea30b4ff9379f4d141c3f4f564001c0caf
Alex Vardakostas <avardakostas@gmail.com>
2025-06-23 11:29:56 -0700

    lowering train time

---
+++ devops/skypilot/setup_shell.fish
@@ -0,0 +1,18 @@
+set -gx AWS_PROFILE softmax
+
+# list jobs
+alias jj="sky jobs queue --skip-finished" # avoid conflict with jq (JSON processor)
+alias jja="sky jobs queue"
+
+# cancel ("kill") job
+alias jk="sky jobs cancel -y"
+alias jka="sky jobs cancel -a -y"
+
+# get logs
+alias jl="sky jobs logs"
+alias jlc="sky jobs logs --controller"
+alias jll='sky jobs logs (jj | grep -A1 TASK | grep -v TASK | awk "{print \$1}")'
+alias jllc='sky jobs logs --controller (jj | grep -A1 TASK | grep -v TASK | awk "{print \$1}")'
+
+# launch training
+alias lt="./devops/skypilot/launch.py train"
