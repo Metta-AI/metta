0af0d7145bd94866ff8b4cccf35d4eb1e44f92e9
mstormbull <111159631+mstormbull@users.noreply.github.com>
2025-06-23 22:46:25 -0700

    Adding lost __init__

---
+++ mettagrid/src/metta/mettagrid/curriculum/__init__.py
@@ -0,0 +1,19 @@
+from .core import Curriculum, SingleTaskCurriculum, Task
+from .learning_progress import LearningProgressCurriculum
+from .low_reward import LowRewardCurriculum
+from .multi_task import MultiTaskCurriculum
+from .progressive import ProgressiveCurriculum
+from .random import RandomCurriculum
+from .sampling import SamplingCurriculum
+
+__all__ = [
+    "Curriculum",
+    "SingleTaskCurriculum",
+    "Task",
+    "LearningProgressCurriculum",
+    "LowRewardCurriculum",
+    "MultiTaskCurriculum",
+    "ProgressiveCurriculum",
+    "RandomCurriculum",
+    "SamplingCurriculum",
+]
