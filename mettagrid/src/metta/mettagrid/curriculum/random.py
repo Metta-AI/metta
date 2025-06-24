e643b7dc1ef835d1d8e43f3eeecde9ac0c5447c0
Robb Walters <robb@2amlogic.com>
2025-06-24 12:16:24 -0700

    lint

--- mettagrid/src/metta/mettagrid/curriculum/random.py
+++ mettagrid/src/metta/mettagrid/curriculum/random.py
@@ -3,9 +3,8 @@ from __future__ import annotations
 import logging
 import random
 
-from mmetta.mettagrid.curriculum.multi_task import MultiTaskCurriculum
-
 from metta.mettagrid.curriculum.core import Task
+from metta.mettagrid.curriculum.multi_task import MultiTaskCurriculum
 
 logger = logging.getLogger(__name__)
 
