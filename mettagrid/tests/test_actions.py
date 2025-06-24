d587ba6c4596d422a41868734fa7e0f3e63bcb8b
S. Alex Smith <sasmith@stem.ai>
2025-06-24 15:44:30 -0700

    wip

--- mettagrid/tests/test_actions.py
+++ mettagrid/tests/test_actions.py
@@ -41,8 +41,8 @@ def base_config():
             "change_color": {"enabled": True},
         },
         "groups": {"red": {"id": 0, "props": {}}},
-        "objects": {"wall": {}, "altar": {}},
-        "agent": {},
+        "objects": {"wall": {}, "altar": {"max_output": -1, "conversion_ticks": 1, "cooldown": 10, "initial_items": 0}},
+        "agent": {"rewards": {}},
     }
 
 
