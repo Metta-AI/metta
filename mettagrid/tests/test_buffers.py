1e52c68a45a2c03337927df955d6bed0e5b8b480
Alex Smith <sasmith@gmail.com>
2025-06-24 18:44:48 -0700

    Adds a mettagrid_c configuration type (#1107)
    Introduces a new configuration system for MettaGrid's cpp environment
    that's distinct and more digested than the yaml-written-by-humans.
    
    This primarily accomplishes:
    - Takes the group -> agent denormalizations out of cpp and moves them to
    Python.
    - Introduces the idea of there being a separate config for the python
    facing side and the cpp facing side
    
    This is something like a 3/4-baked PR. Moving the agent-group merging
    out of cpp feels like a win; but aside from that this just adds another
    layer. But this is a layer where I'm going to be building more stuff.
    
    [Asana
    Task](https://app.asana.com/1/1209016784099267/project/1210348820405981/task/1210622116093600)
    
    [Asana
    Task](https://app.asana.com/1/1209016784099267/project/1209230969804461/task/1210622116093600)
    
    ---------
    
    Co-authored-by: S. Alex Smith <sasmith@stem.ai>

--- mettagrid/tests/test_buffers.py
+++ mettagrid/tests/test_buffers.py
@@ -2,6 +2,7 @@ import numpy as np
 import pytest
 
 from metta.mettagrid.mettagrid_c import MettaGrid
+from metta.mettagrid.mettagrid_c_config import cpp_config_dict
 from metta.mettagrid.mettagrid_env import (
     dtype_actions,
     dtype_observations,
@@ -58,8 +59,8 @@ def create_minimal_mettagrid_c_env(max_steps=10, width=5, height=5, config_overr
         },
         "groups": {"red": {"id": 0, "props": {}}},
         "objects": {
-            "wall": {"type_id": 1},
-            "block": {"type_id": 2},
+            "wall": {},
+            "block": {},
         },
         "agent": {},
     }
@@ -77,7 +78,7 @@ def create_minimal_mettagrid_c_env(max_steps=10, width=5, height=5, config_overr
 
         deep_merge(game_config, config_override)
 
-    return MettaGrid(game_config, game_map.tolist())
+    return MettaGrid(cpp_config_dict(game_config), game_map.tolist())
 
 
 class TestBuffers:
