73fa512c5436bba1ef500100160b4a23bf67b00e
Alex Smith <sasmith@gmail.com>
2025-06-24 18:44:48 -0700

    Adds a mettagrid_c configuration type (#1107)
    Introduces a new configuration system for MettaGrid's cpp environment
    that's distinct and more digested than the yaml-written-by-humans.
    
    This primarily accomplishes:
    - Takes the group -> agent denormalizations out of cpp and moves them to
    Python.
    - Introduces the idea of there being a separate config for the python
    facing side and the cpp facing side
    
    This is something like a 3/4-baked PR. Moving the agent-group merging
    out of cpp feels like a win; but aside from that this just adds another
    layer. But this is a layer where I'm going to be building more stuff.
    
    [Asana
    Task](https://app.asana.com/1/1209016784099267/project/1210348820405981/task/1210622116093600)
    
    [Asana
    Task](https://app.asana.com/1/1209016784099267/project/1209230969804461/task/1210622116093600)
    
    ---------
    
    Co-authored-by: S. Alex Smith <sasmith@stem.ai>

--- mettagrid/tests/test_mettagrid.py
+++ mettagrid/tests/test_mettagrid.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from metta.mettagrid.mettagrid_c import MettaGrid
+from metta.mettagrid.mettagrid_c_config import cpp_config_dict
 from metta.mettagrid.mettagrid_env import dtype_actions
 
 NUM_AGENTS = 2
@@ -46,13 +47,13 @@ def create_minimal_mettagrid_c_env(max_steps=10, width=5, height=5):
         },
         "groups": {"red": {"id": 0, "props": {}}},
         "objects": {
-            "wall": {"type_id": 1},
-            "block": {"type_id": 2},
+            "wall": {},
+            "block": {},
         },
         "agent": {},
     }
 
-    return MettaGrid(game_config, game_map.tolist())
+    return MettaGrid(cpp_config_dict(game_config), game_map.tolist())
 
 
 def test_grid_hash():
