0fb2a6c7dbd4fda2949074de62b29d29d331ef74
daphnedemekas <daphnedemekas@gmail.com>
2025-06-25 19:17:39 -0700

    update nav recipe - single gpu

--- recipes/navigation.sh
+++ recipes/navigation.sh
@@ -1,2 +1,5 @@
 #NAVIGATION
-./devops/skypilot/launch.py train run=$USER.navigation.bucketed.baseline  trainer.curriculum=env/mettagrid/curriculum/navigation/bucketed --skip-git-check \
+
+#Best recipe for navigation
+#For baseline to compare, see wandb run: #TODO
+./devops/skypilot/launch.py train run=$USER.navigation.bucketed.06-25  trainer.curriculum=env/mettagrid/curriculum/navigation/bucketed --skip-git-check --gpus=1 +trainer.env_overrides.game.num_agents=4 \
