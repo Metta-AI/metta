fea3b5ba0c71317402c1eca938f1add326c4982b
Robb Walters <robb@2amlogic.com>
2025-06-26 18:33:05 -0700

    lap should always be present

--- metta/rl/trainer.py
+++ metta/rl/trainer.py
@@ -911,8 +911,8 @@ class MettaTrainer:
         }
 
         epoch_steps = self.timer.get_lap_steps()
-        if epoch_steps is None:
-            epoch_steps = self.agent_step
+        logger.info(f"epoch_steps = {epoch_steps}")
+
         epoch_steps_per_second = epoch_steps / wall_time_for_lap if wall_time_for_lap > 0 else 0
         steps_per_second = self.timer.get_rate(self.agent_step) if wall_time > 0 else 0
 
