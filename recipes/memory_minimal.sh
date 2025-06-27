0a438951857bf0c66ef525cbcc00ef1e930c4a72
daphnedemekas <daphnedemekas@gmail.com>
2025-06-27 10:23:00 +0300

    udpate user

--- recipes/memory_minimal.sh
+++ recipes/memory_minimal.sh
@@ -1,2 +1,2 @@
 #MINIMAL-MEMORY
-./devops/skypilot/launch.py train run=USER.minimal_memory.baseline  trainer.curriculum=env/mettagrid/curriculum/minimal_memory --no-spot --gpus=4 --skip-git-check \
+./devops/skypilot/launch.py train run=$USER.minimal_memory.baseline  trainer.curriculum=env/mettagrid/curriculum/minimal_memory --no-spot --gpus=1 --skip-git-check \
--- recipes/memory_varied.sh
+++ recipes/memory_varied.sh
@@ -1,3 +1,2 @@
 #MEMORY-VARIED
-./devops/skypilot/launch.py train run=USER.memory_varied.baseline  trainer.curriculum=env/mettagrid/curriculum/memory_training_varied --no-spot --gpus=4 --skip-git-check \
-
+./devops/skypilot/launch.py train run=$USER.memory_varied.baseline  trainer.curriculum=env/mettagrid/curriculum/memory_training_varied --no-spot --gpus=1 --skip-git-check \
