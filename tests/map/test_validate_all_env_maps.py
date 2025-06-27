1508e3a95ef07154fa203e285930f9851b3fe598
Vyacheslav Matyukhin <me@berekuk.ru>
2025-06-27 20:47:04 -0300

    fix numpy tests skipping

--- tests/map/test_validate_all_env_maps.py
+++ tests/map/test_validate_all_env_maps.py
@@ -1,5 +1,5 @@
 import pytest
-from botocore.exceptions import NoCredentialsError
+from botocore.exceptions import NoCredentialsError, ProfileNotFound
 
 from metta.common.util.mettagrid_cfgs import MettagridCfgFileMetadata
 from metta.common.util.resolvers import register_resolvers
@@ -34,10 +34,10 @@ def map_or_env_configs() -> list[MettagridCfgFileMetadata]:
 
 @pytest.mark.parametrize("cfg_metadata", map_or_env_configs(), ids=[cfg.path for cfg in map_or_env_configs()])
 def test_validate_cfg(cfg_metadata):
-    map_cfg = cfg_metadata.get_cfg().get_map_cfg()
     try:
+        map_cfg = cfg_metadata.get_cfg().get_map_cfg()
         map_builder_cfg_to_storable_map(map_cfg)
-    except NoCredentialsError as e:
+    except (NoCredentialsError, ProfileNotFound) as e:
         pytest.skip(f"Skipping {cfg_metadata.path} because it requires AWS credentials: {e}")
     except Exception as e:
         pytest.fail(f"Failed to validate map config {cfg_metadata.path}: {e}")
