65985945e69b65a0956af6a5691c4ce77ebd4fd0
daphnedemekas <daphnedemekas@gmail.com>
2025-06-27 11:18:57 +0300

    cleanup

--- recipes/memory_varied.sh
+++ recipes/memory_varied.sh
@@ -1,2 +1,2 @@
 #MEMORY-VARIED
-./devops/skypilot/launch.py train run=$USER.memory_varied.baseline  trainer.curriculum=env/mettagrid/curriculum/memory_training_varied --no-spot --gpus=1 --skip-git-check \
+./devops/skypilot/launch.py train run=$USER.memory_varied.baseline  trainer.curriculum=env/mettagrid/curriculum/memory/memory_varied --no-spot --gpus=1 --skip-git-check \
