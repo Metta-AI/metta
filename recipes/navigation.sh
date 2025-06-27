e7d02e1ca67d0506579b1b0a16ea1f7f4a0ac410
daphnedemekas <daphnedemekas@gmail.com>
2025-06-27 09:42:46 +0300

    recipes

--- recipes/navigation.sh
+++ recipes/navigation.sh
@@ -1,5 +1,4 @@
-#NAVIGATION
-
-#Best recipe for navigation
-#For baseline to compare, see wandb run: #TODO
-./devops/skypilot/launch.py train run=$USER.navigation.bucketed.06-25  trainer.curriculum=env/mettagrid/curriculum/navigation/bucketed --skip-git-check --gpus=1 +trainer.env_overrides.game.num_agents=4 \
+#Navigation Recipe
+#We expect this recipe to achieve >88% on the navigation evals
+#For baseline to compare, see wandb run: https://wandb.ai/metta-research/metta/runs/daphne.navigation.low_reward.1gpu.4agents.06-25
+./devops/skypilot/launch.py train run=$USER.navigation.low_reward.baseline  trainer.curriculum=env/mettagrid/curriculum/navigation/low_reward --skip-git-check --gpus=1 +trainer.env_overrides.game.num_agents=4 \
--- recipes/object_use.sh
+++ recipes/object_use.sh
@@ -1,2 +1,5 @@
-
+#Object Use Recipe
+#For baseline to compare, see wandb run: https://wandb.ai/metta-research/metta/runs/daphne.object_use.06-25?nw=nwuserdaphned
+#We expect this policy to achieve ~60% on the object use evals, in particular it should learn the full mine-generator-altar sequence,
+#but it may not learn lasery/shooting/any combat related objects
 ./devops/skypilot/launch.py train run=$USER.object_use.baseline  trainer.curriculum=env/mettagrid/curriculum/object_use --gpus=1 --skip-git-check \
