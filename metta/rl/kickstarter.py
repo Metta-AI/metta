1524bf526311e826862244f5111d994de973e4c9
Nishad Singh <nishad@stem.ai>
2025-06-30 12:13:22 -0700

    add back default values

--- metta/rl/kickstarter.py
+++ metta/rl/kickstarter.py
@@ -1,7 +1,7 @@
 from typing import List
 
 import torch
-from pydantic import BaseModel, Field
+from pydantic import Field
 from torch import Tensor, nn
 
 from metta.agent.policy_state import PolicyState
@@ -11,16 +11,16 @@ from metta.common.util.typed_config import BaseModelWithForbidExtra
 
 class KickstartTeacherConfig(BaseModelWithForbidExtra):
     teacher_uri: str
-    action_loss_coef: float = Field(ge=0)
-    value_loss_coef: float = Field(ge=0)
+    action_loss_coef: float = Field(default=1, ge=0)
+    value_loss_coef: float = Field(default=1, ge=0)
 
 
-class KickstartConfig(BaseModel):
-    teacher_uri: str | None
-    action_loss_coef: float = Field(ge=0)
-    value_loss_coef: float = Field(ge=0)
-    anneal_ratio: float = Field(ge=0, le=1.0)
-    kickstart_steps: int = Field(gt=0)
+class KickstartConfig(BaseModelWithForbidExtra):
+    teacher_uri: str | None = None
+    action_loss_coef: float = Field(default=1, ge=0)
+    value_loss_coef: float = Field(default=1, ge=0)
+    anneal_ratio: float = Field(default=0.65, ge=0, le=1.0)
+    kickstart_steps: int = Field(default=1_000_000_000, gt=0)
     additional_teachers: list[KickstartTeacherConfig] | None = None
 
 
