8c056df1d008a589cb317ec72f848977f9ba0b3b
Nishad Singh <nishad@stem.ai>
2025-07-02 23:13:57 -0700

    rm mention of unsupported argument being deprecated

--- devops/setup_dev.sh
+++ devops/setup_dev.sh
@@ -7,8 +7,9 @@ echo "This script has been replaced by the metta command. Setting up with defaul
 # Navigate to the repo root
 cd "$(dirname "$0")/.."
 
-# Run installer with --add-to-path flag for non-interactive setup
-./install.sh --add-to-path
+make clean
+
+./install.sh
 
 # Use the metta wrapper directly with full path (since PATH changes won't take effect until new shell)
 ./metta/setup/installer/bin/metta configure --profile=softmax && ./metta/setup/installer/bin/metta install
