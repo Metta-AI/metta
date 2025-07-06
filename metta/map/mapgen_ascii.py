f30b723f4e72b2d9d9e3a02434ec1b741dee6083
Vyacheslav Matyukhin <me@berekuk.ru>
2025-07-06 13:54:27 -0300

    MapGenAscii shorthand class

---
+++ metta/map/mapgen_ascii.py
@@ -0,0 +1,44 @@
+from metta.common.util.config import Config
+from metta.map.mapgen import MapGen
+
+
+class MapGenAsciiParams(Config):
+    uri: str
+    border_width: int = 0
+
+
+class MapGenAscii(MapGen):
+    """
+    Shortcut for MapGen with ASCII scene.
+
+    It allows using this syntax:
+
+    ```yaml
+    game:
+      map_builder:
+        _target_: metta.map.mapgen_ascii.MapGenAscii
+        uri: "path/to/my/map.map"
+    ```
+
+    Instead of:
+
+    ```yaml
+    game:
+      map_builder:
+        _target_: metta.map.mapgen.MapGen
+        border_width: 0
+        root:
+          type: metta.map.scenes.ascii.Ascii
+          params:
+            uri: "path/to/my/map.map"
+    ```
+
+    This class doesn't support any additional features. If you need more than just ASCII scene, use MapGen directly.
+    """
+
+    def __init__(self, **kwargs):
+        ascii_params = MapGenAsciiParams(**kwargs)
+        super().__init__(
+            border_width=ascii_params.border_width,
+            root={"type": "metta.map.scenes.ascii.Ascii", "params": {"uri": ascii_params.uri}},
+        )
