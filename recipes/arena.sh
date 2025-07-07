e6e31e8ac350087eda65c8645c5de98e22536224
David Bloomin <daveey@gmail.com>
2025-07-07 08:21:23 -0700

    cp

--- recipes/arena.sh
+++ recipes/arena.sh
@@ -2,6 +2,8 @@
 ./devops/skypilot/launch.py train \
 --gpus=4 \
 --nodes=8 \
-run=$USER.arena.baseline.8x4 \
+--no-spot \
+run=$USER.recipes.arena.8x4 \
 trainer.curriculum=/env/mettagrid/arena/basic_easy_shaped \
-trainer.simulation.evaluate_interval=50
+trainer.simulation.evaluate_interval=50 \
+
