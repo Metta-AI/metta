8bf3ab0bb7a1ea46862a9e622e387176e30d64f6
mstormbull <111159631+mstormbull@users.noreply.github.com>
2025-07-09 14:35:28 -0700

    path refine for recipe

--- recipes/navigation.sh
+++ recipes/navigation.sh
@@ -1,2 +1,2 @@
 #NAVIGATION
-./devops/skypilot/launch.py train run=$USER.recipes.navigation  trainer.curriculum=env/mettagrid/curriculum/navigation --gpus=4 --skip-git-check \
+./devops/skypilot/launch.py train run=$USER.recipes.navigation  trainer.curriculum=env/mettagrid/curriculum/navigation/low_reward --gpus=4 --skip-git-check \
