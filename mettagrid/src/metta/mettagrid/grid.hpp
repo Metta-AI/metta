02933062bf82854f3888e5bc58c99ca9a146dc33
Robb Walters <robb@2amlogic.com>
2025-07-09 14:49:41 -0700

    row/col height/width ordering

--- mettagrid/src/metta/mettagrid/grid.hpp
+++ mettagrid/src/metta/mettagrid/grid.hpp
@@ -21,7 +21,7 @@ public:
   GridType grid;
   vector<std::unique_ptr<GridObject>> objects;
 
-  inline Grid(GridCoord height, GridCoord width) : width(width), height(height) {
+  inline Grid(GridCoord height, GridCoord width) : height(height), width(width) {
     grid.resize(height, vector<vector<GridObjectId>>(width, vector<GridObjectId>(GridLayer::GridLayerCount, 0)));
 
     // 0 is reserved for empty space
