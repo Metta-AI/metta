2dc9fdc7cd29316640907b439a96549a72d05a46
mstormbull <111159631+mstormbull@users.noreply.github.com>
2025-07-09 16:21:21 -0700

    quick single node test

--- recipes/arena.sh
+++ recipes/arena.sh
@@ -1,7 +1,7 @@
 
 ./devops/skypilot/launch.py train \
---gpus=4 \
---nodes=8 \
+--gpus=1 \
+--nodes=1 \
 --no-spot \
 run=$USER.recipes.arena.8x4 \
 trainer.curriculum=/env/mettagrid/arena/basic_easy_shaped \
