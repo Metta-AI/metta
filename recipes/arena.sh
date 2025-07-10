fa6d951ae5fe01c4204be87b82cc0f46650f4472
mstormbull <111159631+mstormbull@users.noreply.github.com>
2025-07-09 17:26:20 -0700

    return recipes to baseline

--- recipes/arena.sh
+++ recipes/arena.sh
@@ -1,9 +1,9 @@
-
+#!/bin/bash
 ./devops/skypilot/launch.py train \
 --gpus=8 \
 --nodes=4 \
 --no-spot \
 run=$USER.recipes.arena.8x4 \
 trainer.curriculum=/env/mettagrid/arena/basic_easy_shaped \
-trainer.simulation.evaluate_interval=50 \
+trainer.simulation.evaluate_interval=50
 
--- recipes/navigation.sh
+++ recipes/navigation.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 #NAVIGATION
 ./devops/skypilot/launch.py train run=$USER.recipes.navigation  trainer.curriculum=env/mettagrid/curriculum/navigation/low_reward --gpus=4 --skip-git-check \
