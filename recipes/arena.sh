8f012cd90f86c4164907325735678bf81f5af288
mstormbull <111159631+mstormbull@users.noreply.github.com>
2025-07-10 11:15:34 -0700

    restore arena.sh to main branch version

--- recipes/arena.sh
+++ recipes/arena.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+
 ./devops/skypilot/launch.py train \
---gpus=8 \
---nodes=4 \
+--gpus=4 \
+--nodes=8 \
 --no-spot \
 run=$USER.recipes.arena.8x4.$(date +%m-%d) \
 trainer.curriculum=/env/mettagrid/arena/basic_easy_shaped \
