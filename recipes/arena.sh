b4b5d557e67e4b9eb9f277716e2ee76007484cf3
mstormbull <111159631+mstormbull@users.noreply.github.com>
2025-07-14 15:27:42 -0700

    amending downward for cap

--- recipes/arena.sh
+++ recipes/arena.sh
@@ -1,8 +1,8 @@
 
 ./devops/skypilot/launch.py train \
---gpus=4 \
---nodes=8 \
+--gpus=1 \
+--nodes=1 \
 --no-spot \
-run=$USER.recipes.arena.8x4.$(date +%m-%d) \
+run=$USER.recipes.arena.1x1.$(date +%m-%d) \
 trainer.curriculum=/env/mettagrid/arena/basic_easy_shaped \
 trainer.simulation.evaluate_interval=50 \
