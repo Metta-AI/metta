b56a616b0fb94ee36fc680c8c36f19057f28297d
Gregory Pylypovych <gregorypylypovych@Gregorys-MacBook-Pro-2.local>
2025-07-14 12:08:27 -0700

    test

--- recipes/arena.sh
+++ recipes/arena.sh
@@ -4,5 +4,6 @@
 --nodes=8 \
 --no-spot \
 run=$USER.recipes.arena.8x4.$(date +%m-%d) \
++USER=greg \
 trainer.curriculum=/env/mettagrid/arena/basic_easy_shaped \
 trainer.simulation.evaluate_interval=50 \
