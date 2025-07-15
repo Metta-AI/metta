459589e12aa34e31bb7aaccdf2eaa1db6ff82aa6
Gregory Pylypovych <gregorypylypovych@Gregorys-MBP-2.localdomain>
2025-07-15 14:07:24 -0700

    changing bash

--- recipes/navigation.sh
+++ recipes/navigation.sh
@@ -15,9 +15,9 @@
 
 
 ./devops/skypilot/launch.py train \
-run=$USER.navigation.winners_rewarded_every_10_steps.$(date +%m-%d) \
+run=$USER.navigation.winners_rewarded_every_10_steps_moregpus.$(date +%m-%d) \
 trainer.curriculum=env/mettagrid/curriculum/navigation/prioritize_regressed \
---gpus=1 \
+--gpus=4 \
 +USER=greg \
 +trainer.env_overrides.game.num_agents=4 \
 sim=navigation \
