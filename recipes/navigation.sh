5c14e6118e8bfa329441529381e1f55c8e181059
Gregory Pylypovych <gregorypylypovych@Gregorys-MBP-2.localdomain>
2025-07-15 12:48:18 -0700

    bash script

--- recipes/navigation.sh
+++ recipes/navigation.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-#Navigation Recipe
-#We expect this recipe to achieve >88% on the navigation evals
-#For baseline to compare, see wandb run: https://wandb.ai/metta-research/metta/runs/daphne.navigation.low_reward.1gpu.4agents.06-25
+# #Navigation Recipe
+# #We expect this recipe to achieve >88% on the navigation evals
+# #For baseline to compare, see wandb run: https://wandb.ai/metta-research/metta/runs/daphne.navigation.low_reward.1gpu.4agents.06-25
+
+# ./devops/skypilot/launch.py train \
+# run=$USER.navigation.low_reward.baseline.$(date +%m-%d) \
+# trainer.curriculum=env/mettagrid/curriculum/navigation/prioritize_regressed \
+# --gpus=1 \
+# +USER=greg \
+# +trainer.env_overrides.game.num_agents=4 \
+# sim=navigation \
+# "$@"
+
 
 ./devops/skypilot/launch.py train \
-run=$USER.navigation.low_reward.baseline.$(date +%m-%d) \
+run=$USER.navigation.winners_rewarded_every_10_steps.$(date +%m-%d) \
 trainer.curriculum=env/mettagrid/curriculum/navigation/prioritize_regressed \
 --gpus=1 \
 +USER=greg \
