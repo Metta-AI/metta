0b3c8455de5db45b6e08057f1f8129086134277e
monofuel <monofuel@japura.net>
2025-07-16 15:37:41 -0400

    check for output item

--- mettascope/src/worldmap.ts
+++ mettascope/src/worldmap.ts
@@ -2,7 +2,7 @@ import { Vec2f } from './vector_math.js'
 import { Grid } from './grid.js'
 import * as Common from './common.js'
 import { ui, state, ctx, setFollowSelection } from './common.js'
-import { getAttr, sendAction } from './replay.js'
+import { getAttr, sendAction, getObjectConfig } from './replay.js'
 import { PanelInfo } from './panels.js'
 import { onFrame, updateSelection } from './main.js'
 import { parseHtmlColor, find } from './htmlutils.js'
@@ -177,16 +177,21 @@ function drawObject(gridObject: any) {
 
     // Draw the item layer.
     if (hasInventory(gridObject)) {
-      if (typeName !== 'altar') {
-        // Default render case
-        ctx.drawSprite(state.replay.object_images[type][1], x * Common.TILE_SIZE, y * Common.TILE_SIZE)
-      } else {
-        // Special handling for heart altars to only show if it contains the output item
-        let hasHeart = gridObject['inv:heart'] > 0
+      // Only render the overlay if the inventory contains output resources
+      let objectConfig = getObjectConfig(gridObject)
+      let outputItemExists = false
 
-        if (hasHeart) {
-          ctx.drawSprite(state.replay.object_images[type][1], x * Common.TILE_SIZE, y * Common.TILE_SIZE)
-        }  // else don't render item if it only has a battery
+      if (objectConfig && objectConfig.output_resources) {
+        // Check if any output resources are in the inventory
+        for (let resource in objectConfig.output_resources) {
+          if (getAttr(gridObject, 'inv:' + resource) > 0) {
+            outputItemExists = true
+            break
+          }
+        }
+      }
+      if (outputItemExists) {
+        ctx.drawSprite(state.replay.object_images[type][1], x * Common.TILE_SIZE, y * Common.TILE_SIZE)
       }
     }
   }
