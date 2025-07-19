f0c5d1e9811d58b7a099591fd95d27c3e3ad46d7
Brandon Harvey <sandover@gmail.com>
2025-07-18 18:25:09 -0700

    cleanup commit

--- library/src/posts/actions/toggleStarAction.ts
+++ library/src/posts/actions/toggleStarAction.ts
@@ -12,7 +12,7 @@ const inputSchema = zfd.formData({
   paperId: zfd.text(z.string().min(1)),
 });
 
-export const toggleStarActionPrisma = actionClient
+export const toggleStarAction = actionClient
   .inputSchema(inputSchema)
   .action(async ({ parsedInput: input }) => {
     const session = await getSessionOrRedirect();
