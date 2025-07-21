ac70bee5686f4462c34f4e69a89eb4283d2c3bb1
Gregory Pylypovych <gregorypylypovych@Gregorys-MacBook-Pro-2.local>
2025-07-21 15:25:05 -0700

    new branch

---
+++ fun.sh
@@ -0,0 +1,25 @@
+./tools/play.py run=test-play \
++hardware=macbook cmd=play ++policy_uri=./train_dir/gregorypylypovych.navigation.winners_rewarded_every_10_steps.07-15/checkpoints/model_0000.pt \
+seed=0 \
+++replay_job.sim.env=env/mettagrid/navigation/training/cylinder_world \
+++replay_job.sim.env_overrides.game.max_steps=20
+
+# ./tools/play.py run=test-play \
+# +hardware=macbook cmd=play ++policy_uri=./train_dir/gregorypylypovych.navigation.winners_rewarded_every_10_steps.07-15/checkpoints/model_0000.pt \
+# seed=1 \
+# ++replay_job.sim.env=env/mettagrid/navigation/training/cylinder_world
+
+# ./tools/play.py run=test-play \
+# +hardware=macbook cmd=play ++policy_uri=./train_dir/gregorypylypovych.navigation.winners_rewarded_every_10_steps.07-15/checkpoints/model_0000.pt \
+# seed=2 \
+# ++replay_job.sim.env=env/mettagrid/navigation/training/cylinder_world
+
+# ./tools/play.py run=test-play \
+# +hardware=macbook cmd=play ++policy_uri=./train_dir/gregorypylypovych.navigation.winners_rewarded_every_10_steps.07-15/checkpoints/model_0000.pt \
+# seed=3 \
+# ++replay_job.sim.env=env/mettagrid/navigation/training/cylinder_world
+
+# ./tools/play.py run=test-play \
+# +hardware=macbook cmd=play ++policy_uri=./train_dir/gregorypylypovych.navigation.winners_rewarded_every_10_steps.07-15/checkpoints/model_0000.pt \
+# seed=4 \
+# ++replay_job.sim.env=env/mettagrid/navigation/training/cylinder_world
---
+++ recipes/navigation_local.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Local navigation training script for use with tools/train.py
+
+agent_cluster_type=positions_in_same_area
+num_rooms=4
+num_agents_per_room=4
+seed=0
+python3 tools/train.py \
+run=$USER.navigation.ffa_8x_red_minibatch_conc_reward_gamma_0.999_MASSIVE_${agent_cluster_type}_${num_rooms}rooms_of_${num_agents_per_room}_seed${seed}.$(date +%m-%d) \
+trainer.curriculum=env/mettagrid/curriculum/navigation/random \
++USER=greg \
+++trainer.env_overrides.special_reward_mode=ffa \
+++trainer.env_overrides.game.map_builder.room.agent_cluster_type=${agent_cluster_type} \
+++trainer.env_overrides.game.num_agents=$((num_agents_per_room * num_rooms)) \
+++trainer.env_overrides.game.map_builder.room.agents=${num_agents_per_room} \
+++trainer.env_overrides.game.map_builder.num_rooms=${num_rooms} \
+seed=${seed} \
+sim=navigation \
+"$@"
+# ++trainer.env_overrides.game.num_agents=16 \
+
+#no_clustering, right_next_to_each_other, positions_in_same_area
+
+
+# ++replay_job.sim.env_overrides.game.num_agents=8 \
+# ++replay_job.sim.env_overrides.sparse_reward_top_heart_winners_every_N_steps=false \
+# ++replay_job.sim.env_overrides.game.map_builder.room.agents=2 \
+# ++replay_job.sim.env_overrides.game.num_agents=4 \
