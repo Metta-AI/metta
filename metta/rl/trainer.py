2c690df7514cf151e91cb48c95b9d4c2ace9be41
Richard Higgins <richard@softmax.com>
2025-07-21 12:45:26 -0400

    reduce diff

--- metta/rl/trainer.py
+++ metta/rl/trainer.py
@@ -19,8 +19,6 @@ from metta.common.util.heartbeat import record_heartbeat
 from metta.common.util.system_monitor import SystemMonitor
 from metta.eval.eval_request_config import EvalRewardSummary
 from metta.eval.eval_service import evaluate_policy as eval_service_evaluate_policy
-
-# from metta.interface import HyperparameterScheduler as SimpleHyperparameterScheduler
 from metta.mettagrid.curriculum.util import curriculum_from_config_path
 from metta.mettagrid.mettagrid_env import MettaGridEnv, dtype_actions
 from metta.rl.experience import Experience
@@ -266,6 +264,8 @@ def _train(
                 losses=losses,
                 device=device,
             )
+
+            # Optimizer step
             optimizer.zero_grad()
             loss.backward()
 
@@ -617,10 +617,6 @@ def train(
                 # Update learning rate scheduler
                 if lr_scheduler is not None:
                     lr_scheduler.step()
-
-                # Update hyperparameter scheduler
-                # if hyperparameter_scheduler is not None:
-                #     hyperparameter_scheduler.step(state.agent_step)
 
         torch_profiler.on_epoch_end(state.epoch)
 
@@ -1010,15 +1006,6 @@ def create_training_components(
         memory_monitor.add(experience, name="Experience", track_attributes=True)
         memory_monitor.add(policy, name="Policy", track_attributes=False)
 
-    # Create hyperparameter scheduler
-    # Use the simple interface wrapper like run.py does
-    # hyperparameter_scheduler = SimpleHyperparameterScheduler(
-    #     optimizer=optimizer,
-    #     total_timesteps=trainer_cfg.total_timesteps,
-    #     learning_rate=trainer_cfg.optimizer.learning_rate,
-    #     ppo_config=trainer_cfg.ppo,
-    #     scheduler_config=trainer_cfg.hyperparameter_scheduler,
-    # )
     hyperparameter_scheduler = None  # Disabled for now
 
     # Return all components in the expected order
