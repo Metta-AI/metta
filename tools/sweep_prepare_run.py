d7c9a5fadd01ede2bc006b10715d267632b58ce8
Axel Kerbec <akerbec@umich.edu>
2025-07-21 10:59:58 -0700

    refactor(sweep): replace sweep_rollout.sh by Python version + write dist_cfg in run_dir

--- tools/sweep_prepare_run.py
+++ tools/sweep_prepare_run.py
@@ -32,10 +32,10 @@ def main(cfg: DictConfig | ListConfig) -> int:
     return 0
 
 
-def setup_next_run(cfg: DictConfig | ListConfig, logger: Logger) -> str:
+def setup_next_run(cfg: DictConfig | ListConfig, logger: Logger) -> tuple[str, str]:
     """
     Create a new run for an existing sweep.
-    Returns the run ID.
+    Returns the run ID and the dist_cfg_path.
     """
     # Load sweep metadata
     sweep_metadata = OmegaConf.load(os.path.join(cfg.sweep_dir, "metadata.yaml"))
@@ -52,6 +52,7 @@ def setup_next_run(cfg: DictConfig | ListConfig, logger: Logger) -> str:
 
     cfg.run = run_id  # Top-level for training scripts
     cfg.run_dir = run_dir  # Top-level for training scripts
+    dist_cfg_path = os.path.join(run_dir, "dist_cfg.yaml")
 
     def init_run():
         with WandbContext(cfg.wandb, cfg) as wandb_run:
@@ -92,7 +93,7 @@ def setup_next_run(cfg: DictConfig | ListConfig, logger: Logger) -> str:
             sweep_job_final = OmegaConf.to_container(sweep_job_copy, resolve=True)
             assert isinstance(sweep_job_final, dict), "sweep_job_final must be a dictionary"
 
-            #
+            # TODO: Use sweep_config_utils.py to save config into run_dir
             train_cfg_overrides = DictConfig(
                 {
                     **sweep_job_final,
@@ -108,13 +109,13 @@ def setup_next_run(cfg: DictConfig | ListConfig, logger: Logger) -> str:
             )
             OmegaConf.save(train_cfg_overrides, save_path)
 
-            os.makedirs(os.path.dirname(cfg.dist_cfg_path), exist_ok=True)
+            # We are now saving into the run directory directly
             OmegaConf.save(
                 {
                     "run": run_id,
                     "wandb_run_id": wandb_run_id,
                 },
-                cfg.dist_cfg_path,
+                dist_cfg_path,
             )
 
     # TODO: Why are we doing this in wandb.agent? What is WandB agent?
@@ -127,7 +128,7 @@ def setup_next_run(cfg: DictConfig | ListConfig, logger: Logger) -> str:
     )
 
     logger.info(f"Run created: {run_id}")
-    return run_id
+    return run_id, dist_cfg_path
 
 
 def validate_protein_suggestion(config: DictConfig, suggestion: dict):
