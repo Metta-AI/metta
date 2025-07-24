ec099a01378673df275401fb8d049f9e9a6379ce
mstormbull <111159631+mstormbull@users.noreply.github.com>
2025-07-24 11:20:36 -0700

    override in recipe

--- recipes/navigation_with_context.sh
+++ recipes/navigation_with_context.sh
@@ -12,8 +12,4 @@ trainer.curriculum=env/mettagrid/curriculum/navigation/prioritize_regressed \
 sim=navigation \
 agent=fast_with_context \
 ++agent.environmental_context.enabled=true \
-+trainer.batch_size=524288 \
-+trainer.minibatch_size=16384 \
-+trainer.forward_pass_minibatch_target_size=4096 \
-+trainer.num_workers=4 \
 "$@" 
