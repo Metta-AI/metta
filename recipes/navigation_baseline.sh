6cbbc01a10a2c4635a705babb487660b907f6437
mstormbull <111159631+mstormbull@users.noreply.github.com>
2025-07-23 19:10:20 -0700

    recipe refinement

--- recipes/navigation_baseline.sh
+++ recipes/navigation_baseline.sh
@@ -11,8 +11,4 @@ trainer.curriculum=env/mettagrid/curriculum/navigation/prioritize_regressed \
 +trainer.env_overrides.game.num_agents=4 \
 sim=navigation \
 agent=fast \
-+trainer.batch_size=524288 \
-+trainer.minibatch_size=16384 \
-+trainer.forward_pass_minibatch_target_size=4096 \
-+trainer.num_workers=4 \
 "$@" 
--- recipes/navigation_with_context.sh
+++ recipes/navigation_with_context.sh
@@ -11,9 +11,5 @@ trainer.curriculum=env/mettagrid/curriculum/navigation/prioritize_regressed \
 +trainer.env_overrides.game.num_agents=4 \
 sim=navigation \
 agent=fast_with_context \
-++agent.environmental_context.enabled=true \
-+trainer.batch_size=524288 \
-+trainer.minibatch_size=16384 \
-+trainer.forward_pass_minibatch_target_size=4096 \
-+trainer.num_workers=4 \
++agent.environmental_context.enabled=true \
 "$@" 
