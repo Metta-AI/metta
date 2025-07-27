e1c0ff71ec59cadc43c5678882012617f32b6622
Nishad <nishad@stem.ai>
2025-07-27 09:48:27 -0700

    rm reference to missing recipe_trainer config

--- recipes/nav_memory_sequence.sh
+++ recipes/nav_memory_sequence.sh
@@ -16,7 +16,6 @@
 #baseline to compare: https://wandb.ai/metta-research/metta/runs/daphne.moretime.nav_memory_sequence.navigation_finetuned.06-25?nw=nwuserdaphned
 ./devops/skypilot/launch.py train \
   run=$USER.nav_memory_sequence.navigation_finetuned.$(date +%m-%d) \
-  trainer=recipe_trainer \
   trainer.curriculum=env/mettagrid/curriculum/nav_memory_sequence \
   --gpus=1 \
   trainer.initial_policy.uri=wandb://run/daphne.navigation.low_reward.1gpu.4agents.06-25 \
