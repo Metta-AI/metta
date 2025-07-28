cba90d1e24a25096c00a52e09e67e679e1be44cc
monofuel <monofuel@japura.net>
2025-07-28 19:17:43 -0400

    use newer packages

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Metta development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-python.url = "github:cachix/nixpkgs-python";
   };
 
