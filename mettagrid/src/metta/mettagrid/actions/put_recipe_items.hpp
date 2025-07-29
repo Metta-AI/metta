4c2a571cf573e5c3f3a48c785d5ae094392eb624
Gregory Pylypovych <gregorypylypovych@Gregorys-MBP-2.localdomain>
2025-07-29 16:11:41 -0700

    no box

--- mettagrid/src/metta/mettagrid/actions/put_recipe_items.hpp
+++ mettagrid/src/metta/mettagrid/actions/put_recipe_items.hpp
@@ -33,14 +33,15 @@ protected:
     Converter* converter = dynamic_cast<Converter*>(_grid->object_at(target_loc));
     if (!converter) {
       if (_grid->is_empty(target_loc.r, target_loc.c) && blue_battery_item_ >= 0) {
-        if (actor->inventory.count(blue_battery_item_) && actor->inventory[blue_battery_item_] > 0) {
-          actor->update_inventory(blue_battery_item_, -1);
-          actor->how_long_blue_battery_held = 0;
-          Box* box = new Box(target_loc.r, target_loc.c, box_type_id_, box_type_name_, actor->id, actor->agent_id, blue_battery_item_);
-          _grid->add_object(box);
-          actor->stats.add("box.created", 1.0f);
-          return true;
-        }
+        // if (actor->inventory.count(blue_battery_item_) && actor->inventory[blue_battery_item_] > 0) {
+        //   actor->update_inventory(blue_battery_item_, -1);
+        //   actor->how_long_blue_battery_held = 0;
+        //   Box* box = new Box(target_loc.r, target_loc.c, box_type_id_, box_type_name_, actor->id, actor->agent_id, blue_battery_item_);
+        //   _grid->add_object(box);
+        //   actor->stats.add("box.created", 1.0f);
+        //   return true;
+        // }
+        return true;
       }
 
       return false;
