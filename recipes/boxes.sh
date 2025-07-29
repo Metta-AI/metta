68a2ad99b55c771b4e463d7b3ad15348674c4a2f
Gregory Pylypovych <gregorypylypovych@Gregorys-MBP-2.localdomain>
2025-07-29 16:25:45 -0700

    bash

--- recipes/boxes.sh
+++ recipes/boxes.sh
@@ -23,15 +23,14 @@
 #     sim=navigation \
 #     "$@"
 # done
-
 agent_cluster_type=positions_in_same_area
 num_rooms=4
 num_agents_per_room=1
 for seed in 0; do
   python3 tools/train.py \
     run=$USER.navigation.ffa_NAVIG_BOXES_${num_rooms}rooms_of_${num_agents_per_room}_seed${seed}.$(date +%m-%d) \
-    trainer.curriculum=env/mettagrid/curriculum/autocurricula/random \
-    ++trainer.env=env/mettagrid/autocurricula/defaults \
+    trainer.curriculum=env/mettagrid/curriculum/navigation/random \
+    ++trainer.env=env/mettagrid/navigation/training/defaults \
     +USER=greg \
     ++trainer.ppo.gamma=0.999 \
     ++trainer.minibatch_size=16384 \
@@ -46,3 +45,26 @@ for seed in 0; do
     sim=navigation \
     "$@"
 done
+
+# agent_cluster_type=positions_in_same_area
+# num_rooms=4
+# num_agents_per_room=1
+# for seed in 0; do
+#   python3 tools/train.py \
+#     run=$USER.navigation.ffa_NAVIG_BOXES_${num_rooms}rooms_of_${num_agents_per_room}_seed${seed}.$(date +%m-%d) \
+#     trainer.curriculum=env/mettagrid/curriculum/autocurricula/random \
+#     ++trainer.env=env/mettagrid/autocurricula/defaults \
+#     +USER=greg \
+#     ++trainer.ppo.gamma=0.999 \
+#     ++trainer.minibatch_size=16384 \
+#     ++trainer.env_overrides.special_reward_mode=ffa \
+#     ++trainer.env_overrides.game.num_agents=$((num_agents_per_room * num_rooms)) \
+#     ++trainer.env_overrides.game.map_builder.room.agents=${num_agents_per_room} \
+#     ++trainer.env_overrides.game.map_builder.num_rooms=${num_rooms} \
+#     ++trainer.checkpoint.checkpoint_interval=50 \
+#     ++trainer.checkpoint.wandb_checkpoint_interval=50 \
+#     ++trainer.simulation.evaluate_interval=50 \
+#     seed=${seed} \
+#     sim=navigation \
+#     "$@"
+# done
