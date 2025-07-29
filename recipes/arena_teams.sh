025952087247c5fba96bdb69953d035900d06592
mstormbull <111159631+mstormbull@users.noreply.github.com>
2025-07-29 14:57:01 -0700

    use teams structure to make reward sharing arena

---
+++ recipes/arena_teams.sh
@@ -0,0 +1,10 @@
+./devops/skypilot/launch.py train \
+  --gpus=4 \
+  --nodes=8 \
+  --no-spot \
+  run=$USER.recipes.arena_teams.8x4.$(date +%m-%d) \
+  trainer.curriculum=/env/mettagrid/curriculum/arena/teams \
+  trainer.optimizer.learning_rate=0.0045 \
+  trainer.optimizer.type=muon \
+  trainer.simulation.evaluate_interval=50 \
+  "$@"
