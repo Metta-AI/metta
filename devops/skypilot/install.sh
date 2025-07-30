25cf285f074b2af19a563c5363d099eda0790c68
Vyacheslav Matyukhin <me@berekuk.ru>
2025-07-29 21:24:25 -0300

    fix skypilot setup

--- devops/skypilot/install.sh
+++ devops/skypilot/install.sh
@@ -4,4 +4,10 @@ SERVER=https://skypilot-api.softmax-research.net
 
 echo "Logging in to Skypilot API server at $SERVER"
 
+echo "Skypilot might ask you to copy the token. What you need to do is:
+1. Copy the token in browser
+3. Press Ctrl+C once
+3. Paste it into the terminal
+"
+
 uv run sky api login -e "$SERVER"
