e384a0f621b4cd4c4e6d5442deba99f14da64230
Gregory Pylypovych <gregorypylypovych@Gregorys-MacBook-Pro-2.local>
2025-07-30 02:16:51 -0700

    bash

--- recipes/boxes.sh
+++ recipes/boxes.sh
@@ -27,10 +27,13 @@ agent_cluster_type=positions_in_same_area
 num_rooms=4
 num_agents_per_room=4
 for seed in 0; do
-   python3 tools/train.py \
-    run=$USER.navigation.ffa_NAV_BOXES_fullobs_${num_rooms}rooms_of_${num_agents_per_room}_seed${seed}.$(date +%m-%d) \
+   ./devops/skypilot/launch.py train \
+    run=$USER.navigation.ffa_NAV_BOXES_maxobs_${num_rooms}rooms_of_${num_agents_per_room}_seed${seed}.$(date +%m-%d) \
     trainer.curriculum=env/mettagrid/curriculum/autocurricula/random \
     ++trainer.env=env/mettagrid/autocurricula/defaults \
+    --gpus=1 \
+    --nodes=1 \
+    --no-spot \
     +USER=greg \
     ++trainer.ppo.gamma=0.999 \
     ++trainer.minibatch_size=16384 \
