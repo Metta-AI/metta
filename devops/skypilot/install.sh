2f756cb117ba19a90d40d44a259d4bbf5bc7dfde
Vyacheslav Matyukhin <me@berekuk.ru>
2025-07-29 21:32:32 -0300

    typo fix

--- devops/skypilot/install.sh
+++ devops/skypilot/install.sh
@@ -6,7 +6,7 @@ echo "Logging in to Skypilot API server at $SERVER"
 
 echo "Skypilot might ask you to copy the token. What you need to do is:
 1. Copy the token in browser
-3. Press Ctrl+C once
+2. Press Ctrl+C once
 3. Paste it into the terminal
 "
 
