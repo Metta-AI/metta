4373d6b9fa0606eb020d435b8160fbc77117d966
Vyacheslav Matyukhin <me@berekuk.ru>
2025-07-31 19:15:33 -0300

    terraform softmax-infra-oauth secret

---
+++ devops/tf/eks/secrets.tf
@@ -0,0 +1,18 @@
+data "aws_secretsmanager_secret" "oauth_secret" {
+  arn = var.oauth_secret_arn
+}
+
+data "aws_secretsmanager_secret_version" "oauth_secret_version" {
+  secret_id = data.aws_secretsmanager_secret.oauth_secret.id
+}
+
+resource "kubernetes_secret" "oauth_secret" {
+  for_each = var.oauth_secret_namespaces
+
+  metadata {
+    name      = "softmax-infra-oauth"
+    namespace = each.value
+  }
+
+  data = jsondecode(data.aws_secretsmanager_secret_version.oauth_secret_version.secret_string)
+}
--- devops/tf/eks/variables.tf
+++ devops/tf/eks/variables.tf
@@ -9,3 +9,12 @@ variable "cluster_name" {
 variable "cluster_version" {
   default = "1.32"
 }
+
+variable "oauth_secret_arn" {
+  default = "arn:aws:secretsmanager:us-east-1:751442549699:secret:softmax-infra-oauth-rLFTw6"
+}
+
+variable "oauth_secret_namespaces" {
+  default     = ["skypilot", "monitoring", "observatory"]
+  description = "The secret with google oauth credentials will be created in these namespaces"
+}
