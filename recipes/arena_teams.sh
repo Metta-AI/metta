d2e73a8f52839076da2b5c99d0fdc1ec996c291d
Krishna Kanth <krishnakanth@Krishnas-MacBook-Pro.local>
2025-07-31 10:48:00 -0700

    initialcommit

---
+++ recipes/arena_teams.sh
@@ -0,0 +1,10 @@
+./devops/skypilot/launch.py train \
+  --gpus=1 \
+  --nodes=1 \
+  --no-spot \
+  run=$USER.recipes.arena.8x4.$(date +%m-%d) \
+  trainer.curriculum=/env/mettagrid/curriculum/arena/learning_progress \
+  trainer.optimizer.learning_rate=0.0045 \
+  trainer.optimizer.type=muon \
+  trainer.simulation.evaluate_interval=50 \
+  "$@"
