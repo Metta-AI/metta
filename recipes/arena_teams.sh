4df626c3cf51c0bd5e3c127b1032253011a0c8a0
Krishna Kanth <krishnakanth@Krishnas-MBP.localdomain>
2025-08-01 09:56:07 -0700

    arenateams

--- recipes/arena_teams.sh
+++ recipes/arena_teams.sh
@@ -3,8 +3,8 @@
   --nodes=1 \
   --no-spot \
   run=$USER.recipes.arena.8x4.$(date +%m-%d) \
-  user = krishnakanth \
-  trainer.curriculum=/env/mettagrid/curriculum/arena/learning_progress \
+  user=krishnakanth \
+  trainer.curriculum=/env/mettagrid/curriculum/arena/teams \
   trainer.optimizer.learning_rate=0.0045 \
   trainer.optimizer.type=muon \
   trainer.simulation.evaluate_interval=50 \
