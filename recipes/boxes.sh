e69798709c8f4c02e4b5ec905e0c9eb7998cad6c
Gregory Pylypovych <gregorypylypovych@Gregorys-MacBook-Pro-2.local>
2025-08-01 15:56:23 -0700

    fix

--- recipes/boxes.sh
+++ recipes/boxes.sh
@@ -1,3 +1,25 @@
+USER=greg
+agent_cluster_type=positions_in_same_area
+num_rooms=4
+num_agents_per_room=4
+for seed in 0; do
+   python3 tools/train.py \
+    run=$USER.navigation.ffa_building_to_BOXES_v1 \
+    trainer.curriculum=env/mettagrid/curriculum/autocurricula/random \
+    ++trainer.ppo.gamma=0.999 \
+    ++trainer.minibatch_size=16384 \
+    ++trainer.env_overrides.special_reward_mode=ffa \
+    ++trainer.env_overrides.game.num_agents=$((num_agents_per_room * num_rooms)) \
+    ++trainer.env_overrides.game.map_builder.room.agents=${num_agents_per_room} \
+    ++trainer.env_overrides.game.map_builder.num_rooms=${num_rooms} \
+    ++trainer.checkpoint.checkpoint_interval=5 \
+    ++trainer.checkpoint.wandb_checkpoint_interval=5 \
+    ++trainer.simulation.evaluate_interval=5 \
+    seed=${seed} \
+    sim=navigation \
+    "$@"
+done
+
 # agent_cluster_type=positions_in_same_area
 # num_rooms=4
 # num_agents_per_room=4
@@ -45,33 +67,6 @@
 #     sim=navigation \
 #     "$@"
 # done
-
-USER=greg
-agent_cluster_type=positions_in_same_area
-num_rooms=4
-num_agents_per_room=4
-for seed in 0; do
-   python3 tools/train.py \
-    run=$USER.navigation.ffa_building_to_BOXES_v1 \
-    trainer.curriculum=env/mettagrid/curriculum/autocurricula/random \
-    ++trainer.env=env/mettagrid/autocurricula/defaults \
-    +USER=greg \
-    ++trainer.ppo.gamma=0.999 \
-    ++trainer.minibatch_size=16384 \
-    ++trainer.env_overrides.special_reward_mode=ffa \
-    ++trainer.env_overrides.game.num_agents=$((num_agents_per_room * num_rooms)) \
-    ++trainer.env_overrides.game.map_builder.room.agents=${num_agents_per_room} \
-    ++trainer.env_overrides.game.map_builder.num_rooms=${num_rooms} \
-    ++trainer.checkpoint.checkpoint_interval=5 \
-    ++trainer.checkpoint.wandb_checkpoint_interval=5 \
-    ++trainer.simulation.evaluate_interval=5 \
-    seed=${seed} \
-    sim=navigation \
-    "$@"
-done
-
-
-
 
 # agent_cluster_type=positions_in_same_area
 # num_rooms=4
