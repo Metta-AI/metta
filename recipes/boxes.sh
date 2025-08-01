f0122cb13d91669757f7009a12860d422af43b69
Gregory Pylypovych <gregorypylypovych@Gregorys-MacBook-Pro-2.local>
2025-08-01 15:21:01 -0700

    boxes and configs

--- recipes/boxes.sh
+++ recipes/boxes.sh
@@ -46,13 +46,13 @@
 #     "$@"
 # done
 
-
+USER=greg
 agent_cluster_type=positions_in_same_area
 num_rooms=4
 num_agents_per_room=4
 for seed in 0; do
    python3 tools/train.py \
-    run=$USER.navigation.ffa_NAV_BOXES_skypilot_${num_rooms}rooms_of_${num_agents_per_room}_seed${seed}.$(date +%m-%d) \
+    run=$USER.navigation.ffa_building_to_BOXES_v1 \
     trainer.curriculum=env/mettagrid/curriculum/autocurricula/random \
     ++trainer.env=env/mettagrid/autocurricula/defaults \
     +USER=greg \
@@ -62,12 +62,11 @@ for seed in 0; do
     ++trainer.env_overrides.game.num_agents=$((num_agents_per_room * num_rooms)) \
     ++trainer.env_overrides.game.map_builder.room.agents=${num_agents_per_room} \
     ++trainer.env_overrides.game.map_builder.num_rooms=${num_rooms} \
-    ++trainer.checkpoint.checkpoint_interval=50 \
-    ++trainer.checkpoint.wandb_checkpoint_interval=50 \
-    ++trainer.simulation.evaluate_interval=50 \
+    ++trainer.checkpoint.checkpoint_interval=5 \
+    ++trainer.checkpoint.wandb_checkpoint_interval=5 \
+    ++trainer.simulation.evaluate_interval=5 \
     seed=${seed} \
     sim=navigation \
-    --debug-env-features \
     "$@"
 done
 
