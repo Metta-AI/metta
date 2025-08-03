8a4984acf95025f836f61c0d1d47a46e7f272e7e
Gregory Pylypovych <gregorypylypovych@Gregorys-MacBook-Pro-2.local>
2025-08-02 22:51:40 -0700

    fixing autocurricula configs to use mapgen

--- recipes/boxes.sh
+++ recipes/boxes.sh
@@ -4,14 +4,11 @@ num_rooms=4
 num_agents_per_room=4
 for seed in 0; do
    python3 tools/train.py \
-    run=$USER.navigation.ffa_building_to_BOXES_v3 \
+    run=$USER.navigation.ffa_building_to_BOXES_v4 \
     trainer.curriculum=env/mettagrid/curriculum/autocurricula/random \
     ++trainer.ppo.gamma=0.999 \
     ++trainer.minibatch_size=16384 \
     ++trainer.env_overrides.special_reward_mode=ffa \
-    ++trainer.env_overrides.game.num_agents=$((num_agents_per_room * num_rooms)) \
-    ++trainer.env_overrides.game.map_builder.room.agents=${num_agents_per_room} \
-    ++trainer.env_overrides.game.map_builder.num_rooms=${num_rooms} \
     ++trainer.checkpoint.checkpoint_interval=5 \
     ++trainer.checkpoint.wandb_checkpoint_interval=5 \
     ++trainer.simulation.evaluate_interval=5 \
@@ -19,56 +16,10 @@ for seed in 0; do
     sim=navigation \
     "$@"
 done
-
-# agent_cluster_type=positions_in_same_area
-# num_rooms=4
-# num_agents_per_room=4
-# for seed in 0; do
-#   ./devops/skypilot/launch.py train \
-#     run=$USER.navigation.ffa_NAV_BOXES_${num_rooms}rooms_of_${num_agents_per_room}_seed${seed}.$(date +%m-%d) \
-#     trainer.curriculum=env/mettagrid/curriculum/autocurricula/random \
-#     ++trainer.env=env/mettagrid/autocurricula/defaults \
-#     --gpus=1 \
-#     --nodes=1 \
-#     --no-spot \
-#     +USER=greg \
-#     ++trainer.ppo.gamma=0.999 \
-#     ++trainer.minibatch_size=16384 \
-#     ++trainer.env_overrides.special_reward_mode=ffa \
-#     ++trainer.env_overrides.game.num_agents=$((num_agents_per_room * num_rooms)) \
-#     ++trainer.env_overrides.game.map_builder.room.agents=${num_agents_per_room} \
-#     ++trainer.env_overrides.game.map_builder.num_rooms=${num_rooms} \
-#     ++trainer.checkpoint.checkpoint_interval=50 \
-#     ++trainer.checkpoint.wandb_checkpoint_interval=50 \
-#     ++trainer.simulation.evaluate_interval=50 \
-#     seed=${seed} \
-#     sim=navigation \
-#     "$@"
-# done
-# agent_cluster_type=positions_in_same_area
-# num_rooms=4
-# num_agents_per_room=4
-# for seed in 0; do
-#    ./devops/skypilot/launch.py train \
-#     run=$USER.navigation.ffa_NAV_BOXES_skypilot_${num_rooms}rooms_of_${num_agents_per_room}_seed${seed}.$(date +%m-%d) \
-#     trainer.curriculum=env/mettagrid/curriculum/autocurricula/random \
-#     ++trainer.env=env/mettagrid/autocurricula/defaults \
-#     +USER=greg \
-#     ++trainer.ppo.gamma=0.999 \
-#     ++trainer.minibatch_size=16384 \
-#     ++trainer.env_overrides.special_reward_mode=ffa \
-#     ++trainer.env_overrides.game.num_agents=$((num_agents_per_room * num_rooms)) \
-#     ++trainer.env_overrides.game.map_builder.room.agents=${num_agents_per_room} \
-#     ++trainer.env_overrides.game.map_builder.num_rooms=${num_rooms} \
-#     ++trainer.checkpoint.checkpoint_interval=50 \
-#     ++trainer.checkpoint.wandb_checkpoint_interval=50 \
-#     ++trainer.simulation.evaluate_interval=50 \
-#     seed=${seed} \
-#     sim=navigation \
-#     "$@"
-# done
-
-# agent_cluster_type=positions_in_same_area
+# ++trainer.env_overrides.game.map_builder.root.params.rows=2 \
+#     ++trainer.env_overrides.game.map_builder.root.params.columns=2 \
+#     ++trainer.env_overrides.game.map_builder.root.children.0.scene.params.agents=${num_agents_per_room} \
+#agent_cluster_type=positions_in_same_area
 # num_rooms=4
 # num_agents_per_room=4
 # for seed in 0; do
@@ -94,25 +45,3 @@ done
 #     "$@"
 # done
 
-# agent_cluster_type=positions_in_same_area
-# num_rooms=4
-# num_agents_per_room=1
-# for seed in 0; do
-#   python3 tools/train.py \
-#     run=$USER.navigation.ffa_NAVIG_BOXES_${num_rooms}rooms_of_${num_agents_per_room}_seed${seed}.$(date +%m-%d) \
-#     trainer.curriculum=env/mettagrid/curriculum/autocurricula/random \
-#     ++trainer.env=env/mettagrid/autocurricula/defaults \
-#     +USER=greg \
-#     ++trainer.ppo.gamma=0.999 \
-#     ++trainer.minibatch_size=16384 \
-#     ++trainer.env_overrides.special_reward_mode=ffa \
-#     ++trainer.env_overrides.game.num_agents=$((num_agents_per_room * num_rooms)) \
-#     ++trainer.env_overrides.game.map_builder.room.agents=${num_agents_per_room} \
-#     ++trainer.env_overrides.game.map_builder.num_rooms=${num_rooms} \
-#     ++trainer.checkpoint.checkpoint_interval=50 \
-#     ++trainer.checkpoint.wandb_checkpoint_interval=50 \
-#     ++trainer.simulation.evaluate_interval=50 \
-#     seed=${seed} \
-#     sim=navigation \
-#     "$@"
-# done
