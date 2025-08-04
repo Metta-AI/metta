ba2b26705cbf466b6a596b83ff6968fb49e6941b
Gregory Pylypovych <gregorypylypovych@Gregorys-MacBook-Pro-2.local>
2025-08-04 15:04:07 -0700

    horizon 100 exp

--- recipes/boxes.sh
+++ recipes/boxes.sh
@@ -1,21 +1,45 @@
+# USER=greg
+# agent_cluster_type=positions_in_same_area
+# num_rooms=4
+# num_agents_per_room=4
+# for seed in 0; do
+#    python3 tools/train.py \
+#     run=$USER.navigation.ffa_BOXES_1000_regular \
+#     trainer.curriculum=env/mettagrid/curriculum/autocurricula/random \
+#     ++trainer.ppo.gamma=0.999 \
+#     ++trainer.minibatch_size=16384 \
+#     ++trainer.env_overrides.special_reward_mode=ffa \
+#     ++trainer.checkpoint.checkpoint_interval=100 \
+#     ++trainer.checkpoint.wandb_checkpoint_interval=100 \
+#     ++trainer.simulation.evaluate_interval=100 \
+#     seed=${seed} \
+#     sim=navigation \
+#     "$@"
+# done
+
+
 USER=greg
 agent_cluster_type=positions_in_same_area
 num_rooms=4
 num_agents_per_room=4
-for seed in 0; do
-   python3 tools/train.py \
-    run=$USER.navigation.ffa_building_to_BOXES_v6 \
+for seed in 0 1 2 3 4; do
+   ./devops/skypilot/launch.py train \
+    run=$USER.navigation.ffa_BOXES_100_regular \
     trainer.curriculum=env/mettagrid/curriculum/autocurricula/random \
-    ++trainer.ppo.gamma=0.999 \
+    --gpus=1 \
+    --nodes=1 \
+    --no-spot \
+    ++trainer.ppo.gamma=0.977 \
     ++trainer.minibatch_size=16384 \
     ++trainer.env_overrides.special_reward_mode=ffa \
-    ++trainer.checkpoint.checkpoint_interval=5 \
-    ++trainer.checkpoint.wandb_checkpoint_interval=5 \
-    ++trainer.simulation.evaluate_interval=5 \
+    ++trainer.checkpoint.checkpoint_interval=100 \
+    ++trainer.checkpoint.wandb_checkpoint_interval=100 \
+    ++trainer.simulation.evaluate_interval=100 \
     seed=${seed} \
     sim=navigation \
     "$@"
 done
+
 # ++trainer.env_overrides.game.map_builder.root.params.rows=2 \
 #     ++trainer.env_overrides.game.map_builder.root.params.columns=2 \
 #     ++trainer.env_overrides.game.map_builder.root.children.0.scene.params.agents=${num_agents_per_room} \
