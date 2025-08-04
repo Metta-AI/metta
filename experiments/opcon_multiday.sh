d1c7c1508d7976a245e8d481e80c03a3a86cb87e
George Deane <george@stema.ai>
2025-08-04 15:04:57 -0700

    training file multiday

---
+++ experiments/opcon_multiday.sh
@@ -0,0 +1,17 @@
+./devops/skypilot/launch.py train run=george.operantconditioning.multiday.1trial.08-04 sim=opcon_multiday trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/multiday_pairs --skip-git-check +trainer.env_overrides.num_trials=1 \
+
+./devops/skypilot/launch.py train run=george.operantconditioning.multiday.10trials.08-04 sim=opcon_multiday trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/multiday_pairs --skip-git-check +trainer.env_overrides.num_trials=10 \
+
+./devops/skypilot/launch.py train run=george.operantconditioning.multiday.20trials.08-04 sim=opcon_multiday trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/multiday_pairs --skip-git-check +trainer.env_overrides.num_trials=20 \
+
+./devops/skypilot/launch.py train run=george.operantconditioning.multiday.30trials.08-04 sim=opcon_multiday trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/multiday_pairs --skip-git-check +trainer.env_overrides.num_trials=30 \
+
+
+
+./devops/skypilot/launch.py train run=george.operantconditioning.cued.1trial.08-04 sim=opcon_cued trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/cued --skip-git-check +trainer.env_overrides.num_trials=1 \
+
+./devops/skypilot/launch.py train run=george.operantconditioning.cued.10trials.08-04 sim=opcon_cued trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/cued --skip-git-check +trainer.env_overrides.num_trials=10 \
+
+./devops/skypilot/launch.py train run=george.operantconditioning.cued.20trials.08-04 sim=opcon_cued trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/cued --skip-git-check +trainer.env_overrides.num_trials=20 \
+
+./devops/skypilot/launch.py train run=george.operantconditioning.cued.30trials.08-04 sim=opcon_cued trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/cued --skip-git-check +trainer.env_overrides.num_trials=30 \
