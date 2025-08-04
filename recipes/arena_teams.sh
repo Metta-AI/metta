9fb727e79a7df92e2f740c5e059ccba1cfa54465
Krishna Kanth <krishnakanth@Krishnas-MacBook-Pro.local>
2025-08-04 14:30:03 -0700

    arena_teams with random

--- recipes/arena_teams.sh
+++ recipes/arena_teams.sh
@@ -4,8 +4,10 @@
   --no-spot \
   run=$USER.recipes.arena.teams.no.rs.$(date +%m-%d) \
   +user=krishnakanth \
-  trainer.curriculum=/env/mettagrid/curriculum/arena/teams \
+  trainer.curriculum=/env/mettagrid/curriculum/arena/random \
   trainer.optimizer.learning_rate=0.0045 \
   trainer.optimizer.type=muon \
   trainer.simulation.evaluate_interval=50 \
   "$@"
+
+#trainer.curriculum=/env/mettagrid/curriculum/arena/teams \
