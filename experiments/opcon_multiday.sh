8767f6d63aad215e6703522f20a8bbca55a9bc31
daphnedemekas <daphnedemekas@gmail.com>
2025-08-04 22:52:01 -0700

    experiments

--- experiments/opcon_multiday.sh
+++ experiments/opcon_multiday.sh
@@ -1,17 +1,26 @@
-./devops/skypilot/launch.py train run=george.operantconditioning.multiday.1trial.08-04 sim=opcon_multiday trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/multiday_pairs --skip-git-check +trainer.env_overrides.num_trials=1 \
+./devops/skypilot/launch.py train run=george.operantconditioning.multiday_1m.1trial.08-04 trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/multiday_1m +trainer.env_overrides.num_trials=1 \
 
-./devops/skypilot/launch.py train run=george.operantconditioning.multiday.10trials.08-04 sim=opcon_multiday trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/multiday_pairs --skip-git-check +trainer.env_overrides.num_trials=10 \
+./devops/skypilot/launch.py train run=george.operantconditioning.multiday_1m.8trials.08-04 trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/multiday_1m --skip-git-check +trainer.env_overrides.num_trials=8 \
 
-./devops/skypilot/launch.py train run=george.operantconditioning.multiday.20trials.08-04 sim=opcon_multiday trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/multiday_pairs --skip-git-check +trainer.env_overrides.num_trials=20 \
-
-./devops/skypilot/launch.py train run=george.operantconditioning.multiday.30trials.08-04 sim=opcon_multiday trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/multiday_pairs --skip-git-check +trainer.env_overrides.num_trials=30 \
+# ./devops/skypilot/launch.py train run=george.operantconditioning.multiday_1m.15trials.08-04 trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/multiday_1m --skip-git-check +trainer.env_overrides.num_trials=15 \
 
 
+./devops/skypilot/launch.py train run=george.operantconditioning.multiday_2m.1trial.08-04 trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/multiday_2m --skip-git-check +trainer.env_overrides.num_trials=1 \
 
-./devops/skypilot/launch.py train run=george.operantconditioning.cued.1trial.08-04 sim=opcon_cued trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/cued --skip-git-check +trainer.env_overrides.num_trials=1 \
+./devops/skypilot/launch.py train run=george.operantconditioning.multiday_2m.8trials.08-04 trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/multiday_2m --skip-git-check +trainer.env_overrides.num_trials=8 \
 
-./devops/skypilot/launch.py train run=george.operantconditioning.cued.10trials.08-04 sim=opcon_cued trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/cued --skip-git-check +trainer.env_overrides.num_trials=10 \
+# ./devops/skypilot/launch.py train run=george.operantconditioning.multiday_2m.15trials.08-04 trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/multiday_2m --skip-git-check +trainer.env_overrides.num_trials=15 \
 
-./devops/skypilot/launch.py train run=george.operantconditioning.cued.20trials.08-04 sim=opcon_cued trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/cued --skip-git-check +trainer.env_overrides.num_trials=20 \
 
-./devops/skypilot/launch.py train run=george.operantconditioning.cued.30trials.08-04 sim=opcon_cued trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/cued --skip-git-check +trainer.env_overrides.num_trials=30 \
+./devops/skypilot/launch.py train run=george.operantconditioning.multiday_3m.1trial.08-04 trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/multiday_3m --skip-git-check +trainer.env_overrides.num_trials=1 \
+
+./devops/skypilot/launch.py train run=george.operantconditioning.multiday_3m.8trials.08-04 trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/multiday_3m --skip-git-check +trainer.env_overrides.num_trials=8 \
+
+# ./devops/skypilot/launch.py train run=george.operantconditioning.multiday_3m.15trials.08-04 trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/multiday_3m --skip-git-check +trainer.env_overrides.num_trials=15  \
+
+
+./devops/skypilot/launch.py train run=george.operantconditioning.cued.1trial.08-04 trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/cued --skip-git-check +trainer.env_overrides.num_trials=1 \
+
+./devops/skypilot/launch.py train run=george.operantconditioning.cued.8trials.08-04 trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/cued --skip-git-check +trainer.env_overrides.num_trials=8 \
+
+# ./devops/skypilot/launch.py train run=george.operantconditioning.cued.15trials.08-04 trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/cued --skip-git-check +trainer.env_overrides.num_trials=15 \
