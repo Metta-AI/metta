6d364118330cb3bda695d0bc3ba135a3e18660d4
daphnedemekas <daphnedemekas@gmail.com>
2025-08-06 11:47:23 -0700

    oc smoke test recipe

---
+++ recipes/operantconditioning_smoketest.sh
@@ -0,0 +1,5 @@
+
+./devops/skypilot/launch.py train \
+run=george.operantconditioning.smoke_test.08-06 \
+trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/smoke_test \
+sim=sim/operant_condition_smoketest \
