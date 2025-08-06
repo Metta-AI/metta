543aeef2cd684149c92d80925ff6a7f8722d0773
daphnedemekas <daphnedemekas@gmail.com>
2025-08-06 11:49:15 -0700

    update recipe

--- recipes/operantconditioning_smoketest.sh
+++ recipes/operantconditioning_smoketest.sh
@@ -2,5 +2,5 @@
 ./devops/skypilot/launch.py train \
 run=$USER.operantconditioning.smoke_test.$(date +%m-%d) \
 trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/smoke_test \
-sim=sim/operant_condition_smoketest \
+sim=operant_condition_smoketest \
 "$@"
