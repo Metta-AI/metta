63811bc004ddf230e7d668e26b4ac2c2aed5af44
daphnedemekas <daphnedemekas@gmail.com>
2025-08-07 12:31:38 -0700

    update recipes

--- recipes/operant_conditioning.sh
+++ recipes/operant_conditioning.sh
@@ -5,7 +5,6 @@
 ./devops/skypilot/launch.py train \
   run=$USER.operant_conditioning.singletrial.smoke_test.$(date +%m-%d) \
   trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/2_converters \
-  --gpus=1 \
   +trainer.env_overrides.num_trials=1 \
   sim=operant_conditioning_multiday \
   "$@"
@@ -14,7 +13,6 @@
 ./devops/skypilot/launch.py train \
   run=$USER.operant_conditioning.2converters.$(date +%m-%d) \
   trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/2_converters \
-  --gpus=1 \
   trainer.bptt_horizon=256 \
   trainer.batch_size=2064384 \
   sim=operant_conditioning_multiday \
@@ -24,7 +22,6 @@
 ./devops/skypilot/launch.py train \
   run=$USER.operant_conditioning.3converters.$(date +%m-%d) \
   trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/3_converters \
-  --gpus=1 \
   trainer.bptt_horizon=256 \
   trainer.batch_size=2064384 \
   sim=operant_conditioning_multiday \
@@ -34,7 +31,6 @@
 ./devops/skypilot/launch.py train \
   run=$USER.operant_conditioning.4converters.$(date +%m-%d) \
   trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/4_converters \
-  --gpus=1 \
   trainer.bptt_horizon=256 \
   trainer.batch_size=2064384 \
   sim=operant_conditioning_multiday \
@@ -44,7 +40,6 @@
 ./devops/skypilot/launch.py train \
   run=$USER.operant_conditioning.all.$(date +%m-%d) \
   trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/all \
-  --gpus=1 \
   trainer.bptt_horizon=256 \
   trainer.batch_size=2064384 \
   sim=operant_conditioning_multiday \
