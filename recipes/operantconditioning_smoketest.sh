3f67fa90ca4c70073268746b15e04cdc56536ad1
daphnedemekas <daphnedemekas@gmail.com>
2025-08-07 11:29:05 -0700

    fix recipes

--- recipes/operantconditioning_smoketest.sh
+++ recipes/operantconditioning_smoketest.sh
@@ -1,27 +1,27 @@
 #2 converter smoke test
 ./devops/skypilot/launch.py train \
-run=$USER.operantconditioning.smoke_test.$(date +%m-%d) \
+run=$USER.operantconditioning.smoke_test.2_converters.$(date +%m-%d) \
 trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/smoke_tests/2_converters \
 sim=operant_condition_smoketest \
 "$@"
 
 #3 converter smoke test
 ./devops/skypilot/launch.py train \
-run=$USER.operantconditioning.smoke_test.$(date +%m-%d) \
+run=$USER.operantconditioning.smoke_test.3_converters.$(date +%m-%d) \
 trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/smoke_tests/3_converters \
 sim=operant_condition_smoketest \
 "$@"
 
 #4 converter smoke test
 ./devops/skypilot/launch.py train \
-run=$USER.operantconditioning.smoke_test.$(date +%m-%d) \
+run=$USER.operantconditioning.smoke_test.4_converters.$(date +%m-%d) \
 trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/smoke_tests/4_converters \
 sim=operant_condition_smoketest \
 "$@"
 
 #all converter smoke test
 ./devops/skypilot/launch.py train \
-run=$USER.operantconditioning.smoke_test.$(date +%m-%d) \
+run=$USER.operantconditioning.smoke_test.all_converters.$(date +%m-%d) \
 trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/smoke_tests/all \
 sim=operant_condition_smoketest \
 "$@"
