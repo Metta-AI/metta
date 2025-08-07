861df44d015ad4e51e4d440e6ec0fd099a833037
daphnedemekas <daphnedemekas@gmail.com>
2025-08-07 12:33:39 -0700

    fix recipes

--- recipes/operant_conditioning_singleepisode.sh
+++ recipes/operant_conditioning_singleepisode.sh
@@ -5,7 +5,7 @@
   run=$USER.operant_conditioning.singleepisode.2converters.128.$(date +%m-%d) \
   trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/2_converters \
   trainer.bptt_horizon=128 \
-  trainer.batch_size=516096 \
+  trainer.batch_size=1032192 \
   sim=operant_conditioning_singleepisode \
   "$@"
 
@@ -22,7 +22,7 @@
   run=$USER.operant_conditioning.singleepisode.3converters.128.$(date +%m-%d) \
   trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/3_converters \
   trainer.bptt_horizon=128 \
-  trainer.batch_size=516096 \
+  trainer.batch_size=1032192 \
   sim=operant_conditioning_singleepisode \
   "$@"
 
@@ -38,7 +38,7 @@
   run=$USER.operant_conditioning.singleepisode.4converters.128.$(date +%m-%d) \
   trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/4_converters \
   trainer.bptt_horizon=128 \
-  trainer.batch_size=516096 \
+  trainer.batch_size=1032192 \
   sim=operant_conditioning_singleepisode \
   "$@"
 
@@ -54,13 +54,13 @@
   run=$USER.operant_conditioning.singleepisode.all.128.$(date +%m-%d) \
   trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/all \
   trainer.bptt_horizon=128 \
-  trainer.batch_size=516096 \
+  trainer.batch_size=1032192 \
   sim=operant_conditioning_singleepisode \
-  +trainer.env_overrides.max_steps=512 \
   "$@"
 
 ./devops/skypilot/launch.py train \
   run=$USER.operant_conditioning.singleepisode.all.64.$(date +%m-%d) \
   trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/all \
   sim=operant_conditioning_singleepisode \
+  +trainer.env_overrides.max_steps=512 \
   "$@"
