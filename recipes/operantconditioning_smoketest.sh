ef779b9abf7a960a4731239d7d3d4b665327b69d
daphnedemekas <daphnedemekas@gmail.com>
2025-08-07 10:22:17 -0700

    make curricula per smoke test converter number

--- recipes/operantconditioning_smoketest.sh
+++ recipes/operantconditioning_smoketest.sh
@@ -1,6 +1,27 @@
-
+#2 converter smoke test
 ./devops/skypilot/launch.py train \
 run=$USER.operantconditioning.smoke_test.$(date +%m-%d) \
-trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/smoke_test \
+trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/smoke_tests/2_converters \
+sim=operant_condition_smoketest \
+"$@"
+
+#3 converter smoke test
+./devops/skypilot/launch.py train \
+run=$USER.operantconditioning.smoke_test.$(date +%m-%d) \
+trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/smoke_tests/3_converters \
+sim=operant_condition_smoketest \
+"$@"
+
+#4 converter smoke test
+./devops/skypilot/launch.py train \
+run=$USER.operantconditioning.smoke_test.$(date +%m-%d) \
+trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/smoke_tests/4_converters \
+sim=operant_condition_smoketest \
+"$@"
+
+#all converter smoke test
+./devops/skypilot/launch.py train \
+run=$USER.operantconditioning.smoke_test.$(date +%m-%d) \
+trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/smoke_tests/all \
 sim=operant_condition_smoketest \
 "$@"
