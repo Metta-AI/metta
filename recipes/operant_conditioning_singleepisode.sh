61283cabd85ef491d90e7aa2531666e755762a1f
daphnedemekas <daphnedemekas@gmail.com>
2025-08-07 11:56:10 -0700

    update cooldown

--- recipes/operant_conditioning_singleepisode.sh
+++ recipes/operant_conditioning_singleepisode.sh
@@ -6,14 +6,14 @@
   trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/2_converters \
   trainer.bptt_horizon=128 \
   trainer.batch_size=1032192 \
-  sim=operant_conditioning_multiday \
+  sim=operant_conditioning_singleepisode \
   "$@"
 
 #2 converters - 64 bptt
 ./devops/skypilot/launch.py train \
   run=$USER.operant_conditioning.singleepisode.2converters.64.$(date +%m-%d) \
   trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/2_converters \
-  sim=operant_conditioning_multiday \
+  sim=operant_conditioning_singleepisode \
   "$@"
 
 #3 converters
@@ -22,13 +22,13 @@
   trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/3_converters \
   trainer.bptt_horizon=128 \
   trainer.batch_size=1032192 \
-  sim=operant_conditioning_multiday \
+  sim=operant_conditioning_singleepisode \
   "$@"
 
 ./devops/skypilot/launch.py train \
   run=$USER.operant_conditioning.singleepisode.3converters.64.$(date +%m-%d) \
   trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/3_converters \
-  sim=operant_conditioning_multiday \
+  sim=operant_conditioning_singleepisode \
   "$@"
 
 #4 converters
@@ -37,13 +37,13 @@
   trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/4_converters \
   trainer.bptt_horizon=128 \
   trainer.batch_size=1032192 \
-  sim=operant_conditioning_multiday \
+  sim=operant_conditioning_singleepisode \
   "$@"
 
 ./devops/skypilot/launch.py train \
   run=$USER.operant_conditioning.singleepisode.4converters.64.$(date +%m-%d) \
   trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/4_converters \
-  sim=operant_conditioning_multiday \
+  sim=operant_conditioning_singleepisode \
   "$@"
 
 #all
@@ -52,11 +52,11 @@
   trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/all \
   trainer.bptt_horizon=128 \
   trainer.batch_size=1032192 \
-  sim=operant_conditioning_multiday \
+  sim=operant_conditioning_singleepisode \
   "$@"
 
 ./devops/skypilot/launch.py train \
   run=$USER.operant_conditioning.singleepisode.all.64.$(date +%m-%d) \
   trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/all \
-  sim=operant_conditioning_multiday \
+  sim=operant_conditioning_singleepisode \
   "$@"
