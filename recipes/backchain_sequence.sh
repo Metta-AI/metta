50897b6d069d300896c7da5b3595366d911a5a6e
daphnedemekas <daphnedemekas@gmail.com>
2025-08-08 16:27:38 -0700

    add additional backchain step

--- recipes/backchain_sequence.sh
+++ recipes/backchain_sequence.sh
@@ -1,5 +1,5 @@
 ./devops/skypilot/launch.py train \
-  run=$USER.backchain_sequence.bucketed.$(date +%m-%d) \
+  run=$USER.backchain_sequence.bucketed.6steps.$(date +%m-%d) \
   trainer.curriculum=env/mettagrid/curriculum/backchain_sequence \
   sim=backchain_sequence \
   "$@"
