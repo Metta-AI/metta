5e7f3798caddcab358e58b0952b6e2ce07e1aa24
daphnedemekas <daphnedemekas@gmail.com>
2025-08-08 16:24:52 -0700

    update recipe

--- recipes/backchain_sequence.sh
+++ recipes/backchain_sequence.sh
@@ -1,6 +1,5 @@
 ./devops/skypilot/launch.py train \
-  run=$USER.backchain_sequence.prioritized_regressed.$(date +%m-%d) \
+  run=$USER.backchain_sequence.bucketed.$(date +%m-%d) \
   trainer.curriculum=env/mettagrid/curriculum/backchain_sequence \
-  --gpus=1 \
   sim=backchain_sequence \
   "$@"
