3d4e135abc03e468f6d69959eccbd1f0c531e834
Gregory Pylypovych <gregorypylypovych@Mac.localdomain>
2025-08-11 16:58:00 -0700

    fixing tfn import

---
+++ mettagrid/src/metta/mettagrid/room/__init__.py
@@ -0,0 +1,6 @@
+from .multi_room import MultiRoom
+from .random import Random
+from .room import Room
+from .terrain_from_numpy import TerrainFromNumpy
+
+__all__ = ["TerrainFromNumpy", "Random", "MultiRoom", "Room"]
