9338cea2e92bce0c3e2135fb788d8df314a97aad
jacke <jacke@Jonathans-MacBook-Pro.local>
2025-08-12 23:27:09 -0700

    update navigation to use random seed

--- recipes/navigation.sh
+++ recipes/navigation.sh
@@ -4,11 +4,13 @@
 #We expect this recipe to achieve >88% on the navigation evals
 #For baseline to compare, see wandb run: https://wandb.ai/metta-research/metta/runs/daphne.navigation.low_reward.1gpu.4agents.06-25
 
+seed=$RANDOM
+
 ./devops/skypilot/launch.py train \
-  run=$USER.navigation.low_reward.baseline.seed27115.$(date +%m-%d) \
+  run=$USER.navigation.low_reward.baseline.seed$seed.$(date +%m-%d) \
   trainer.curriculum=env/mettagrid/curriculum/navigation/learning_progress \
   --gpus=1 \
   +trainer.env_overrides.game.num_agents=4 \
-  sim=navigation \
-  seed=27115 \
+  sim=navigation \ 
+  seed=$seed \
   "$@"
