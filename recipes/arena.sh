aaeb0ea6b372df5719408c5ccf5a755987be1088
pygma-tau <yudhister@softmax.com>
2025-08-14 16:10:59 -0700

    change to arena recipe for baseline

--- recipes/arena.sh
+++ recipes/arena.sh
@@ -2,9 +2,10 @@
   --gpus=4 \
   --nodes=8 \
   --no-spot \
-  run=$USER.contrastive_arena_recipe_skypilot.$(date +%m-%d) \
+  run=$USER.contrastive_baseline_arena_recipe_skypilot.$(date +%m-%d) \
   trainer.curriculum=/env/mettagrid/curriculum/arena/learning_progress \
   trainer.optimizer.learning_rate=0.0045 \
   trainer.optimizer.type=muon \
   trainer.simulation.evaluate_interval=50 \
+  trainer.contrastive.enabled=false\
   "$@"
