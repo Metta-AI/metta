5151c168911cf8dadddc15924ab29d2015443d2c
pygma-tau <yudhister@softmax.com>
2025-08-14 23:30:34 -0700

    fixing typo in learning progress py

--- recipes/arena.sh
+++ recipes/arena.sh
@@ -2,11 +2,12 @@
   --gpus=4 \
   --nodes=8 \
   --no-spot \
-  run=$USER.contrastive_baseline_arena_recipe_skypilot.$(date +%m-%d) \
+  run=$USER.contrastive_baseline_arena_recipe_muon_on.$(date +%m-%d) \
   trainer.curriculum=/env/mettagrid/curriculum/arena/learning_progress \
   trainer.optimizer.learning_rate=0.0045 \
   trainer.optimizer.type=muon \
   trainer.simulation.evaluate_interval=50 \
   trainer.contrastive.enabled=false\
+  --skip-git-check \
   "$@"
 
