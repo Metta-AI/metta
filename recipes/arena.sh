2ce3287f227c101cfd92902427b1c3ea113a0b81
pygma-tau <yudhister@softmax.com>
2025-08-15 03:35:24 -0700

    contrastive sweep recipe

--- recipes/arena.sh
+++ recipes/arena.sh
@@ -1,13 +1,15 @@
-./devops/skypilot/launch.py train \
-  --gpus=4 \
-  --nodes=8 \
-  --no-spot \
-  run=$USER.contrastive_baseline_arena_recipe_muon_on.$(date +%m-%d) \
-  trainer.curriculum=/env/mettagrid/curriculum/arena/learning_progress \
-  trainer.optimizer.learning_rate=0.0045 \
-  trainer.optimizer.type=muon \
-  trainer.simulation.evaluate_interval=50 \
-  trainer.contrastive.enabled=false\
-  --skip-git-check \
-  "$@"
+for i in 1 2 3 4 5; do
+  seed=$(( $(od -An -N4 -t u4 /dev/urandom) % 1073741824 ))
+  ./devops/skypilot/launch.py train \
+    --gpus=4 \
+    --nodes=1 \
+    --no-spot \
+    run=$USER.arena_recipe_muon_on_1x4_no_contrastive.$(date +%m-%d-%H-%M).s${seed} \
+    ++trainer.curriculum=/env/mettagrid/curriculum/arena/learning_progress \
+    ++trainer.optimizer.learning_rate=0.0045 \
+    ++trainer.optimizer.type=muon \
+    ++trainer.simulation.evaluate_interval=50 \
+    ++trainer.contrastive.enabled=false\
+    "$@"
+done
 
