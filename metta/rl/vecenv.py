f5af102f3f67079b91f74a8c7406cc4951de7599
Richard Higgins <richard@softmax.com>
2025-08-15 17:48:59 -0700

    reduce diff

--- metta/rl/vecenv.py
+++ metta/rl/vecenv.py
@@ -47,15 +47,12 @@ def make_env_func(
     current_task_holder = [task]
     
     def on_episode_done(obs, rew, term, trunc, info):
-        # Complete the current task
         current_task = current_task_holder[0]
         current_task.complete(rew.mean())
         
-        # Get a new task and update the environment
         new_task = curriculum.get_task()
         env.set_env_config(new_task.get_env_cfg())
         
-        # Replace the task reference (old one can now be GC'd)
         current_task_holder[0] = new_task
 
     env.set_on_episode_done(on_episode_done)
