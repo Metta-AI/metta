a69bbcc713cd000c38b36fa0eae2ce46593b1a04
Vyacheslav Matyukhin <me@berekuk.ru>
2025-08-15 22:31:02 -0300

    fix --overrides

--- tools/run.py
+++ tools/run.py
@@ -6,11 +6,13 @@ import os
 import signal
 import sys
 import warnings
-from typing import Any, cast
+from typing import Any, NoReturn, cast
 
 from omegaconf import OmegaConf
-from pydantic import validate_call
+from pydantic import TypeAdapter, validate_call
+from typing_extensions import TypeVar
 
+from metta.common.util.config import Config
 from metta.common.util.logging_helpers import init_logging
 from metta.common.util.tool import Tool
 from metta.rl.system_config import seed_everything
@@ -35,6 +37,52 @@ def init_mettagrid_system_environment() -> None:
     warnings.filterwarnings("ignore", category=DeprecationWarning, module="pygame.pkgdata")
 
 
+T = TypeVar("T", bound=Tool)
+
+
+def apply_override(tool: T, key: str, value: Any) -> T:
+    key_path = key.split(".")
+
+    def fail(error: str) -> NoReturn:
+        raise ValueError(f"Override failed. Full config:\n {tool.yaml()}\nOverride {key} failed: {error}")
+
+    cfg: Config = tool
+    traversed_path: list[str] = []
+    for key_part in key_path[:-1]:
+        if not hasattr(cfg, key_part):
+            failed_path = ".".join(traversed_path + [key_part])
+            fail(f"key {failed_path} not found")
+
+        sub_cfg = getattr(cfg, key_part)
+        if not isinstance(sub_cfg, Config):
+            failed_path = ".".join(traversed_path + [key_part])
+            fail(f"key {failed_path} is not a Config object")
+
+        cfg = sub_cfg
+        traversed_path.append(key_part)
+
+    if not hasattr(cfg, key_path[-1]):
+        fail(f"key {key} not found")
+
+    cls = type(cfg)
+    field = cls.model_fields.get(key_path[-1])
+    if field is None:
+        fail(f"key {key} is not a valid field")
+
+    value = TypeAdapter(field.annotation).validate_python(value)
+    setattr(cfg, key_path[-1], value)
+
+    return tool
+
+
+def apply_overrides(tool: T, overrides: list[str]) -> T:
+    for override in overrides:
+        key, value = override.split("=")
+        apply_override(tool, key, value)
+
+    return tool
+
+
 def main():
     # Parse CLI arguments
     parser = argparse.ArgumentParser()
@@ -49,11 +97,10 @@ def main():
     # Exit on ctrl+c
     signal.signal(signal.SIGINT, lambda sig, frame: os._exit(0))
 
-    overrides_conf = OmegaConf.to_container(OmegaConf.from_cli(args.overrides or []))
-    assert isinstance(overrides_conf, dict)
-    overrides_conf = cast(dict[str, Any], overrides_conf)
+    make_tool_args = args.args or []
+    assert isinstance(make_tool_args, list)
 
-    args_conf = OmegaConf.to_container(OmegaConf.from_cli(args.args or []))
+    args_conf = OmegaConf.to_container(OmegaConf.from_cli(make_tool_args))
     assert isinstance(args_conf, dict)
     args_conf = cast(dict[str, Any], args_conf)
 
@@ -65,11 +112,19 @@ def main():
     if not isinstance(tool_cfg, Tool):
         raise ValueError(f"The result of running {args.make_tool_cfg_path} must be a ToolConfig, got {tool_cfg}")
 
+    tool_cfg = apply_overrides(tool_cfg, args.overrides)
+
+    logger.info(
+        f"Tool config produced by {args.make_tool_cfg_path}({', '.join(make_tool_args)}), "
+        + f"with overrides {', '.join(args.overrides)}:"
+        + "\n---------------------\n"
+        + str(tool_cfg.yaml())
+    )
+
     # Seed random number generators
     seed_everything(tool_cfg.system)
 
     # Run the tool from config
-    logger.info(f"Running tool config: {module_name}.{func_name}")
     result = tool_cfg.invoke()
 
     if result is not None:
