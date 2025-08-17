de980aa98780ebe538486abd39c47f22aa85b7cb
Vyacheslav Matyukhin <me@berekuk.ru>
2025-08-17 00:08:37 -0300

    cleanup legacy mapgen-related builders

--- metta/map/tools/view.py
+++ metta/map/tools/view.py
@@ -1,8 +1,6 @@
 import logging
 
 from metta.common.util.tool import Tool
-from metta.map.load_random import get_random_map_uri
-from metta.map.tools.gen import uri_is_file
 from metta.map.utils.show import ShowMode, show_map
 from metta.map.utils.storable_map import StorableMap
 
@@ -15,13 +13,6 @@ class ViewTool(Tool):
 
     def invoke(self):
         uri = self.uri
-
-        if not uri_is_file(uri):
-            # probably a directory
-            while uri.endswith("/"):
-                uri = uri[:-1]
-            logger.info(f"Loading random map from directory {uri}")
-            uri = get_random_map_uri(uri)
 
         logger.info(f"Loading map from {uri}")
         storable_map = StorableMap.from_uri(uri)
