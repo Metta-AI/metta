fd4089cd1570c3b7bef6de6fe4ebc3af090fdd92
Krishna Kanth <krishnakanth@Krishnas-MacBook-Pro.local>
2025-08-18 09:55:40 -0700

    coop rewardsharing small map

--- recipes/coop_rewardsharing.sh
+++ recipes/coop_rewardsharing.sh
@@ -2,7 +2,8 @@
   --gpus=4 \
   --nodes=8 \
   --no-spot \
-  run=$USER.recipes.coop_rewardsharing.8x4.$(date +%m-%d) \
+  --skip-git-check \
+  run=$USER.recipes.coop_rewardsharing_v2.8x4.$(date +%m-%d_%H-%M-%S) \
   trainer.curriculum=/env/mettagrid/curriculum/coop_rewardsharing/learning_progress \
   trainer.optimizer.learning_rate=0.0045 \
   trainer.optimizer.type=muon \
