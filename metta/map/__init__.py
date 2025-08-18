70deaba1e1a3d204ba8fa5bfd47139ec9904c46f
David Bloomin <daveey@gmail.com>
2025-08-18 14:28:31 -0700

    cp

--- metta/map/__init__.py
+++ metta/map/__init__.py
@@ -1,1 +1,26 @@
 # metta/map/__init__.py
+
+"""Map builder module for MettaGrid."""
+
+from typing import Annotated, Union
+
+from pydantic import Field
+
+from metta.map.mapgen import MapGenConfig
+from metta.map.terrain_from_numpy import TerrainFromNumpyConfig
+from metta.mettagrid.map_builder.ascii import AsciiMapBuilderConfig
+
+# Define the discriminated union here to avoid circular imports
+MapGenConfigUnion = Annotated[
+    Union[
+        Annotated[AsciiMapBuilderConfig, Field(discriminator="type")],
+        Annotated[MapGenConfig, Field(discriminator="type")],
+        Annotated[TerrainFromNumpyConfig, Field(discriminator="type")],
+    ],
+    Field(discriminator="type"),
+]
+
+# Rebuild the model now that MapGenConfigUnion is defined
+MapGenConfig.model_rebuild()
+
+__all__ = ["MapGenConfigUnion"]
