8499fd6246d4c2e557b184ccf6156c7bbd087f9a
daphnedemekas <daphnedemekas@gmail.com>
2025-08-18 14:55:13 -0700

    update recipes for breadcrumbing

--- recipes/operantconditioning_incontext.sh
+++ recipes/operantconditioning_incontext.sh
@@ -1,68 +1,36 @@
 
 
-#2 all chain lengths, no sinks
-# ./devops/skypilot/launch.py train \
-#   run=$USER.operant_conditioning.in_context_learning.all_chains_no_sinks.128.$(date +%m-%d) \
-#   trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/in_context_learning/all_chains_no_sinks \
-#   trainer.bptt_horizon=128 \
-#   trainer.batch_size=1032192 \
-#   sim=operant_conditioning_incontext \
-#   "$@"
 
 ./devops/skypilot/launch.py train \
-  run=$USER.operant_conditioning.in_context_learning.all_chains_no_sinks.1gpu.$(date +%m-%d) \
+  run=$USER.operant_conditioning.in_context_learning.all_chains_no_sinks.breadcrumb_reward.$(date +%m-%d) \
   trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/in_context_learning/all_chains_no_sinks \
   sim=operant_conditioning_incontext \
   trainer.bptt_horizon=256 \
   trainer.batch_size=2064384 \
-  # --gpus=4 \
-  # --nodes=4 \
   "$@"
 
-#
-# ./devops/skypilot/launch.py train \
-#   run=$USER.operant_conditioning.in_context_learning.01_sinks_chainlength4.128.$(date +%m-%d) \
-#   trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/in_context_learning/01_sinks_chainlength4 \
-#   trainer.bptt_horizon=128 \
-#   trainer.batch_size=1032192 \
-#   sim=operant_conditioning_incontext \
-#   "$@"
 
 ./devops/skypilot/launch.py train \
-  run=$USER.operant_conditioning.in_context_learning.all_chains_one_sink.1gpus.$(date +%m-%d) \
+  run=$USER.operant_conditioning.in_context_learning.all_chains_one_sink.breadcrumb_reward.$(date +%m-%d) \
   trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/in_context_learning/all_chains_one_sink \
   sim=operant_conditioning_incontext \
   trainer.bptt_horizon=256 \
   trainer.batch_size=2064384 \
-  # --gpus=4 \
-  # --nodes=4
   "$@"
 
 # 4 converters
 ./devops/skypilot/launch.py train \
-  run=$USER.operant_conditioning.in_context_learning.1gpu.all$(date +%m-%d) \
+  run=$USER.operant_conditioning.in_context_learning.breadcrumb_reward.all$(date +%m-%d) \
   trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/in_context_learning/all \
   trainer.bptt_horizon=256 \
   trainer.batch_size=2064384 \
-  # --gpus=4 \
-  # --nodes=4 \
   sim=operant_conditioning_incontext \
   "$@"
 
-# ./devops/skypilot/launch.py train \
-#   run=$USER.operant_conditioning.in_context_learning.chain_length_2.128.$(date +%m-%d) \
-#   trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/in_context_learning/chain_length_2/all \
-#   trainer.bptt_horizon=128 \
-#   trainer.batch_size=1032192 \
-#   sim=operant_conditioning_incontext \
-#   "$@"
-
 ./devops/skypilot/launch.py train \
-  run=$USER.operant_conditioning.in_context_learning.chains_2_3_allsinks.2gpu.$(date +%m-%d) \
+  run=$USER.operant_conditioning.in_context_learning.chains_2_3_allsinks.breadcrumb_reward.$(date +%m-%d) \
   trainer.curriculum=env/mettagrid/curriculum/operant_conditioning/in_context_learning/chains_2_3_allsinks \
   sim=operant_conditioning_incontext \
   trainer.bptt_horizon=256 \
   trainer.batch_size=2064384 \
-  # --gpus=4 \
-  # --nodes=4 \
   "$@"
