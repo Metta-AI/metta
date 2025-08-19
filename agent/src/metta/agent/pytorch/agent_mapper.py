c0185341369095b126c2880465bae69663372c98
David Bloomin <daveey@gmail.com>
2025-08-19 12:39:11 -0700

    Revert "Component Pytorch Policies w/o YAML (#2149)"
    This reverts commit 2492a920f0930fc95051bff3e443e6615c782875.

---
+++ agent/src/metta/agent/pytorch/agent_mapper.py
@@ -0,0 +1,14 @@
+from metta.agent.pytorch.example import Example
+from metta.agent.pytorch.fast import Fast
+from metta.agent.pytorch.latent_attn_med import LatentAttnMed
+from metta.agent.pytorch.latent_attn_small import LatentAttnSmall
+from metta.agent.pytorch.latent_attn_tiny import LatentAttnTiny
+
+# Map from agent type names to their pytorch implementations
+agent_classes = {
+    "example": Example,
+    "fast": Fast,
+    "latent_attn_small": LatentAttnSmall,
+    "latent_attn_med": LatentAttnMed,
+    "latent_attn_tiny": LatentAttnTiny,
+}
