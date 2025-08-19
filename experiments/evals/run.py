585a5c54712243417499b2d1e5fa16078d47dd35
David Bloomin <daveey@gmail.com>
2025-08-19 13:48:36 -0700

    cp

--- experiments/evals/run.py
+++ experiments/evals/run.py
@@ -1,6 +1,4 @@
 from experiments.evals.registry import get_eval_suite
-from metta.common.util.constants import SOFTMAX_S3_BASE
-from metta.rl.system_config import SystemConfig
 from metta.tools.sim import SimTool
 
 
@@ -11,9 +9,4 @@ def eval(policy_uri: str, sim_suite: str) -> SimTool:
     return SimTool(
         simulations=simulations,
         policy_uris=[policy_uri],
-        replay_dir=f"{SOFTMAX_S3_BASE}/replays",
-        system=SystemConfig(
-            device="cpu",
-            vectorization="serial",
-        ),
     )
