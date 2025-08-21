b9b686be9cb4dcce7455cf45f9cea299297e269b
Gregory Pylypovych <gregorypylypovych@Mac.localdomain>
2025-08-21 11:36:58 -0700

    adding helpers to icl

--- experiments/recipes/icl.py
+++ experiments/recipes/icl.py
@@ -1,4 +1,16 @@
 import numpy as np
+from typing import Optional
+import metta.cogworks.curriculum as cc
+from metta.cogworks.curriculum.curriculum import CurriculumConfig
+from metta.cogworks.curriculum.task_generator import ValueRange as vr
+from metta.mettagrid.map_builder.random import RandomMapBuilder
+from metta.mettagrid.mettagrid_config import EnvConfig
+from metta.rl.trainer_config import EvaluationConfig, TrainerConfig
+from metta.sim.simulation_config import SimulationConfig
+from metta.tools.play import PlayTool
+from metta.tools.replay import ReplayTool
+from metta.tools.sim import SimTool
+from metta.tools.train import TrainTool
 from metta.mettagrid.config import building
 
 CONVERTER_TYPES = {
@@ -104,3 +116,86 @@ class InContextResourceChain:
             self.game_objects[obj].cooldown = cooldown
 
         return self.game_objects, self.map_builder_objects
+
+
+def make_env() -> EnvConfig:
+    return InContextResourceChain(
+        resource_chain=[
+            "ore_red",
+            "ore_blue",
+            "ore_green",
+            "battery_red",
+            "battery_blue",
+            "battery_green",
+            "laser",
+            "blueprint",
+            "armor",
+        ],
+        num_sinks=1,
+    ).make_env()
+
+
+def make_curriculum(nav_env: Optional[EnvConfig] = None) -> CurriculumConfig:
+    nav_env = make_env()
+
+    # make a set of training tasks for navigation
+    dense_tasks = cc.bucketed(nav_env)
+
+    maps = ["terrain_maps_nohearts"]
+    for size in ["large", "medium", "small"]:
+        for terrain in ["balanced", "maze", "sparse", "dense", "cylinder-world"]:
+            maps.append(f"varied_terrain/{terrain}_{size}")
+
+    dense_tasks.add_bucket("game.map_builder.instance_map.dir", maps)
+    dense_tasks.add_bucket(
+        "game.map_builder.instance_map.objects.altar", [vr.vr(3, 50)]
+    )
+
+    sparse_nav_env = nav_env.model_copy()
+    sparse_nav_env.game.map_builder = RandomMapBuilder.Config(
+        agents=4,
+        objects={"altar": 10},
+    )
+    sparse_tasks = cc.bucketed(sparse_nav_env)
+    sparse_tasks.add_bucket("game.map_builder.width", [vr.vr(60, 120)])
+    sparse_tasks.add_bucket("game.map_builder.height", [vr.vr(60, 120)])
+    sparse_tasks.add_bucket("game.map_builder.objects.altar", [vr.vr(1, 10)])
+
+    nav_tasks = cc.merge([dense_tasks, sparse_tasks])
+
+    return nav_tasks.to_curriculum()
+
+
+def train(curriculum: Optional[CurriculumConfig] = None) -> TrainTool:
+    trainer_cfg = TrainerConfig(
+        curriculum=curriculum or make_curriculum(),
+        evaluation=EvaluationConfig(
+            simulations=make_env(),
+        ),
+    )
+
+    return TrainTool(trainer=trainer_cfg)
+
+
+def play(env: Optional[EnvConfig] = None) -> PlayTool:
+    eval_env = env or make_env()
+    return PlayTool(
+        sim=SimulationConfig(
+            env=eval_env,
+            name="navigation",
+        ),
+    )
+
+
+def replay(env: Optional[EnvConfig] = None) -> ReplayTool:
+    eval_env = env or make_env()
+    return ReplayTool(
+        sim=SimulationConfig(
+            env=eval_env,
+            name="navigation",
+        ),
+    )
+
+
+def eval() -> SimTool:
+    return SimTool(simulations=make_env())
