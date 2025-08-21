648575cf9c7e8978c74c648bf7ce1a0121f4a58b
Krishna Kanth <krishnakanth@Krishnas-MBP.localdomain>
2025-08-21 10:48:07 -0700

    skypilot run custom maps

--- recipes/coop_rewardsharing.sh
+++ recipes/coop_rewardsharing.sh
@@ -4,8 +4,8 @@
   --no-spot \
   --skip-git-check \
   run=$USER.recipes.coop_rewardsharing_bucket.8x4.$(date +%m-%d_%H-%M-%S) \
-  trainer.curriculum=/env/mettagrid/curriculum/coop_rewardsharing/combined \
-  sim=coop_rewardsharing_only \
+  trainer.curriculum=/env/mettagrid/curriculum/coop_rewardsharing/rewardsharing_buckets \
+  sim=coop_rewardsharing \
   trainer.optimizer.learning_rate=0.0045 \
   trainer.optimizer.type=muon \
   trainer.simulation.evaluate_interval=50 \
