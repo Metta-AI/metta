f1aa93ae6545df299863415441b0cf8df96ff57c
Vyacheslav Matyukhin <me@berekuk.ru>
2025-08-22 00:16:04 -0300

    nonsensitive

--- devops/tf/library/outputs.tf
+++ devops/tf/library/outputs.tf
@@ -2,10 +2,6 @@ output "postgres_endpoint" {
   value = aws_db_instance.postgres.endpoint
 }
 
-output "postgres_password" {
-  value = random_password.db.result
-}
-
 output "postgres_url" {
-  value = "postgresql://${aws_db_instance.postgres.username}:${random_password.db.result}@${aws_db_instance.postgres.endpoint}/${aws_db_instance.postgres.db_name}"
+  value = nonsensitive(local.postgres_url)
 }
