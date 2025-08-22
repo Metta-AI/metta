7f4f981b78b231a78e5898d12d901bbe91fb49ae
Vyacheslav Matyukhin <me@berekuk.ru>
2025-08-22 00:13:57 -0300

    non-sensitive db url

--- devops/tf/library/outputs.tf
+++ devops/tf/library/outputs.tf
@@ -3,11 +3,9 @@ output "postgres_endpoint" {
 }
 
 output "postgres_password" {
-  value     = random_password.db.result
-  sensitive = true
+  value = random_password.db.result
 }
 
 output "postgres_url" {
-  value     = "postgresql://${aws_db_instance.postgres.username}:${random_password.db.result}@${aws_db_instance.postgres.endpoint}/${aws_db_instance.postgres.db_name}"
-  sensitive = true
+  value = "postgresql://${aws_db_instance.postgres.username}:${random_password.db.result}@${aws_db_instance.postgres.endpoint}/${aws_db_instance.postgres.db_name}"
 }
