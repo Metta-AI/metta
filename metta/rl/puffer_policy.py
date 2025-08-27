5ed446daf466b8333501f0629202c380e02c1a33
Richard Higgins <richard@relh.net>
2025-08-27 16:51:35 -0700

    cooling: initialize_to_environment one-stop-shop for agent save/load state (#2365)
    [Asana
    Task](https://app.asana.com/1/1209016784099267/project/1210348820405981/task/1211167011481696)

--- metta/rl/puffer_policy.py
+++ metta/rl/puffer_policy.py
@@ -38,9 +38,7 @@ def _init_env() -> SimpleNamespace:
 
 
 def load_pytorch_policy(path: str, device: str = "cpu", pytorch_cfg: Optional[DictConfig] = None) -> MettaAgent:
-    """
-    Create or loads a PyTorch policy.
-    """
+    """Create or loads a PyTorch policy."""
     # TODO(richard): #dehydration - this is a hack to get the policy to work. We need to fix this.
     raise NotImplementedError("This is a hack to get the policy to work. We need to fix this.")
     # try:
