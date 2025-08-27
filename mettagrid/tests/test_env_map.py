d66792fa614ad202aafc485cfaf56df0436fdfb1
S. Alex Smith <sasmith@stem.ai>
2025-08-27 15:36:56 -0700

    rename some env_config

--- mettagrid/tests/test_env_map.py
+++ mettagrid/tests/test_env_map.py
@@ -3,8 +3,8 @@ from metta.mettagrid.mettagrid_config import MettaGridConfig
 
 
 def test_env_map():
-    env_config = MettaGridConfig.EmptyRoom(width=3, height=4, num_agents=1, border_width=1)
-    env = MettaGridGymEnv(env_config=env_config, render_mode="human")
+    config = MettaGridConfig.EmptyRoom(width=3, height=4, num_agents=1, border_width=1)
+    env = MettaGridGymEnv(env_config=config, render_mode="human")
 
     # The map dimensions should match the specified width/height
     assert env.map_width == 3
