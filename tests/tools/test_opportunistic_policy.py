cb6ff8453e999c696176fd4caf2ff94b24452e24
Richard Higgins <richard@softmax.com>
2025-08-27 18:23:18 -0700

    tests re-add

---
+++ tests/tools/test_opportunistic_policy.py
@@ -0,0 +1,27 @@
+# import pytest
+
+# from metta.mettagrid import dtype_actions, dtype_observations
+# from metta.mettagrid.mettagrid_config import MettaGridConfig
+# from metta.mettagrid.mettagrid_env import MettaGridEnv
+# from tools.renderer import OpportunisticPolicy
+
+# TODO: (richard) #dehydration
+# @pytest.fixture
+# def env_with_agent_and_resource():
+#     env_cfg = MettaGridConfig.EmptyRoom(num_agents=1)
+#     env = MettaGridEnv(env_cfg, render_mode="human")
+#     obs, _ = env.reset()
+#     assert obs.dtype == dtype_observations
+#     try:
+#         yield env
+#     finally:
+#         env.close()
+
+
+# def test_opportunistic_roam_path(env_with_agent_and_resource):
+#     env = env_with_agent_and_resource
+#     policy = OpportunisticPolicy(env)
+#     obs, _ = env.reset()
+#     actions = policy.predict(obs)
+#     assert actions.dtype == dtype_actions
+#     assert actions.shape[0] == env.num_agents
