00d4985e23cdc7fc4e894e5a5bbabddf03737591
Richard Higgins <richard@softmax.com>
2025-08-29 17:36:29 -0700

    no more cache

--- metta/rl/checkpoint_manager.py
+++ metta/rl/checkpoint_manager.py
@@ -105,11 +105,6 @@ class CheckpointManager:
         self.cache_size = cache_size
         self._cache = OrderedDict()
 
-    @staticmethod
-    def clear_cache():
-        """Clear the global cache used by all CheckpointManager instances."""
-        global _global_cache
-        _global_cache.clear()
 
     @staticmethod
     def load_from_uri(uri: str):
