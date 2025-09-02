c125f48ec9d47e77b86c038366a8971ae7924506
Robb Walters <robb@2amlogic.com>
2025-09-02 11:22:35 -0700

    mv system monitor

--- metta/core/monitoring.py
+++ metta/core/monitoring.py
@@ -25,6 +25,7 @@ def setup_monitoring(
     system_monitor = SystemMonitor(
         sampling_interval_sec=1.0,
         history_size=100,
+        logger=logger,
         auto_start=True,
         external_timer=timer,
     )
