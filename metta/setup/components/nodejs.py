5b1f751ca5cfc86c4e327f7839486fc514047d88
Zachary Fogg <me@zfo.gg>
2025-09-02 16:52:24 -0700

    change to trigger ci

--- metta/setup/components/nodejs.py
+++ metta/setup/components/nodejs.py
@@ -7,8 +7,6 @@ from metta.setup.components.base import SetupModule
 from metta.setup.registry import register_module
 from metta.setup.utils import info, warning
 
-print("change to trigger CI")
-
 
 @register_module
 class NodejsSetup(SetupModule):
