e5977017f4887fe4b7485b87f6bacf13faaff68f
Robb Walters <rjwalters@users.noreply.github.com>
2025-09-04 10:07:44 -0700

    fix: move profiling tests to mettagrid (#2487)
    Profiling was moved to mettagrid in #2466 - this moves the tests next to
    the code.
    
    I am also adding some defensive checks on logging setup back in.
    
    [Asana
    Task](https://app.asana.com/1/1209016784099267/project/1210348820405981/task/1211240936486651)

--- mettagrid/src/metta/mettagrid/profiling/system_monitor.py
+++ mettagrid/src/metta/mettagrid/profiling/system_monitor.py
@@ -28,6 +28,16 @@ class SystemMonitor:
         if log_level is None:
             self.logger.disabled = True
         else:
+            root_logger = logging.getLogger()
+            has_stream_handler = any(isinstance(h, logging.StreamHandler) for h in root_logger.handlers)
+            if not has_stream_handler:
+                handler = logging.StreamHandler()
+                handler.setLevel(logging.DEBUG)
+                self.logger.addHandler(handler)
+                self.logger.warning(
+                    "No handlers found on logger - added StreamHandler. This should only happen during testing."
+                )
+
             self.logger.setLevel(log_level)
 
         self.sampling_interval_sec = sampling_interval_sec
