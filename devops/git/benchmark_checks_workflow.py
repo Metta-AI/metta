2f490cf6e0a231ef822a7749961edbb092a394f4
Robb Walters <robb@2amlogic.com>
2025-09-04 17:21:31 -0700

    lint

--- devops/git/benchmark_checks_workflow.py
+++ devops/git/benchmark_checks_workflow.py
@@ -330,7 +330,7 @@ def summarize(results_by_branch: dict[str, dict[str, Any]]):
 
         for job_name in sorted_job_names:
             row = f"{job_name[:39]:<40}"
-            for branch, results in results_by_branch.items():
+            for _branch, results in results_by_branch.items():
                 job_times = [
                     d.job_durations.get(job_name) for d in results["detailed_timings"] if job_name in d.job_durations
                 ]
