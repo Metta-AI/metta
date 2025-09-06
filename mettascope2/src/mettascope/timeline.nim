77ecabc0395448c4d7235d0fc607a52dab562a54
Andre von Houck <starplant@gmail.com>
2025-09-05 21:56:22 -0700

    Add Fidget2 UI library. (#2482)
    [Asana
    Task](https://app.asana.com/1/1209016784099267/project/1210348820405981/task/1211274383757573)

--- mettascope2/src/mettascope/timeline.nim
+++ mettascope2/src/mettascope/timeline.nim
@@ -1,6 +1,6 @@
 import
   std/[strformat],
-  boxy, vmath, windy,
+  boxy, vmath, windy, fidget2/[hybridrender, common],
   common, panels, sim, actions, utils
 
 const
@@ -33,52 +33,6 @@ proc playControls*() =
     echo "step: ", step
   if window.buttonPressed[KeyRightBracket]:
     step += 1
-    step = clamp(step, 0, replay.maxSteps - 1)
-    stepFloat = step.float32
-    echo "step: ", step
-
-proc drawTimeline*(panel: Panel) =
-  playControls()
-
-  bxy.drawRect(
-    rect = Rect(
-      x: 0,
-      y: 0,
-      w: panel.rect.w.float32,
-      h: panel.rect.h.float32
-    ),
-    color = BgColor
-  )
-
-  # Draw the scrubber bg.
-  bxy.drawRect(
-    rect = Rect(
-      x: 16,
-      y: 32,
-      w: panel.rect.w.float32 - 32,
-      h: 16
-    ),
-    color = parseHtmlColor("#717171")
-  )
-
-  var progress = step.float32 / replay.maxSteps.float32
-
-  # Draw the progress bar.
-  bxy.drawRect(
-    rect = Rect(
-      x: 16,
-      y: 32,
-      w: (panel.rect.w.float32 - 32) * progress,
-      h: 16
-    ),
-    color = color(1, 1, 1, 1)
-  )
-
-  # Clicking on the timeline will set it.
-  panel.updateMouse()
-  if panel.hasMouse and window.buttonDown[MouseLeft]:
-    let progress = (window.boxyMouse.vec2.x - 16) / (panel.rect.w.float32 - 32)
-    step = int(progress * replay.maxSteps.float32)
     step = clamp(step, 0, replay.maxSteps - 1)
     stepFloat = step.float32
     echo "step: ", step
