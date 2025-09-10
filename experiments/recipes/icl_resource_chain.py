138ca3a9a50f06d75d98795efbd30f73722a549d
Daphne Demekas <daphnedemekas@Daphnes-MacBook-Pro-2.local>
2025-09-10 14:46:25 -0700

    add to wandb plots

--- experiments/recipes/icl_resource_chain.py
+++ experiments/recipes/icl_resource_chain.py
@@ -166,11 +166,13 @@ class ConverterChainTaskGenerator(TaskGenerator):
 
         total_objects = num_resources + num_sinks + 1
         perimeter = 2 * (width + height) - 8
-        avg_hop = perimeter // total_objects  # use this for cooldown
+        avg_hop = perimeter // total_objects #use this for cooldown
 
         # optimal reward estimates for the task, to be used in evaluation
         most_efficient_optimal_reward, least_efficient_optimal_reward = (
-            self._estimate_max_rewards(num_resources, num_sinks, max_steps, avg_hop)
+            self._estimate_max_rewards(
+                num_resources, num_sinks, max_steps, avg_hop
+            )
         )
 
         icl_env = self._make_env_cfg(
@@ -182,6 +184,7 @@ class ConverterChainTaskGenerator(TaskGenerator):
             max_steps=max_steps,
             rng=rng,
         )
+
 
         icl_env.game.reward_estimates = {
             "most_efficient_optimal_reward": most_efficient_optimal_reward,
@@ -317,7 +320,7 @@ def replay(env: Optional[MettaGridConfig] = None) -> ReplayTool:
             env=eval_env,
             name="in_context_resource_chain",
         ),
-        policy_uri="wandb://run/georgedeane.operant_conditioning.in_context_learning.all.0.1_progress_smoothing.08-19:v50",
+        policy_uri="wandb://run/georgedeane.operant_conditioning.in_context_learning.all.0.1.08-19:v50",
     )
 
 
