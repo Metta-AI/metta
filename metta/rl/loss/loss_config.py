80727f35bc798cc4c144de83d4393ce6d127d2b5
Tasha Pais <tasha@stem.ai>
2025-09-15 15:29:10 -0700

    added a config file for loss parameters to check whether loss works

--- metta/rl/loss/loss_config.py
+++ metta/rl/loss/loss_config.py
@@ -5,6 +5,7 @@ from pydantic import Field
 
 from metta.agent.metta_agent import PolicyAgent
 from metta.rl.checkpoint_manager import CheckpointManager
+from metta.rl.loss.contrastive_config import ContrastiveConfig
 from metta.rl.loss.ppo_config import PPOConfig
 from mettagrid.config import Config
 
@@ -15,11 +16,7 @@ class LossSchedule(Config):
 
 
 class LossConfig(Config):
-    loss_configs: Dict[str, Any] = Field(
-        default={
-            "ppo": PPOConfig(),
-        }
-    )
+    loss_configs: Dict[str, Any] = Field(default={"ppo": PPOConfig(), "contrastive": ContrastiveConfig()})
 
     def init_losses(
         self,
