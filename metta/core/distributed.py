417bd1f9be6b279d1fffd4566ee1783980bd67fd
Robb Walters <robb@2amlogic.com>
2025-09-16 15:44:12 -0700

    add torch utils

--- metta/core/distributed.py
+++ metta/core/distributed.py
@@ -2,6 +2,7 @@
 
 import logging
 import os
+from datetime import timedelta
 
 import torch
 
@@ -19,6 +20,50 @@ class TorchDistributedConfig(Config):
     distributed: bool
 
 
+def _init_process_group() -> bool:
+    world_size_str = os.environ.get("WORLD_SIZE") or os.environ.get("NUM_NODES") or "1"
+    world_size = int(world_size_str) if world_size_str.strip() else 1
+    if world_size <= 1:
+        return False
+    if torch.distributed.is_initialized():
+        return False
+
+    rank = int(os.environ.get("RANK", os.environ.get("NODE_INDEX", "0")))
+    torch.distributed.init_process_group(
+        backend="nccl",
+        timeout=timedelta(seconds=30),
+        init_method=os.environ.get("DIST_URL", "env://"),
+        world_size=world_size,
+        rank=rank,
+    )
+    return True
+
+
+def enable_nccl_watchdog():
+    """
+    Enable (and reset) the NCCL watchdog
+    """
+    # Enable watchdog
+    os.environ["NCCL_WATCHDOG_ENABLE"] = "1"
+    os.environ["NCCL_ASYNC_ERROR_HANDLING"] = "1"
+
+    # Setting these environment variables effectively resets the timer
+    # as NCCL will read them on the next collective operation
+
+    logger.warning("NCCL watchdog enabled!")
+
+
+def disable_nccl_watchdog():
+    """
+    Disable the NCCL watchdog.
+    """
+    # Disable watchdog
+    os.environ["NCCL_WATCHDOG_ENABLE"] = "0"
+    os.environ["NCCL_ASYNC_ERROR_HANDLING"] = "0"
+
+    logger.warning("NCCL watchdog disabled")
+
+
 def setup_torch_distributed(device: str) -> TorchDistributedConfig:
     assert not torch.distributed.is_initialized()
 
@@ -29,7 +74,7 @@ def setup_torch_distributed(device: str) -> TorchDistributedConfig:
     distributed = False
 
     if "LOCAL_RANK" in os.environ and device.startswith("cuda"):
-        torch.distributed.init_process_group(backend="nccl")
+        _init_process_group()
 
         torch.cuda.set_device(device)
         distributed = True
