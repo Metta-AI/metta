fd467f93e2c23aec2fca4a0938d168d40a1336d7
Daphne <45669133+daphnedemekas@users.noreply.github.com>
2025-09-18 14:33:22 -0700

    Update experiments/evals/icl_resource_chain.py
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

--- experiments/evals/icl_resource_chain.py
+++ experiments/evals/icl_resource_chain.py
@@ -40,7 +40,7 @@ def make_icl_resource_chain_eval_suite() -> list[SimulationConfig]:
     return [
         SimulationConfig(
             name="in_context_learning/2c_1s_small",
-            env=make_icl_resource_chain_eval_env(2, 2, "medium"),
+            env=make_icl_resource_chain_eval_env(2, 1, "medium"),
         ),
         SimulationConfig(
             name="in_context_learning/2c_2s_small",
