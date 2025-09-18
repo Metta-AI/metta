14487e0e941048ffbe1e7361847fcb1e438a4737
Daphne Demekas <daphnedemekas@Daphnes-MacBook-Pro-2.local>
2025-09-17 19:59:07 -0700

    small and medium

--- experiments/recipes/icl_resource_chain.py
+++ experiments/recipes/icl_resource_chain.py
@@ -287,7 +287,7 @@ def make_curriculum(
     task_generator_cfg = ConverterChainTaskGenerator.Config(
         chain_lengths=[2, 3, 4, 5],
         num_sinks=[0, 1, 2],
-        room_sizes=["small"],
+        room_sizes=["small","medium"],
     )
     if algorithm_config is None:
         algorithm_config = LearningProgressConfig(
