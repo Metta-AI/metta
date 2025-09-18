6db066e95863e6dd092c4f586f88f7f2b6c5256f
Daphne <45669133+daphnedemekas@users.noreply.github.com>
2025-09-17 23:38:11 -0700

    Update distributed.py

--- metta/core/distributed.py
+++ metta/core/distributed.py
@@ -2,7 +2,6 @@
 
 import logging
 import os
-from datetime import timedelta
 import torch
 
 from metta.mettagrid.config import Config
@@ -29,7 +28,7 @@ def setup_torch_distributed(device: str) -> TorchDistributedConfig:
     distributed = False
 
     if "LOCAL_RANK" in os.environ and device.startswith("cuda"):
-        torch.distributed.init_process_group(backend="nccl", timeout=timedelta(hours=1))
+        torch.distributed.init_process_group(backend="nccl")
 
         torch.cuda.set_device(device)
         distributed = True
