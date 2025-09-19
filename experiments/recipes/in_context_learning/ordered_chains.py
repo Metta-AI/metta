73a8c54e2e2b791e770487e4a95baffaab5c9b3f
Daphne Demekas <daphnedemekas@Daphnes-MacBook-Pro-2.local>
2025-09-19 12:01:48 -0700

    lin terror

--- experiments/recipes/in_context_learning/ordered_chains.py
+++ experiments/recipes/in_context_learning/ordered_chains.py
@@ -250,7 +250,7 @@ class ConverterChainTaskGenerator(TaskGenerator):
         }
 
         icl_env.label = (
-            f"{num_resources}resources_{num_sinks}sinks_{room_size}" + f"_terrain"
+            f"{num_resources}resources_{num_sinks}sinks_{room_size}" + "_terrain"
             if obstacle_type
             else "" + f"_{density}"
             if density
