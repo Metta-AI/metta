a2fa65d09427d89eca0d02e36745c1556a76a44f
Daphne Demekas <daphnedemekas@Daphnes-MacBook-Pro-2.local>
2025-09-19 11:53:38 -0700

    update permieter algoirthm to handle cases

--- experiments/recipes/in_context_learning/ordered_chains.py
+++ experiments/recipes/in_context_learning/ordered_chains.py
@@ -250,8 +250,7 @@ class ConverterChainTaskGenerator(TaskGenerator):
         }
 
         icl_env.label = (
-            f"{num_resources}resources_{num_sinks}sinks_{room_size}"
-            + f"_terrain"
+            f"{num_resources}resources_{num_sinks}sinks_{room_size}" + f"_terrain"
             if obstacle_type
             else "" + f"_{density}"
             if density
@@ -365,7 +364,7 @@ def make_curriculum(
 
 
 def train(
-    curriculum_style: str = "longer_chains_more_sinks", lp_params: LPParams = LPParams()
+    curriculum_style: str = "terrain ", lp_params: LPParams = LPParams()
 ) -> TrainTool:
     # Local import to avoid circular import at module load time
     from experiments.evals.in_context_learning.ordered_chains import (
@@ -439,9 +438,7 @@ def play(env: Optional[MettaGridConfig] = None) -> PlayTool:
 def replay(env: Optional[MettaGridConfig] = None) -> ReplayTool:
     eval_env = env or make_mettagrid()
     # Default to the research policy if none specified
-    default_policy_uri = (
-        "s3://softmax-public/policies/icl_resource_chain_terrain_PS0.05_EB0.15_NAT1000_RTR0.25.09-19/icl_resource_chain_terrain_PS0.05_EB0.15_NAT1000_RTR0.25.09-19:v960.pt"
-    )
+    default_policy_uri = "s3://softmax-public/policies/icl_resource_chain_terrain_PS0.05_EB0.15_NAT1000_RTR0.25.09-19/icl_resource_chain_terrain_PS0.05_EB0.15_NAT1000_RTR0.25.09-19:v960.pt"
     return ReplayTool(
         sim=SimulationConfig(
             env=eval_env,
