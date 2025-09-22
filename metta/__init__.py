03be1f8c9d927c6ca3d8fa6f31144c1439851e06
Richard Higgins <richard@relh.net>
2025-09-21 22:50:44 -0700

    Revert "Merge remote-tracking branch 'origin/richard-cu130' into richard-transformer"
    This reverts commit fad70ead876a4d18d8ced596169f4f4eaa293c99, reversing
    changes made to 569e11d61b52b9ce4cfbad9c429449afc61d138e.

--- metta/__init__.py
+++ metta/__init__.py
@@ -1,9 +1,2 @@
-"""Metta package initialization."""
-
-from pkgutil import extend_path
-
-from ._cuda_env import prune_conflicting_nvidia_paths
-
-prune_conflicting_nvidia_paths()
-
-__path__ = extend_path(__path__, __name__)
+# metta/__init__.py
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
--- sitecustomize.py
+++ sitecustomize.py
@@ -1,8 +1,4 @@
 import warnings
 
-from metta._cuda_env import prune_conflicting_nvidia_paths
-
 # Suppress Gym warnings about being unmaintained
 warnings.filterwarnings("ignore", message=".*Gym has been unmaintained.*", category=UserWarning)
-
-prune_conflicting_nvidia_paths()
