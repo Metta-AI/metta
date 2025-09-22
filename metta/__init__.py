a337f431d7e3a00d2eee04e1a498c269144517a8
Richard Higgins <richard@relh.net>
2025-09-21 21:39:03 -0700

    fix: prune conflicting CUDA libs during import

--- metta/__init__.py
+++ metta/__init__.py
@@ -1,2 +1,9 @@
-# metta/__init__.py
-__path__ = __import__("pkgutil").extend_path(__path__, __name__)
+"""Metta package initialization."""
+
+from pkgutil import extend_path
+
+from ._cuda_env import prune_conflicting_nvidia_paths
+
+prune_conflicting_nvidia_paths()
+
+__path__ = extend_path(__path__, __name__)
--- sitecustomize.py
+++ sitecustomize.py
@@ -1,4 +1,8 @@
 import warnings
 
+from metta._cuda_env import prune_conflicting_nvidia_paths
+
 # Suppress Gym warnings about being unmaintained
 warnings.filterwarnings("ignore", message=".*Gym has been unmaintained.*", category=UserWarning)
+
+prune_conflicting_nvidia_paths()
