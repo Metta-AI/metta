2eacc461e138c9cdd043709b7c2f357b119c6b2d
Richard Higgins <richard@softmax.com>
2025-09-24 15:47:55 -0700

    more speed

--- experiments/recipes/arena_basic_easy_shaped.py
+++ experiments/recipes/arena_basic_easy_shaped.py
@@ -110,7 +110,7 @@ def train(
     eval_simulations = make_evals()
     trainer_cfg = TrainerConfig(
         losses=LossConfig(),
-        batch_size=1_048_576,
+        batch_size=2_097_152,
         minibatch_size=8_192,
     )
 
