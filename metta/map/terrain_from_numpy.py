a6c333d61318d93a3287d82ad9a394786334f3b1
Daphne Demekas <daphnedemekas@Daphnes-MacBook-Pro-2.local>
2025-09-24 11:20:08 -0700

    shuffle

--- metta/map/terrain_from_numpy.py
+++ metta/map/terrain_from_numpy.py
@@ -184,7 +184,9 @@ class InContextLearningFromNumpy(TerrainFromNumpy):
         assert len(self.config.object_names) == len(converter_indices), (
             f"Mismatch between object names ({len(self.config.object_names)}) "
             f"and available positions ({len(converter_indices)})"
+
         )
+        self.config.rng.shuffle(self.config.object_names)
         for object, index in zip(self.config.object_names, converter_indices, strict=True):
             grid[tuple(index)] = object
 
