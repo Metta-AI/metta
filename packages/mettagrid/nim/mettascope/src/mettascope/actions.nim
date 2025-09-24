11265a4065902dc5bb806875c69c5248ccafb748
treeform <starplant@gmail.com>
2025-09-24 11:13:43 -0700

    Add more actions.

--- packages/mettagrid/nim/mettascope/src/mettascope/actions.nim
+++ packages/mettagrid/nim/mettascope/src/mettascope/actions.nim
@@ -2,8 +2,6 @@ import std/[random],
   windy, fidget2,
   common, replays
 
-# var
-#   actionsArray*: array[MapAgents, array[2, uint8]]
 type
   Orientation* = enum
     N = 0
@@ -11,26 +9,8 @@ type
     W = 2
     E = 3
 
-proc simStep*() =
-  # Random actions
-  discard
-  # for j, agent in env.agents:
-  #   if selection != agent:
-  #     var action = rand(0 .. 9)
-  #     var argument = 0
-  #     if action == 1: # mov
-  #       argument = rand(0 .. 1)
-  #     elif action == 2: # rotate
-  #       argument = rand(0 .. 3)
-  #     elif action == 7: # attack
-  #       argument = rand(0 .. 9)
-  #       #argument = 2
-  #     actionsArray[j] = [action.uint8, argument.uint8]
-  # env.nextStep(addr actionsArray)
-
 proc sendAction*(agentId, actionId, argument: int) =
-  echo replay.actionNames
-  echo "Sending action: ", agentId, " ", actionId, " ", argument
+  ## Send an action to the Python from the user.
   requestAction = true
   requestActionAgentId = agentId
   requestActionActionId = actionId
@@ -62,54 +42,19 @@ proc agentControls*() =
     elif window.buttonPressed[KeyA] or window.buttonPressed[KeyLeft]:
       sendAction(agent.agentId, moveId, W.int)
 
-  #   # Move
-  #   if window.buttonPressed[KeyE]:
-  #     actionsArray[agent.agentId] = [1, 0]
-  #     simStep()
-  #   elif window.buttonPressed[KeyQ]:
-  #     actionsArray[agent.agentId] = [1, 1]
-  #     simStep()
+    # Put items
+    if window.buttonPressed[KeyQ]:
+      sendAction(agent.agentId, putItemsId, 0)
 
-  #   # Use
-  #   if window.buttonPressed[KeyU]:
-  #     actionsArray[agent.agentId] = [3, 0]
-  #     simStep()
+    # Get items
+    if window.buttonPressed[KeyE]:
+      sendAction(agent.agentId, getItemsId, 0)
 
-  #   # Shield
-  #   if window.buttonPressed[KeyO]:
-  #     actionsArray[agent.agentId] = [6, 0]
-  #     simStep()
+    # Attack
+    if window.buttonPressed[KeyZ]:
+      # TODO: Get implementation attack selection ui.
+      sendAction(agent.agentId, attackId, 0)
 
-  #   # Swap
-  #   if window.buttonPressed[KeyP]:
-  #     actionsArray[agent.agentId] = [8, 0]
-  #     simStep()
-
-  #   # Attack
-  #   if window.buttonPressed[Key1]:
-  #     actionsArray[agent.agentId] = [4, 1]
-  #     simStep()
-  #   if window.buttonPressed[Key2]:
-  #     actionsArray[agent.agentId] = [4, 2]
-  #     simStep()
-  #   if window.buttonPressed[Key3]:
-  #     actionsArray[agent.agentId] = [4, 3]
-  #     simStep()
-  #   if window.buttonPressed[Key4]:
-  #     actionsArray[agent.agentId] = [4, 4]
-  #     simStep()
-  #   if window.buttonPressed[Key5]:
-  #     actionsArray[agent.agentId] = [4, 5]
-  #     simStep()
-  #   if window.buttonPressed[Key6]:
-  #     actionsArray[agent.agentId] = [4, 6]
-  #     simStep()
-  #   if window.buttonPressed[Key7]:
-  #     actionsArray[agent.agentId] = [4, 7]
-  #     simStep()
-  #   if window.buttonPressed[Key8]:
-  #     actionsArray[agent.agentId] = [4, 8]
-  #     simStep()
-  #   if window.buttonPressed[Key9]:
-  #     actionsArray[agent.agentId] = [4, 9]
-  #     simStep()
+    # Noop
+    if window.buttonPressed[KeyX]:
+      sendAction(agent.agentId, noopId, 0)
