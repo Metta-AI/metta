0982cc06acdbf73bdbb1fa41d2cf2c17fc16f4bf
Alex Vardakostas <avardakostas@gmail.com>
2025-09-24 18:07:33 -0700

    sp

--- metta/rl/trainer_config.py
+++ metta/rl/trainer_config.py
@@ -53,7 +53,7 @@ class TrainerConfig(Config):
     verbose: bool = True
 
     batch_size: int = Field(default=131072, gt=0)  # 524288
-    minibatch_size: int = Field(default=16384, gt=0)
+    minibatch_size: int = Field(default=4096, gt=0)  # 16384
     bptt_horizon: int = Field(default=64, gt=0)
     update_epochs: int = Field(default=1, gt=0)
     scale_batches_by_world_size: bool = False
