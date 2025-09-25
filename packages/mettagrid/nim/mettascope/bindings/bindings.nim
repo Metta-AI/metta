2f05bb5068742bfc0e8bc47b60f6239500cade16
treeform <starplant@gmail.com>
2025-09-25 15:10:53 -0700

    Fix replay looping.

--- packages/mettagrid/nim/mettascope/bindings/bindings.nim
+++ packages/mettagrid/nim/mettascope/bindings/bindings.nim
@@ -14,6 +14,7 @@ proc init(dataDir: string, replay: string): RenderResponse =
   try:
     #echo "Replay from python: ", replay
     echo "Data dir: ", dataDir
+    playMode = Realtime
     initFidget(
       figmaUrl = "https://www.figma.com/design/hHmLTy7slXTOej6opPqWpz/MetaScope-V2-Rig",
       windowTitle = "MetaScope V2",
