355486951135e328d3a9e3b26aad75f53508a96e
Daphne Demekas <daphnedemekas@Daphnes-MacBook-Pro-2.local>
2025-10-01 14:00:58 -0700

    remove daphne

--- experiments/recipes/cogs_v_clips/level_1.py
+++ experiments/recipes/cogs_v_clips/level_1.py
@@ -550,7 +550,7 @@ def experiment():
                 [
                     "./devops/skypilot/launch.py",
                     "experiments.recipes.cogs_v_clips.level_1.train",
-                    f"run=daphne.cogs_v_clips.level_1.{curriculum_style}_{architecture}.{time.strftime('%Y-%m-%d')}",
+                    f"run=cogs_v_clips.level_1.{curriculum_style}_{architecture}.{time.strftime('%Y-%m-%d')}",
                     f"curriculum_style={curriculum_style}",
                     f"architecture={architecture}",
                     "--gpus=4",
