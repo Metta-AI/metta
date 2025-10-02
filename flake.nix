6edc2a4abe74845a7a619da6d64f2938e6209a70
monofuel <monofuel@japura.net>
2025-10-02 17:01:49 -0400

    add libz to ld library path

--- flake.nix
+++ flake.nix
@@ -47,19 +47,20 @@
           curl
           udev
           libevdev
+          zlib
         ];
 
         shellHook = ''
           # Prevent uv from downloading its own Python
           export UV_PYTHON="${mettaPython}/bin/python3.11"
           # Clear PYTHONPATH to avoid conflicts
-          export PYTHONPATH=""
+          export PYTHONPATH=""s
 
           # Set LD_LIBRARY_PATH for bazel to run properly during uv sync
           export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
 
           # Provide X/GL shared libraries for mettascope2.
-          export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [ pkgs.xorg.libX11 pkgs.xorg.libXext pkgs.xorg.libXcursor pkgs.libGL pkgs.curl pkgs.udev pkgs.libevdev ]}:$LD_LIBRARY_PATH"
+          export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [ pkgs.xorg.libX11 pkgs.xorg.libXext pkgs.xorg.libXcursor pkgs.libGL pkgs.curl pkgs.udev pkgs.libevdev pkgs.zlib ]}:$LD_LIBRARY_PATH"
 
           # Use a writable cache for Emscripten.
           # Emscripten default cache points to the read-only nix store, which does not work.
@@ -67,7 +68,7 @@
           mkdir -p "$EM_CACHE"
 
           # Create and activate a virtual environment with uv
-          uv sync
+          #uv sync
           source .venv/bin/activate
 
           # Build frontend
