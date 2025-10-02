e0f8bb12666426d1fa25d2975f324146da4794fa
Richard Higgins <richard@softmax.com>
2025-10-01 23:01:16 -0400

    suppress

--- packages/pufferlib-core/src/pufferlib/pufferl.py
+++ packages/pufferlib-core/src/pufferlib/pufferl.py
@@ -660,7 +660,7 @@ class PuffeRL:
             self.last_stats = self.stats
 
         for metric, value in (self.stats or self.last_stats).items():
-            if metric.startswith("game/"):
+            if not (metric.startswith("agent/") or "heart" in metric):
                 continue
             try:  # Discard non-numeric values
                 int(value)
