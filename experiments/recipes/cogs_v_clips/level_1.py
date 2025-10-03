d274a8595149aa4156708a7746b642e6804ab58f
Harsh Bhatt <harshbhatt7585@gmail.com>
2025-10-03 16:41:16 +0530

    fix

--- experiments/recipes/cogs_v_clips/level_1.py
+++ experiments/recipes/cogs_v_clips/level_1.py
@@ -555,7 +555,7 @@ def experiment():
                     f"architecture={architecture}",
                     "--gpus=4",
                     "--heartbeat-timeout=3600",
-                    "--skip-git-check flag"
+                    "--skip-git-check"
                 ]
             )
         time.sleep(1)
