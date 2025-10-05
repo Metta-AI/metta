7c53129f111253d7d7cb810bad196c6d1cab143f
Paul Tsier <paul@elementanalytics.com>
2025-10-04 17:32:49 -0700

    Simplify

--- login/src/app/layout.tsx
+++ login/src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { SessionProvider } from "next-auth/react";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -7,11 +10,16 @@ export const metadata: Metadata = {
   description: "Authentication service for Softmax applications",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
+
   return (
     <html lang="en">
       <body className="antialiased">
