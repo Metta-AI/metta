650485fa10106feeb5a1c1e5a472099d130c05fd
Vyacheslav Matyukhin <me@berekuk.ru>
2025-10-06 02:55:53 -0300

    various MapViewer improvements (performance, debug/hover info, etc.)

--- gridworks/src/lib/draw/Drawer.ts
+++ gridworks/src/lib/draw/Drawer.ts
@@ -1,3 +1,4 @@
+import { MettaGrid } from "../MettaGrid";
 import { loadMettaTileSets, TILE_NAMES } from "./mettaTileSets";
 import { TileSetCollection } from "./TileSetCollection";
 
@@ -34,6 +35,36 @@ const objectDrawers: Record<string, ObjectDrawer> = {
 
 export const objectNames = Object.keys(objectDrawers);
 
+const BACKGROUND_COLOR = "#cfa970";
+
+function visibleRegion(ctx: CanvasRenderingContext2D, grid: MettaGrid) {
+  // Get the current transformation matrix
+  const transform = ctx.getTransform();
+
+  // Invert it to go from screen space to transformed space
+  const inverse = transform.invertSelf();
+
+  const canvas = ctx.canvas;
+
+  const transformPoint = (x: number, y: number) => {
+    return {
+      x: x * inverse.a + y * inverse.c + inverse.e,
+      y: x * inverse.b + y * inverse.d + inverse.f,
+    };
+  };
+
+  const { x: minX, y: minY } = transformPoint(0, 0);
+  const { x: maxX, y: maxY } = transformPoint(canvas.width, canvas.height);
+
+  return {
+    // -1 and +1 are just to be safe with off-by-one errors
+    minX: Math.max(0, Math.floor(minX) - 1),
+    minY: Math.max(0, Math.floor(minY) - 1),
+    maxX: Math.min(grid.width, Math.ceil(maxX) + 1),
+    maxY: Math.min(grid.height, Math.ceil(maxY) + 1),
+  };
+}
+
 export class Drawer {
   private constructor(public readonly tileSets: TileSetCollection) {}
 
@@ -56,6 +87,59 @@ export class Drawer {
     for (const layer of layers) {
       const bitmap = this.tileSets.bitmap(layer.tile, layer.modulate);
       ctx.drawImage(bitmap, x, y, size, size);
+    }
+  }
+
+  drawGrid(ctx: CanvasRenderingContext2D, grid: MettaGrid) {
+    // Only draw the visible region of the grid - helps performance on big maps when zoomed in
+    const { minX, minY, maxX, maxY } = visibleRegion(ctx, grid);
+
+    // Clear drawing area
+    ctx.fillStyle = BACKGROUND_COLOR;
+    ctx.fillRect(minX, minY, maxX - minX, maxY - minY);
+
+    // Draw the map
+    for (const object of grid.objects) {
+      if (
+        object.c < minX ||
+        object.c > maxX ||
+        object.r < minY ||
+        object.r > maxY
+      ) {
+        continue;
+      }
+      if (object.name === "wall") {
+        // in mettascope, walls have many different types depending on the surrounding terrain.
+        // until we support that, we just draw a single wall type, and highlight it with color.
+        ctx.fillStyle = "#c0bcb8";
+        ctx.fillRect(object.c, object.r, 1, 1);
+      }
+      this.drawObject(object.name, ctx, object.c, object.r, 1);
+    }
+
+    // Draw grid lines
+    {
+      ctx.strokeStyle = "black";
+      ctx.globalAlpha = 0.2;
+      ctx.lineCap = "square";
+      ctx.lineWidth = 0.02;
+
+      // Draw vertical grid lines
+      for (let x = minX; x <= maxX; x++) {
+        ctx.beginPath();
+        ctx.moveTo(x, minY);
+        ctx.lineTo(x, maxY);
+        ctx.stroke();
+      }
+
+      // Draw horizontal grid lines
+      for (let y = minY; y <= maxY; y++) {
+        ctx.beginPath();
+        ctx.moveTo(minX, y);
+        ctx.lineTo(maxX, y);
+        ctx.stroke();
+      }
+      ctx.globalAlpha = 1;
     }
   }
 }
