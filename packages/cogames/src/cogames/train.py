6fc75fb7a5839d4e30309877291ab5924b6c1c45
Richard Higgins <richard@softmax.com>
2025-10-06 22:12:24 -0400

    deal with tests

--- packages/cogames/src/cogames/train.py
+++ packages/cogames/src/cogames/train.py
@@ -84,6 +84,9 @@ def train(
     if remainder:
         vector_batch_size += envs_per_worker - remainder
 
+    if backend is pvector.Serial:
+        vector_batch_size = num_envs
+
     logger.debug(
         "Vec env config: num_envs=%s, num_workers=%s, batch_size=%s (envs/worker=%s)",
         num_envs,
