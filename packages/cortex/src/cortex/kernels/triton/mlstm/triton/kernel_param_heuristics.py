c3b17abe039d597a3cd2755c9049c388f6c78779
subho406 <subhojeetpramanik@gmail.com>
2025-10-08 13:10:42 -0700

    removed todo comment

--- packages/cortex/src/cortex/kernels/triton/mlstm/triton/kernel_param_heuristics.py
+++ packages/cortex/src/cortex/kernels/triton/mlstm/triton/kernel_param_heuristics.py
@@ -5,10 +5,4 @@ import triton
 
 
 def get_head_dim_block_size(head_dim: int, min_block_size: int = 64) -> int:
-    # TODO make proper tests, for when and where this check is necessary.
-    # For 160M model size, this check is not necessary.
-    # assert (
-    #     is_power_of_2(head_dim) or head_dim % min_block_size == 0
-    # ), f"head_dim must be a power of 2 or multiple of {min_block_size}. Got {head_dim}."
-
     return min(min_block_size, triton.next_power_of_2(head_dim))
