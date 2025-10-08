73ce45367e06ed8da7bd2afa0f46b1cced224d92
Morgan Martin <morganm5201@gmail.com>
2025-10-08 13:38:50 -0400

    refactor: remove 3 unused API routes (posts list, institutions list, institutions by name)

--- library/src/app/api/institutions/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-import { loadInstitutions } from "@/posts/data/institutions-server";
-import { withErrorHandler } from "@/lib/api/error-handler";
-
-export const GET = withErrorHandler(async () => {
-  const institutions = await loadInstitutions();
-  return NextResponse.json(institutions);
-});
