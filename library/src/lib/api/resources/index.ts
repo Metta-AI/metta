306c3b89b10b44b429b584a5fa899c7b96ec62ba
Morgan Martin <morganm5201@gmail.com>
2025-10-09 14:13:00 -0400

    fix: unignore library API resources directory to fix module resolution

---
+++ library/src/lib/api/resources/index.ts
@@ -0,0 +1,7 @@
+export * as institutions from "./institutions";
+export * as authors from "./authors";
+export * as posts from "./posts";
+export * as notifications from "./notifications";
+export * as papers from "./papers";
+export * as admin from "./admin";
+export * as mentions from "./mentions";
