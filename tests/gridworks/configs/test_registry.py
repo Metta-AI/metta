27d8f99727da06bcd7dc8e23ab625791bcba1f29
treeform <starplant@gmail.com>
2025-10-14 12:49:05 -0700

    fuck linters

--- tests/gridworks/configs/test_registry.py
+++ tests/gridworks/configs/test_registry.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from metta.gridworks.configs.registry import ConfigMakerRegistry
 
+
 @pytest.mark.skip(reason="Flaky test")
 def test_registry():
     registry = ConfigMakerRegistry(root_dirs=[Path("tests/gridworks/configs/fixtures")])
