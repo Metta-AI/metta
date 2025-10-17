99e806b5e2cc15a74642beaef406c8f845fd246c
mstormbull <111159631+mstormbull@users.noreply.github.com>
2025-10-17 10:15:07 -0700

    update exp

--- experiments/recipes/cogs_v_clips/level_1.py
+++ experiments/recipes/cogs_v_clips/level_1.py
@@ -632,10 +632,10 @@ def experiment():
                 [
                     "./devops/skypilot/launch.py",
                     "experiments.recipes.cogs_v_clips.level_1.train",
-                    f"run=cogs_v_clips.level_1.{curriculum_style}_{architecture}.eval_local.{time.strftime('%Y-%m-%d')}",
+                    f"run=msb_cogs_v_clips.level_1.{curriculum_style}_{architecture}.eval_local.{time.strftime('%Y-%m-%d')}",
                     f"curriculum_style={curriculum_style}",
                     f"architecture={architecture}",
-                    "--gpus=4",
+                    "--gpus=1",
                     "--heartbeat-timeout=3600",
                 ]
             )
