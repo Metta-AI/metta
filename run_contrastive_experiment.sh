00030388ac3a8d6c9ce76be03d968f91e764a614
Tasha Pais <tasha@stem.ai>
2025-10-16 17:49:10 -0700

    27 seeds

--- run_contrastive_experiment.sh
+++ run_contrastive_experiment.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-BASE_RUN_NAME="tasha.10.15.shaped_hypers_vit_2"
+BASE_RUN_NAME="tasha.10.16.shaped_hypers_vit"
 
-PAIRED_SEEDS=(42 123 456)
+PAIRED_SEEDS=(67 134 201 268 335 402 469 536 603 670 737 804 871 938 1005 1072 1139 1206 1273 1340 1407 1474 1541 1608 1675 1742 1809)
 
 for i in "${!PAIRED_SEEDS[@]}"; do
     SEED=${PAIRED_SEEDS[$i]}
@@ -33,5 +33,5 @@ for i in "${!PAIRED_SEEDS[@]}"; do
     echo ""
 done
 
-echo "All 6 jobs launched!"
+echo "All 54 jobs launched! (27 seed pairs)"
 echo "Monitor with: sky jobs queue"
