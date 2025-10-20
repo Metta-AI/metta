edf6ab9cf498086f2afeb2b26b653913c228f77e
Daphne <45669133+daphnedemekas@users.noreply.github.com>
2025-10-20 15:51:38 -0700

    Assembly lines recipe (#3349)
    Recipe for resource chains using assemblers
    
    [Asana
    Task](https://app.asana.com/1/1209016784099267/project/1210348820405981/task/1211700004393211)

--- experiments/evals/assembly_lines.py
+++ experiments/evals/assembly_lines.py
@@ -1,93 +1,91 @@
+import random
+
 from metta.sim.simulation_config import SimulationConfig
 from mettagrid.config.mettagrid_config import MettaGridConfig
-import random
-from experiments.recipes.in_context_learning.converters.converter_chains import (
-    ConverterChainTaskGenerator,
+
+from experiments.recipes.assembly_lines import (
+    AssemblyLinesTaskGenerator,
     make_task_generator_cfg,
 )
 
 
-def converter_chain_eval_env(env: MettaGridConfig) -> MettaGridConfig:
-    env.game.agent.resource_limits["heart"] = 6
-    return env
-
-
-def make_converter_chain_eval_env(
+def make_assembly_line_eval_env(
     chain_length: int,
     num_sinks: int,
     room_size: str,
+    terrain: str,
 ) -> MettaGridConfig:
     task_generator_cfg = make_task_generator_cfg(
         chain_lengths=[chain_length],
         num_sinks=[num_sinks],
         room_sizes=[room_size],
-        map_dir=None,  # for evals, generate the environments algorithmically
+        terrains=[terrain],
     )
-    task_generator = ConverterChainTaskGenerator(task_generator_cfg)
+    task_generator = AssemblyLinesTaskGenerator(task_generator_cfg)
     # different set of resources and converters for evals
     return task_generator.get_task(random.randint(0, 1000000))
 
 
-def make_converter_chain_eval_suite() -> list[SimulationConfig]:
+def make_assembly_line_eval_suite() -> list[SimulationConfig]:
     return [
         SimulationConfig(
             suite="in_context_ordered_chains",
             name="2c_2s_medium",
-            env=make_converter_chain_eval_env(2, 2, "medium"),
+            env=make_assembly_line_eval_env(2, 2, "medium", "no-terrain"),
         ),
         SimulationConfig(
             suite="in_context_ordered_chains",
-            name="2c_2s_medium_terrain",
-            env=make_converter_chain_eval_env(2, 2, "medium"),
+            name="2c_2s_medium_balanced",
+            env=make_assembly_line_eval_env(2, 2, "medium", "balanced"),
         ),
         SimulationConfig(
             suite="in_context_ordered_chains",
             name="3c_1s_medium",
-            env=make_converter_chain_eval_env(3, 1, "medium"),
+            env=make_assembly_line_eval_env(3, 1, "medium", "no-terrain"),
         ),
         SimulationConfig(
             suite="in_context_ordered_chains",
             name="3c_2s_medium",
-            env=make_converter_chain_eval_env(3, 2, "medium"),
+            env=make_assembly_line_eval_env(3, 2, "medium", "no-terrain"),
         ),
         SimulationConfig(
             suite="in_context_ordered_chains",
             name="4c_1s_medium_terrain_dense",
-            env=make_converter_chain_eval_env(4, 1, "medium"),
+            env=make_assembly_line_eval_env(4, 1, "medium", "dense"),
         ),
         SimulationConfig(
             suite="in_context_ordered_chains",
-            name="4c_1s_medium_terrain",
-            env=make_converter_chain_eval_env(4, 1, "medium"),
+            name="4c_1s_medium_balanced",
+            env=make_assembly_line_eval_env(4, 1, "medium", "balanced"),
         ),
         SimulationConfig(
             suite="in_context_ordered_chains",
-            name="4c_2s_medium_terrain",
-            env=make_converter_chain_eval_env(4, 2, "medium"),
+            name="4c_2s_medium_balanced",
+            env=make_assembly_line_eval_env(4, 2, "medium", "balanced"),
         ),
         SimulationConfig(
             suite="in_context_ordered_chains",
-            name="4c_2s_large_terrain",
-            env=make_converter_chain_eval_env(4, 2, "large"),
+            name="4c_2s_large_balanced",
+            env=make_assembly_line_eval_env(4, 2, "large", "balanced"),
         ),
         SimulationConfig(
             suite="in_context_ordered_chains",
             name="5c_1s_medium",
-            env=make_converter_chain_eval_env(5, 1, "medium"),
+            env=make_assembly_line_eval_env(5, 1, "medium", "no-terrain"),
         ),
         SimulationConfig(
             suite="in_context_ordered_chains",
-            name="5c_1s_medium_terrain",
-            env=make_converter_chain_eval_env(5, 1, "medium"),
+            name="5c_1s_medium_balanced",
+            env=make_assembly_line_eval_env(5, 1, "medium", "balanced"),
         ),
         SimulationConfig(
             suite="in_context_ordered_chains",
-            name="5c_2s_medium_terrain",
-            env=make_converter_chain_eval_env(5, 2, "medium"),
+            name="5c_2s_medium_balanced",
+            env=make_assembly_line_eval_env(5, 2, "medium", "balanced"),
         ),
         SimulationConfig(
             suite="in_context_ordered_chains",
-            name="5c_2s_large_terrain_dense",
-            env=make_converter_chain_eval_env(5, 2, "large"),
+            name="5c_2s_large_dense",
+            env=make_assembly_line_eval_env(5, 2, "large", "dense"),
         ),
     ]
