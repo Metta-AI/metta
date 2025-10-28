5c7e949ac8428939ceaa4f1b3bb17a4eb4be3661
Paul Tsier <ptsier@gmail.com>
2025-10-28 13:36:07 -0700

    Fix app_backend build (#3446)
    [Asana
    Task](https://app.asana.com/1/1209016784099267/project/1210348820405981/task/1211774698709713)
    
    Co-authored-by: Paul Tsier <paul@elementanalytics.com>

--- common/src/metta/common/auth/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Authentication utilities for CLI tools.
-
-This module re-exports BaseCLIAuthenticator from cogames.auth.
-"""
-
-from cogames.auth import BaseCLIAuthenticator
-
-__all__ = ["BaseCLIAuthenticator"]
