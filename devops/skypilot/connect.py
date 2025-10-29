65e3456a151a5af97e637e8312c85d12d77662ac
Robb Walters <rjwalters@users.noreply.github.com>
2025-10-29 13:51:55 -0700

    Update devops/skypilot/connect.py
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

--- devops/skypilot/connect.py
+++ devops/skypilot/connect.py
@@ -34,7 +34,7 @@ def main():
 
     print("Looking up EC2 instance...")
 
-    REGIONS = get_regions_from_yaml(Path("devops/skypilot/recipes/skypilot_run.yaml"))
+    REGIONS = get_regions_from_yaml(Path("devops/skypilot/recipes/job.yaml"))
 
     instance = None
     for region in REGIONS:
