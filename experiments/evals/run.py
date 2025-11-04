d4d8fb40ca7856f3f4246b95133f287abbfed64c
Jack Heart <jack@softmax.com>
2025-11-04 15:02:37 -0800

    squash

--- experiments/evals/run.py
+++ experiments/evals/run.py
@@ -16,8 +16,10 @@ def eval(
     simulations = [
         SimulationConfig.model_validate(sim) for sim in json.loads(simulations_json)
     ]
-    return EvaluateRemoteJobTool(
-        simulations=simulations,
-        policy_uri=policy_uri,
-        job_result_file_path=job_result_file_path,
-    )
+
+
+return EvaluateRemoteJobTool(
+    simulations=simulations,
+    policy_uri=policy_uri,
+    job_result_file_path=job_result_file_path,
+)
