196951c646799cafc83b493bb034a85ab3625e49
Alex Smith <sasmith@gmail.com>
2025-11-05 14:17:23 -0800

    Update chests to work on vibes (#3537)
    This switches chests from operating on position to operating on vibes.
    The current default is that you put all your stuff in the chest as the
    default action, and then vibe a specific resource to get that resource
    out. I.e., you vibe germanium to get germanium -- just like you do at
    the extractor! We're so clever.
    
    Happy to play with that the actual vibing should be, but I'm pretty sure
    this is the right mechanic for now.
    
    
    https://app.asana.com/1/1209016784099267/project/1209096222434381/task/1211829872747460?focus=true
    
    [Asana
    Task](https://app.asana.com/1/1209016784099267/project/1210348820405981/task/1211833621304891)
    
    ---------
    
    Co-authored-by: S. Alex Smith <sasmith@stem.ai>

--- packages/mettagrid/python/src/mettagrid/config/mettagrid_c_config.py
+++ packages/mettagrid/python/src/mettagrid/config/mettagrid_c_config.py
@@ -283,25 +283,38 @@ def convert_to_cpp_game_config(mettagrid_config: dict | GameConfig):
             cpp_assembler_config.start_clipped = object_config.start_clipped
             objects_cpp_params[object_type] = cpp_assembler_config
         elif isinstance(object_config, ChestConfig):
-            # Convert resource type name to ID
-            resource_type_id = resource_name_to_id.get(object_config.resource_type, 0)
-
             # Convert tag names to IDs
             tag_ids = [tag_name_to_id[tag] for tag in object_config.tags]
 
-            # Convert position_deltas from (FixedPosition, delta) to (position_index, delta)
-            position_deltas_map = {}
-            for pos, delta in object_config.position_deltas:
-                position_index = FIXED_POSITIONS.index(pos)
-                position_deltas_map[position_index] = delta
+            # Convert vibe_transfers: vibe -> resource -> delta
+            vibe_transfers_map = {}
+            for vibe_name, resource_deltas in object_config.vibe_transfers.items():
+                vibe_id = vibe_name_to_id[vibe_name]
+                resource_deltas_cpp = {
+                    resource_name_to_id[resource]: delta for resource, delta in resource_deltas.items()
+                }
+                vibe_transfers_map[vibe_id] = resource_deltas_cpp
+
+            # Convert initial inventory
+            initial_inventory_cpp = {}
+            for resource, amount in object_config.initial_inventory.items():
+                resource_id = resource_name_to_id[resource]
+                initial_inventory_cpp[resource_id] = amount
+
+            # Create inventory config with limits
+            limits_list = []
+            for resource, limit in object_config.resource_limits.items():
+                resource_id = resource_name_to_id[resource]
+                limits_list.append([[resource_id], limit])
+
+            inventory_config = CppInventoryConfig(limits=limits_list)
 
             cpp_chest_config = CppChestConfig(
                 type_id=object_config.type_id, type_name=object_type, initial_vibe=object_config.vibe
             )
-            cpp_chest_config.resource_type = resource_type_id
-            cpp_chest_config.position_deltas = position_deltas_map
-            cpp_chest_config.initial_inventory = object_config.initial_inventory
-            cpp_chest_config.max_inventory = object_config.max_inventory
+            cpp_chest_config.vibe_transfers = vibe_transfers_map
+            cpp_chest_config.initial_inventory = initial_inventory_cpp
+            cpp_chest_config.inventory_config = inventory_config
             cpp_chest_config.tag_ids = tag_ids
             objects_cpp_params[object_type] = cpp_chest_config
         else:
