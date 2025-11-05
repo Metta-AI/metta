e779219932ef04ad6f998d5d08a1cc9f68843aba
Richard Higgins <richard@relh.net>
2025-11-05 14:57:48 -0800

    neutral_faced supports heart deposit, hearts to diversity (#3574)
    [Asana
    Task](https://app.asana.com/1/1209016784099267/project/1210348820405981/task/1211855119208504)

--- packages/cogames/src/cogames/cogs_vs_clips/missions.py
+++ packages/cogames/src/cogames/cogs_vs_clips/missions.py
@@ -142,16 +142,15 @@ class NeutralFacedVariant(MissionVariant):
             change_vibe.enabled = False
             change_vibe.number_of_vibes = 1
 
-            neutral_vibe = ["default"]
-            cfg.game.vibe_names = list(neutral_vibe)
-            for obj in cfg.game.objects.values():
-                if not isinstance(obj, AssemblerConfig):
-                    continue
-                if not obj.protocols:
-                    continue
-                primary_protocol = obj.protocols[0].model_copy(deep=True)
-                primary_protocol.vibes = list(neutral_vibe)
-                obj.protocols = [primary_protocol]
+            neutral_vibe_name = "default"
+            cfg.game.vibe_names = [neutral_vibe_name]
+            for name, obj in cfg.game.objects.items():
+                if isinstance(obj, AssemblerConfig) and obj.protocols:
+                    primary_protocol = obj.protocols[0].model_copy(deep=True)
+                    primary_protocol.vibes = [neutral_vibe_name]
+                    obj.protocols = [primary_protocol]
+                elif isinstance(obj, ChestConfig) and name == "chest":
+                    obj.vibe_transfers = {neutral_vibe_name: {"heart": 255}}
 
         mission.add_env_modifier(modifier)
         return mission
