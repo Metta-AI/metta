20c3a88b5661fbc119f62cb4c3a3c8f7444207b2
David Bloomin <daveey@gmail.com>
2025-11-06 09:53:29 -0800

    cp

--- packages/mettagrid/python/src/mettagrid/envs/mettagrid_puffer_env.py
+++ packages/mettagrid/python/src/mettagrid/envs/mettagrid_puffer_env.py
@@ -152,38 +152,7 @@ class MettaGridPufferEnv(PufferEnv):
         self._buffers.rewards[:] = 0.0
         self._buffers.terminals[:] = False
         self._buffers.truncations[:] = False
-
-        # xcxc make a teacher(?)
-        # xcxc consider having the teacher be connected to the policy and set actions that way
-        # xcxc
-        class Teacher:
-            def __init__(self):
-                self.tick = 0
-
-            def get_actions(self, observations: np.ndarray, sim: Simulation) -> np.ndarray:
-                actions = np.zeros(observations.shape[0], dtype=dtype_actions)
-                for i, agent_observations in enumerate(sim.observations()):
-                    for token in agent_observations.tokens:
-                        if token.feature.name == "last_action":
-                            # This should be "go in a circle".
-                            if token.value == 0:
-                                # noop => north
-                                actions[i] = 1
-                            elif token.value == 1:
-                                # north => east
-                                actions[i] = 3
-                            elif token.value == 3:
-                                # east => south
-                                actions[i] = 2
-                            elif token.value == 2:
-                                # south => west
-                                actions[i] = 4
-                            # everything (including west) => noop, which we've already written.
-                            break
-                return actions
-
-        self._teacher = Teacher()
-        self._buffers.teacher_actions[:] = self._teacher.get_actions(self._buffers.observations, self._sim)
+        self._buffers.teacher_actions[:] = 0
 
     @override
     def reset(self, seed: Optional[int] = None) -> Tuple[np.ndarray, Dict[str, Any]]:
@@ -200,8 +169,6 @@ class MettaGridPufferEnv(PufferEnv):
         if self._sim._c_sim.terminals().all() or self._sim._c_sim.truncations().all():
             self._new_sim()
 
-        self._buffers.actions[:] = actions
-
         if self._current_cfg.teacher.enabled:
             agents = self._sim.agents()
             teacher_actions = [
@@ -215,10 +182,6 @@ class MettaGridPufferEnv(PufferEnv):
                 self._buffers.actions[:] = self._buffers.teacher_actions[:]
 
         self._sim.step()
-
-        # Set after we step, so the policy has access to the teacher's actions.
-        if self._teacher is not None:
-            self._buffers.teacher_actions[:] = self._teacher.get_actions(self._buffers.observations, self._sim)
 
         return (
             self._buffers.observations,
