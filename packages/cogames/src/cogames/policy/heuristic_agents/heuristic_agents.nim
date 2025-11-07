e1b34b125c1fd3581a18367d4bbe6ce53668f300
treeform <starplant@gmail.com>
2025-11-07 15:56:51 -0800

    map  building works

--- packages/cogames/src/cogames/policy/heuristic_agents/heuristic_agents.nim
+++ packages/cogames/src/cogames/policy/heuristic_agents/heuristic_agents.nim
@@ -1,5 +1,5 @@
 import
-  std/[strformat, strutils, tables, random],
+  std/[strformat, strutils, tables, random, sets],
   genny, jsony
 
 type
@@ -25,18 +25,27 @@ type
     x*: int
     y*: int
 
+  MapBounds* = object
+    minX*: int
+    maxX*: int
+    minY*: int
+    maxY*: int
+
   HeuristicAgent* = ref object
     agentId: int
+
     map: Table[Location, seq[FeatureValue]]
+    seen: HashSet[Location]
     config: Config
     random: Rand
+    location: Location
 
 proc ctrlCHandler() {.noconv.} =
   ## Handle ctrl-c signal to exit cleanly.
   echo "\nNim DLL caught ctrl-c, exiting..."
   quit(0)
 
-proc init() =
+proc initCHook() =
   setControlCHook(ctrlCHandler)
   echo "HeuristicAgents initialized"
 
@@ -74,31 +83,33 @@ proc newHeuristicAgent(agentId: int, environmentConfig: string): HeuristicAgent 
 proc reset(agent: HeuristicAgent) =
   echo "Resetting heuristic agent ", agent.agentId
 
+proc computeMapBounds(map: Table[Location, seq[FeatureValue]]): MapBounds =
+  ## Compute the bounds of the map.
+  result.minX = -5
+  result.maxX = 5
+  result.minY = -5
+  result.maxY = 5
+  for location, featureValues in map:
+    if location.x < result.minX:
+      result.minX = location.x
+    if location.x > result.maxX:
+      result.maxX = location.x
+    if location.y < result.minY:
+      result.minY = location.y
+    if location.y > result.maxY:
+      result.maxY = location.y
+
 proc drawMap(map: Table[Location, seq[FeatureValue]]) =
   ## Draw the map to the console.
-  var
-    minX = -5
-    maxX = 5
-    minY = -5
-    maxY = 5
-  for location, featureValues in map:
-    if location.x < minX:
-      minX = location.x
-    if location.x > maxX:
-      maxX = location.x
-    if location.y < minY:
-      minY = location.y
-    if location.y > maxY:
-      maxY = location.y
-  echo "Map: ", minX, ", ", maxX, ", ", minY, ", ", maxY
+  let bounds = computeMapBounds(map)
   var line = "+"
-  for x in minX .. maxX:
+  for x in bounds.minX .. bounds.maxX:
     line.add "--"
   line.add "+"
   echo line
-  for y in minY .. maxY:
+  for y in bounds.minY .. bounds.maxY:
     line = "|"
-    for x in minX .. maxX:
+    for x in bounds.minX .. bounds.maxX:
       var cell = "  "
       if Location(x: x, y: y) in map:
         for featureValue in map[Location(x: x, y: y)]:
@@ -128,10 +139,104 @@ proc drawMap(map: Table[Location, seq[FeatureValue]]) =
     line.add "|"
     echo line
   line = "+"
-  for x in minX .. maxX:
+  for x in bounds.minX .. bounds.maxX:
     line.add "--"
   line.add "+"
   echo line
+
+proc getTypeId(map: Table[Location, seq[FeatureValue]], location: Location): int =
+  ## Get the type id of the location in the map.
+  if location in map:
+    for featureValue in map[location]:
+      if featureValue.featureId == 0:
+        return featureValue.value
+  return -1
+
+proc getLastAction(visible: Table[Location, seq[FeatureValue]]): int =
+  ## Get the last action of the visible map.
+  let lastActionId = 8 # last_action
+  echo "searching for last action in visible map"
+  if Location(x: 0, y: 0) in visible:
+    for featureValue in visible[Location(x: 0, y: 0)]:
+      echo "featureValue: ", featureValue.featureId, " ", featureValue.value
+      if featureValue.featureId == lastActionId:
+        echo "found last action: ", featureValue.value
+        return featureValue.value
+  return -1
+
+proc updateMap(agent: HeuristicAgent, visible: Table[Location, seq[FeatureValue]]) =
+  ## Update the big map with the small visible map.
+
+  if agent.map.len == 0:
+    # First time we're called, just copy the visible map to the big map.
+    agent.map = visible
+    agent.location = Location(x: 0, y: 0)
+    return
+
+  # Try to best guess where the new map is located in the big map.
+  # We can only move cardinal directions, so we only need to check staying put
+  # and the 4 cardinal directions.
+  var
+    bestScore = 0
+    bestLocation = agent.location
+    possibleOffsets = @[
+      Location(x: 0, y: 0), # Staying put.
+    ]
+  var lastAction = visible.getLastAction()
+  echo "last action: ", lastAction
+  if lastAction == 1 or lastAction == -1: # Moving north.
+    possibleOffsets.add(Location(x: 0, y: -1))
+  if lastAction == 2 or lastAction == -1: # Moving south.
+    possibleOffsets.add(Location(x: 0, y: 1))
+  if lastAction == 3 or lastAction == -1: # Moving west.
+    possibleOffsets.add(Location(x: -1, y: 0))
+  if lastAction == 4 or lastAction == -1: # Moving east.
+    possibleOffsets.add(Location(x: 1, y: 0))
+  echo "possible offsets: ", possibleOffsets
+
+  for offset in possibleOffsets:
+    var score = 0
+    let location = Location(x: agent.location.x + offset.x, y: agent.location.y + offset.y)
+    for x in -5 .. 5:
+      for y in -5 .. 5:
+        let visibleTypeId = visible.getTypeId(Location(x: x, y: y))
+        let mapTypeId = agent.map.getTypeId(Location(x: x + location.x, y: y + location.y))
+        if visibleTypeId == mapTypeId:
+          if visibleTypeId == 0 or visibleTypeId == -1:
+            # No points for empty or agent locations.
+            discard
+          elif visibleTypeId == 1:
+            # There is only one assembler per map, so this is a good score.
+            score += 100
+          elif visibleTypeId == 12:
+            # Walls can repeat and cause issues, so not worth much.
+            score += 1
+          else:
+            # Other types of features are worth 1 point each.
+            score += 10
+    echo "score for location ", location.x, ", ", location.y, " is ", score
+    if score > bestScore:
+      bestScore = score
+      bestLocation = location
+  echo "  best location: ", bestLocation.x, ", ", bestLocation.y, " score: ", bestScore
+
+  # Update the big map with the small visible map.
+  if bestScore < 2:
+    echo "Looks like we are lost?"
+    echo "  current location: ", agent.location.x, ", ", agent.location.y
+    echo "  best location: ", bestLocation.x, ", ", bestLocation.y
+  else:
+    echo "Updating map with location: ", bestLocation.x, ", ", bestLocation.y
+    agent.location =  bestLocation
+    for x in -5 .. 5:
+      for y in -5 .. 5:
+        let visibleLocation = Location(x: x, y: y)
+        let location = Location(x: x + agent.location.x, y: y + agent.location.y)
+        if visibleLocation in visible:
+          agent.map[location] = visible[visibleLocation]
+        else:
+          agent.map[location] = @[]
+
 
 proc step(
   agent: HeuristicAgent,
@@ -144,13 +249,13 @@ proc step(
 ) {.raises: [].} =
   try:
     echo "Thinking heuristic agent ", agent.agentId
-    echo "  numAgents", numAgents
-    echo "  numTokens", numTokens
-    echo "  sizeToken", sizeToken
-    echo "  numActions", numActions
+    # echo "  numAgents", numAgents
+    # echo "  numTokens", numTokens
+    # echo "  sizeToken", sizeToken
+    # echo "  numActions", numActions
     let observations = cast[ptr UncheckedArray[uint8]](rowObservations)
 
-    agent.map.clear()
+    var map: Table[Location, seq[FeatureValue]]
     for token in 0 ..< numTokens:
       let locationPacked = observations[token * sizeToken + agent.agentId * numTokens * sizeToken]
       let featureId = observations[token * sizeToken + agent.agentId * numTokens * sizeToken + 1]
@@ -162,19 +267,26 @@ proc step(
         location.y = (locationPacked shr 4).int - 5
         location.x = (locationPacked and 0x0F).int - 5
       #echo "  token ", token, " loc ", location.x, ", ", location.y, " featureId ", featureId, " value ", value
-      if not agent.map.hasKey(location):
-        agent.map[location] = @[]
-      agent.map[location].add(FeatureValue(featureId: featureId.int, value: value.int))
+      if location notin map:
+        map[location] = @[]
+      map[location].add(FeatureValue(featureId: featureId.int, value: value.int))
 
+    echo "current location: ", agent.location.x, ", ", agent.location.y
+    echo "visible map:"
+    drawMap(map)
+    updateMap(agent, map)
+    echo "updated map:"
     drawMap(agent.map)
 
     let actions = cast[ptr UncheckedArray[int32]](rawActions)
-
-    actions[agent.agentId] = agent.random.rand(1 .. 4).int32
-
+    let action = agent.random.rand(1 .. 4).int32
+    actions[agent.agentId] = action
+    echo "taking action ", action
 
   except:
-    quit("should't happen: " & getCurrentExceptionMsg())
+    echo getCurrentException().getStackTrace()
+    echo getCurrentExceptionMsg()
+    quit()
 
 exportRefObject HeuristicAgent:
   constructor:
@@ -186,7 +298,7 @@ exportRefObject HeuristicAgent:
     step
 
 exportProcs:
-  init
+  initCHook
 
 writeFiles("bindings/generated", "HeuristicAgents")
 
