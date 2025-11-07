b7c6035fb037abef996c04ff1865643e151815a9
root <root@ip-172-31-31-184.ec2.internal>
2025-11-07 23:28:04 +0000

    fix: construct policies on requested device

--- packages/mettagrid/python/src/mettagrid/policy/utils.py
+++ packages/mettagrid/python/src/mettagrid/policy/utils.py
@@ -55,10 +55,10 @@ def initialize_or_load_policy(
 
     policy_class = load_symbol(policy_class_path)
 
-    try:
-        policy = policy_class(policy_env_info, device=device)  # type: ignore[misc]
-    except TypeError:
+    if device is None:
         policy = policy_class(policy_env_info)  # type: ignore[misc]
+    else:
+        policy = policy_class(policy_env_info, device=device)  # type: ignore[misc]
 
     if policy_data_path:
         if not isinstance(policy, TrainablePolicy):
