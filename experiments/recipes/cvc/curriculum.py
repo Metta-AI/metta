0744bf8a0e0894ccb920f1349f526320ea48ab0c
Richard Higgins <richard@softmax.com>
2025-11-07 09:00:25 -0800

    linted

--- experiments/recipes/cvc/curriculum.py
+++ experiments/recipes/cvc/curriculum.py
@@ -1,5 +1,10 @@
 """Curriculum CoGs vs Clips entry points."""
 
-from experiments.recipes.cogs_v_clips import make_curriculum, make_training_env, play, train
+from experiments.recipes.cogs_v_clips import (
+    make_curriculum,
+    make_training_env,
+    play,
+    train,
+)
 
 __all__ = ["train", "make_curriculum", "make_training_env", "play"]
