2e0e0964af39f51822e797ef264b227254506423
Richard Higgins <richard@softmax.com>
2025-11-06 16:57:12 -0800

    updated to match losses config

--- experiments/recipes/cvc_easy.py
+++
@@ -1,8 +0,0 @@
-"""Backwards-compatible wrapper for the easy preset recipe."""
-
-from experiments.recipes.cvc.easy import training as easy_training
-
-train = easy_training.train
-play = easy_training.play
-
-__all__ = ["train", "play"]
