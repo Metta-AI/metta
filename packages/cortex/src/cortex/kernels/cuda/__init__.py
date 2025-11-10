f5303b7c89a1b00b8c3d16b314f8e12e66bae611
subho406 <subhojeetpramanik@gmail.com>
2025-11-10 01:35:29 +0000

    test case failing fix

--- packages/cortex/src/cortex/kernels/cuda/__init__.py
+++ packages/cortex/src/cortex/kernels/cuda/__init__.py
@@ -1,7 +1,8 @@
 """CUDA kernels for Cortex."""
 
 from .agalite import discounted_sum_cuda
-from .rtu import rtu_stream_diag_cuda, rtu_stream_full_cuda
+from .rtu.rtu_stream_diag_cuda import rtu_stream_diag_cuda
+from .rtu.rtu_stream_full_cuda import rtu_stream_full_cuda
 from .srht import srht_cuda
 
 __all__ = [
