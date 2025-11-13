ca5b8575a881e49ee9243c8109d510982972f91c
Richard Higgins <richard@relh.net>
2025-11-12 19:40:47 -0800

    try and except

--- packages/cogames/src/cogames/policy/nim_agents/race_car_agents.nim
+++ packages/cogames/src/cogames/policy/nim_agents/race_car_agents.nim
@@ -105,55 +105,61 @@ proc raceCarStepInternal(
   rawObservation: pointer,
   agentAction: ptr int32
 ) =
-  echo "Driving race car agent ", agent.agentId
-  # echo "  numAgents", numAgents
-  # echo "  numTokens", numTokens
-  # echo "  sizeToken", sizeToken
-  # echo "  numActions", numActions
-  let observations = cast[ptr UncheckedArray[uint8]](rawObservation)
+  try:
+    echo "Driving race car agent ", agent.agentId
+    # echo "  numAgents", numAgents
+    # echo "  numTokens", numTokens
+    # echo "  sizeToken", sizeToken
+    # echo "  numActions", numActions
+    let observations = cast[ptr UncheckedArray[uint8]](rawObservation)
 
-  var map: Table[Location, seq[FeatureValue]]
-  for token in 0 ..< numTokens:
-    let locationPacked = observations[token * sizeToken]
-    let featureId = observations[token * sizeToken + 1]
-    let value = observations[token * sizeToken + 2]
-    if locationPacked == 255 and featureId == 255 and value == 255:
-      break
-    var location: Location
-    if locationPacked != 0xFF:
-      location.y = (locationPacked shr 4).int - 5
-      location.x = (locationPacked and 0x0F).int - 5
-    #echo "  token ", token, " loc ", location.x, ", ", location.y, " featureId ", featureId, " value ", value
-    if location notin map:
-      map[location] = @[]
-    map[location].add(FeatureValue(featureId: featureId.int, value: value.int))
+    var map: Table[Location, seq[FeatureValue]]
+    for token in 0 ..< numTokens:
+      let locationPacked = observations[token * sizeToken]
+      let featureId = observations[token * sizeToken + 1]
+      let value = observations[token * sizeToken + 2]
+      if locationPacked == 255 and featureId == 255 and value == 255:
+        break
+      var location: Location
+      if locationPacked != 0xFF:
+        location.y = (locationPacked shr 4).int - 5
+        location.x = (locationPacked and 0x0F).int - 5
+      #echo "  token ", token, " loc ", location.x, ", ", location.y, " featureId ", featureId, " value ", value
+      if location notin map:
+        map[location] = @[]
+      map[location].add(FeatureValue(featureId: featureId.int, value: value.int))
 
-  echo "current location: ", agent.location.x, ", ", agent.location.y
-  echo "visible map:"
-  agent.cfg.drawMap(map, initHashSet[Location]())
-  updateMap(agent, map)
-  echo "updated map:"
-  agent.cfg.drawMap(agent.map, agent.seen)
+    echo "current location: ", agent.location.x, ", ", agent.location.y
+    echo "visible map:"
+    agent.cfg.drawMap(map, initHashSet[Location]())
+    updateMap(agent, map)
+    echo "updated map:"
+    agent.cfg.drawMap(agent.map, agent.seen)
 
-  let vibe = agent.cfg.getVibe(map)
-  echo "vibe: ", vibe
+    let vibe = agent.cfg.getVibe(map)
+    echo "vibe: ", vibe
 
-  let invEnergy = agent.cfg.getInventory(map, agent.cfg.features.invEnergy)
-  let invCarbon = agent.cfg.getInventory(map, agent.cfg.features.invCarbon)
-  let invOxygen = agent.cfg.getInventory(map, agent.cfg.features.invOxygen)
-  let invGermanium = agent.cfg.getInventory(map, agent.cfg.features.invGermanium)
-  let invSilicon = agent.cfg.getInventory(map, agent.cfg.features.invSilicon)
-  let invHeart = agent.cfg.getInventory(map, agent.cfg.features.invHeart)
-  let invDecoder = agent.cfg.getInventory(map, agent.cfg.features.invDecoder)
-  let invModulator = agent.cfg.getInventory(map, agent.cfg.features.invModulator)
-  let invResonator = agent.cfg.getInventory(map, agent.cfg.features.invResonator)
-  let invScrambler = agent.cfg.getInventory(map, agent.cfg.features.invScrambler)
+    let invEnergy = agent.cfg.getInventory(map, agent.cfg.features.invEnergy)
+    let invCarbon = agent.cfg.getInventory(map, agent.cfg.features.invCarbon)
+    let invOxygen = agent.cfg.getInventory(map, agent.cfg.features.invOxygen)
+    let invGermanium = agent.cfg.getInventory(map, agent.cfg.features.invGermanium)
+    let invSilicon = agent.cfg.getInventory(map, agent.cfg.features.invSilicon)
+    let invHeart = agent.cfg.getInventory(map, agent.cfg.features.invHeart)
+    let invDecoder = agent.cfg.getInventory(map, agent.cfg.features.invDecoder)
+    let invModulator = agent.cfg.getInventory(map, agent.cfg.features.invModulator)
+    let invResonator = agent.cfg.getInventory(map, agent.cfg.features.invResonator)
+    let invScrambler = agent.cfg.getInventory(map, agent.cfg.features.invScrambler)
 
-  echo &"H:{invHeart} E:{invEnergy} C:{invCarbon} O2:{invOxygen} Ge:{invGermanium} Si:{invSilicon} D:{invDecoder} M:{invModulator} R:{invResonator} S:{invScrambler}"
+    echo &"H:{invHeart} E:{invEnergy} C:{invCarbon} O2:{invOxygen} Ge:{invGermanium} Si:{invSilicon} D:{invDecoder} M:{invModulator} R:{invResonator} S:{invScrambler}"
 
-  let action = agent.random.rand(1 .. 4).int32
-  agentAction[] = action
-  echo "taking action ", action
+    let action = agent.random.rand(1 .. 4).int32
+    agentAction[] = action
+    echo "taking action ", action
+
+  except:
+    echo getCurrentException().getStackTrace()
+    echo getCurrentExceptionMsg()
+    quit()
 
 
 proc step*(
