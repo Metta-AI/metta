d6da23c227c74c8b8e2f32764a7933d4327da3cf
Richard Higgins <richard@softmax.com>
2025-11-17 09:05:43 -0800

    pickling problems

--- packages/cogames/src/cogames/curricula.py
+++ packages/cogames/src/cogames/curricula.py
@@ -2,23 +2,25 @@
 
 from collections import deque
 from dataclasses import dataclass
-from typing import Callable, List, Tuple
+from typing import Callable
 
 from mettagrid.config.mettagrid_config import MettaGridConfig
+
+
+@dataclass
+class RotationSupplier:
+    """Picklable callable that cycles through (name, cfg) tuples."""
+
+    rotation: deque
+
+    def __call__(self) -> MettaGridConfig:
+        _, cfg = self.rotation[0]
+        self.rotation.rotate(-1)
+        return cfg
 
 
 def make_rotation(missions: list[tuple[str, MettaGridConfig]]) -> Callable[[], MettaGridConfig]:
     if not missions:
         raise ValueError("Must have at least one mission in rotation")
 
-    # Use a picklable callable to support multiprocessing (spawn) backends.
-    @dataclass
-    class _RotationSupplier:
-        rotation: deque  # deque of (name, cfg) tuples
-
-        def __call__(self) -> MettaGridConfig:
-            _, cfg = self.rotation[0]
-            self.rotation.rotate(-1)
-            return cfg
-
-    return _RotationSupplier(deque(missions))
+    return RotationSupplier(deque(missions))
