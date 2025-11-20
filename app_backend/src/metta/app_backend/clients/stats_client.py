7634d0a907272f92cfcb2e58a6f82ef622876ba5
Vyacheslav Matyukhin <me@berekuk.ru>
2025-11-19 21:09:44 -0300

    change leaderboard routes to GET

--- app_backend/src/metta/app_backend/clients/stats_client.py
+++ app_backend/src/metta/app_backend/clients/stats_client.py
@@ -155,14 +155,14 @@ class StatsClient:
     def get_leaderboard_policies_v2_users_me(self) -> LeaderboardPoliciesResponse:
         return self._make_sync_request(
             LeaderboardPoliciesResponse,
-            "POST",
+            "GET",
             "/leaderboard/v2/users/me",
         )
 
     def get_leaderboard_policies_v2_for_policy(self, policy_version_id: uuid.UUID) -> LeaderboardPoliciesResponse:
         return self._make_sync_request(
             LeaderboardPoliciesResponse,
-            "POST",
+            "GET",
             f"/leaderboard/v2/policy/{policy_version_id}",
         )
 
