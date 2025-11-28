1c33f02a0b049e19565f858e490ad8ca3c2b18f1
Eckhardt (Kaizen) Dreyer <eckhardt.dreyer@gmail.com>
2025-11-28 19:57:24 +0200

    feat(gridworks): Filter mission variants in dropdown (#4086)
    Allow filtering in the variant selector.
    
    [Asana
    Task](https://app.asana.com/1/1209016784099267/project/1210006613945419/task/1212210337332217?focus=true)
    
    [Asana
    Task](https://app.asana.com/1/1209016784099267/project/1210348820405981/task/1212208105815210)

--- gridworks/src/components/FilterInput.tsx
+++ gridworks/src/components/FilterInput.tsx
@@ -1,17 +1,48 @@
-import { FC } from "react";
+"use client";
+import clsx from "clsx";
+import { type FC, useEffect, useRef } from "react";
 
 export const FilterInput: FC<{
-  value: string;
-  onChange: (value: string) => void;
+  className?: string;
+  focus?: boolean;
   placeholder?: string;
-}> = ({ value, onChange, placeholder = "Filter..." }) => {
+  value: string;
+  onBlur?: () => void;
+  onChange: (value: string) => void;
+}> = ({
+  className,
+  focus = false,
+  placeholder = "Filter...",
+  value,
+  onBlur,
+  onChange,
+}) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const prevFocusRef = useRef(false);
+
+  useEffect(() => {
+    if (focus && !prevFocusRef.current) {
+      // Render on next tick
+      const id = setTimeout(() => {
+        console.dir(inputRef.current);
+        inputRef.current?.focus();
+        inputRef.current?.select();
+      }, 0);
+
+      return () => clearTimeout(id);
+    }
+    prevFocusRef.current = focus;
+  }, [focus]);
+
   return (
     <input
+      ref={inputRef}
       type="text"
       value={value}
       placeholder={placeholder}
-      className="w-full rounded border border-gray-300 p-2 md:w-1/2 lg:w-1/3"
+      className={clsx("w-full rounded border border-gray-300 p-2", className)}
       onChange={(e) => onChange(e.target.value)}
+      onBlur={onBlur}
     />
   );
 };
