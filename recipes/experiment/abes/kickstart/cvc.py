2d0cad02d764a13201b3bd87aee832c8b35e545a
Alex Vardakostas <avardakostas@gmail.com>
2025-12-01 17:23:03 -0800

    sp

--- recipes/experiment/abes/kickstart/cvc.py
+++ recipes/experiment/abes/kickstart/cvc.py
@@ -11,7 +11,7 @@ from typing import Optional, Sequence
 
 import metta.cogworks.curriculum as cc
 from cogames.cli.mission import find_mission, parse_variants
-from cogames.cogs_vs_clips.evals.eval_missions import EVAL_MISSIONS
+from cogames.cogs_vs_clips.evals.integrated_evals import EVAL_MISSIONS
 from cogames.cogs_vs_clips.mission import MAP_MISSION_DELIMITER, Mission, NumCogsVariant
 from cogames.cogs_vs_clips.missions import MISSIONS
 from cogames.cogs_vs_clips.variants import VARIANTS
