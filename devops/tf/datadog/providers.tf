e9bd194fd52a198754635bf3fd047e8eb2b593d5
Vyacheslav Matyukhin <me@berekuk.ru>
2025-12-05 19:53:05 -0300

    datadog tf stack - aws integration

---
+++ devops/tf/datadog/providers.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    datadog = {
+      source = "DataDog/datadog"
+    }
+  }
+}
+
+provider "aws" {}
+
+data "aws_secretsmanager_secret" "datadog_api_key" {
+  name = "datadog/api-key"
+}
+
+data "aws_secretsmanager_secret_version" "datadog_api_key_version" {
+  secret_id = data.aws_secretsmanager_secret.datadog_api_key.id
+}
+
+provider "datadog" {
+  api_key = data.aws_secretsmanager_secret_version.datadog_api_key_version.secret_string
+}
