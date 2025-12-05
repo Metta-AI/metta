8e8d2511bd375c7b40af8db545e05bc40cd14836
Vyacheslav Matyukhin <me@berekuk.ru>
2025-12-05 20:09:10 -0300

    app key

--- devops/tf/datadog/providers.tf
+++ devops/tf/datadog/providers.tf
@@ -21,6 +21,15 @@ data "aws_secretsmanager_secret_version" "datadog_api_key_version" {
   secret_id = data.aws_secretsmanager_secret.datadog_api_key.id
 }
 
+data "aws_secretsmanager_secret" "datadog_app_key" {
+  name = "datadog/app-key"
+}
+
+data "aws_secretsmanager_secret_version" "datadog_app_key_version" {
+  secret_id = data.aws_secretsmanager_secret.datadog_app_key.id
+}
+
 provider "datadog" {
   api_key = data.aws_secretsmanager_secret_version.datadog_api_key_version.secret_string
+  app_key = data.aws_secretsmanager_secret_version.datadog_app_key_version.secret_string
 }
