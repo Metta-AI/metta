7753dacb9f16ce69ac8cbeaf34014e69e1155d23
Richard Higgins <richard@softmax.com>
2025-12-09 16:26:31 -0800

    ???

--- recipes/experiment/cogs_v_clips.py
+++ recipes/experiment/cogs_v_clips.py
@@ -402,7 +402,7 @@ def train(
             ]
         else:
             # Run teacher on a fraction equal to initial lead prob (simple perf knob; loss still anneals)
-            tt.training_env.supervisor_subset_fraction = bc_teacher_lead_prob
+            tt.training_env.supervisor_subset_fraction = None  # full-batch teacher to match supervisor expectations
             losses.supervisor.enabled = True
             losses.supervisor.teacher_lead_prob = bc_teacher_lead_prob
             anneal_start = int(bc_total_steps * 0.5)
