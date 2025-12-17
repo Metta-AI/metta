02fa2682d55ad2beda657faa8b83b54ae0b18800
Vyacheslav Matyukhin <me@berekuk.ru>
2025-12-17 18:56:28 -0300

    imports

---
+++ devops/tf/skypilot/imports.tf
@@ -0,0 +1,42 @@
+# Imports for migration from the previous module in eks stack
+import {
+  to = aws_db_instance.postgres
+  id = var.db_identifier
+}
+
+import {
+  to = aws_iam_policy.minimal
+  id = "arn:aws:iam::751442549699:policy/minimal-skypilot-policy"
+}
+
+import {
+  to = aws_iam_user.skypilot_api_server
+  id = "skypilot-api-server"
+}
+
+import {
+  to = aws_iam_user_policy_attachment.skypilot_api_server_attach
+  id = "arn:aws:iam::751442549699:policy/minimal-skypilot-policy"
+}
+
+import {
+  to = aws_iam_role.skypilot_v1
+  id = "skypilot-v1"
+}
+
+import {
+  to = aws_iam_role_policy_attachment.skypilot_v1_attach
+  id = "arn:aws:iam::751442549699:policy/minimal-skypilot-policy"
+}
+
+import {
+  to = aws_iam_access_key.skypilot_api_server
+  id = "skypilot-api-server"
+}
+
+import {
+  to = aws_s3_bucket.skypilot_jobs
+  identity = {
+    bucket = var.jobs_bucket
+  }
+}
