241872085e44b4a372c36e1f71fa92c16b54fa66
Richard Higgins <richard@softmax.com>
2025-12-20 16:43:17 -0500

    undo ppo fix

--- recipes/experiment/user/alex.py
+++ recipes/experiment/user/alex.py
@@ -13,8 +13,6 @@ from metta.cogworks.curriculum.curriculum import (
 )
 from metta.cogworks.curriculum.learning_progress_algorithm import LearningProgressConfig
 from metta.rl.loss.losses import LossesConfig
-from metta.rl.loss.ppo_actor import PPOActorConfig
-from metta.rl.loss.ppo_critic import PPOCriticConfig
 from metta.rl.trainer_config import TrainerConfig
 from metta.rl.training import EvaluatorConfig, TrainingEnvironmentConfig
 from metta.sim.simulation_config import SimulationConfig
@@ -101,10 +99,7 @@ def train(
 
     eval_simulations = make_evals()
     trainer_cfg = TrainerConfig(
-        losses=LossesConfig(
-            ppo_actor=PPOActorConfig(),
-            ppo_critic=PPOCriticConfig(),
-        ),
+        losses=LossesConfig(),
     )
     # policy_config = FastDynamicsConfig()
     # policy_config = FastLSTMResetConfig()
