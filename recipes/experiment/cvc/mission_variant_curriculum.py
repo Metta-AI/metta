1c52af2045683d8a785d4fdae58095b635e438b2
Richard Higgins <richard@softmax.com>
2025-12-23 13:23:24 -0500

    Update mpt references after relocation

--- recipes/experiment/cvc/mission_variant_curriculum.py
+++ recipes/experiment/cvc/mission_variant_curriculum.py
@@ -555,7 +555,7 @@ def _get_policy_action_space(policy_uri: str) -> Optional[int]:
         return None
 
     try:
-        from mettagrid.policy.mpt_artifact import load_mpt
+        from metta.rl.mpt_artifact import load_mpt
 
         artifact = load_mpt(policy_uri)
 
